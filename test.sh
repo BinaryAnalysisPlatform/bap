f67bae50c00125404241c3a8a75800025741a82e
Ivan Gotovchits <ivg@ieee.org>
2014-12-22 12:51:18 -0500

    added x86 to a test suite.

--- test.sh
+++ test.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
+TARGETS="arm x86"
+
 cd examples
 ./run.sh
 cd ..
-readbin /bin/ls > /dev/null
 
-git clone https://github.com/BinaryAnalysisPlatform/arm-binaries.git
-cd arm-binaries/coreutils
+for target in $TARGETS; do
+    git clone --depth=1 https://github.com/BinaryAnalysisPlatform/$target-binaries.git
 
-for file in `ls *`; do
-    printf '%-30s ' $file;
-    readbin $file > /dev/null;
-    if [ $? -eq 0 ]; then
-        echo 'ok';
-    else
-        echo 'fail';
-        exit 1;
-    fi;
+    for file in `find -type f -regex '.*utils_.*'`; do
+        printf '%-60s ' $file;
+        readbin $file > /dev/null;
+        if [ $? -eq 0 ]; then
+            echo 'ok';
+        else
+            echo 'fail';
+            exit 1;
+        fi;
+    done
+
+    cd -
+    rm -rf $target-binaries
 done
-
-cd -
-rm -rf arm-binaries
