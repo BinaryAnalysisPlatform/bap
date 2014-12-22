fbda3209fc74c7b1a4ac7c6726767344b1af7b65
Ivan Gotovchits <ivg@ieee.org>
2014-12-22 18:04:19 -0500

    trying to figure out what happens.

--- test.sh
+++ test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+ERROR=0
 
 test_case() {
-    printf '%-60s ' $1;
+    printf '%-70s ' $1;
     readbin $1 > /dev/null;
     if [ $? -eq 0 ]; then
         echo 'ok';
     else
         echo 'fail';
-        exit 1;
+        ERROR=1
     fi;
 }
 
@@ -22,8 +23,8 @@ cd ..
 
 for target in $TARGETS; do
     git clone --depth=1 https://github.com/BinaryAnalysisPlatform/$target-binaries.git
-    cd $target-binaries
-    find -type f -regex '.*utils_.*' | parallel test_case
-    cd -
-    rm -rf $target-binaries
+    find "$target-binaries" -type f -regex '.*utils_.*' | parallel test_case {}
+    rm -rf "$target-binaries"
 done
+
+exit $ERROR
