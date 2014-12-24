c737d40e13f0fa833b82f80fddac1b1aacd0d04b
Ivan Gotovchits <ivg@ieee.org>
2014-12-24 17:55:19 -0500

    added dependencies, fixed warnings.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
-OPAM_DEPENDS="core_kernel oasis piqi zarith bitstring utop cmdliner"
+OPAM_DEPENDS="core_kernel oasis piqi zarith bitstring utop cmdliner faillib ezjsonm lwt-zmq uri"
 SYS_DEPENDS="libgmp-dev time llvm-3.4-dev"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
