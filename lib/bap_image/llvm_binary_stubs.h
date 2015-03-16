44011d05eb19566f67ad74779f956490e2fcae64
feseal <feseal@yandex.ru>
2015-03-16 16:50:27 +0300

    Exported llvm binary class

---
+++ lib/bap_image/llvm_binary_stubs.h
@@ -0,0 +1,14 @@
+#include "caml/mlvalues.h"
+
+#ifdef __cplusplus 
+extern "C" {
+#endif
+
+    value llvm_binary_create_stub(value);
+    value llvm_binary_arch_stub(value);
+    value llvm_binary_symbols_stub(value);
+    value llvm_binary_sections_stub(value);
+
+#ifdef __cplusplus 
+}
+#endif
