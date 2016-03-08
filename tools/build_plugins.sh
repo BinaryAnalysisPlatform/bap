eac50bc653ce8996eec58f102ae5be3618a231d0
Ivan Gotovchits <ivg@ieee.org>
2016-03-08 14:11:19 -0500

    Imported header plugin from bap-plugins
    The new name for the header is api. The pass utilizes
    the new `autoload` feature, and applies known APIs on
    the program. APIs are bundled with the plugin itself.
    New API's can be added by providing a C header file.

--- tools/build_plugins.sh
+++ tools/build_plugins.sh
@@ -10,6 +10,13 @@ for plugin in `ls`; do
         touch $plugin.ml
         bapbuild -package bap-plugin-$plugin $plugin.plugin
         bapbundle update -desc "`ocamlfind query -format "%D" bap-plugin-$plugin`" $plugin.plugin
+        if [ -f $plugin/resources ]; then
+            cd $plugin
+            for line in `cat resources`; do
+                bapbundle update -add-resources $line ../$plugin.plugin
+            done
+            cd ..
+        fi
         bapbundle install $plugin.plugin
         bapbuild -clean
         rm $plugin.ml
