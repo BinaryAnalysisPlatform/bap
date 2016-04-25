911917a0fe04d3a458c8d2e7cc07f44510808362
Ivan Gotovchits <ivg@ieee.org>
2016-04-25 14:14:24 -0400

    rewrote Bap_language.

--- lib/bap_language/bap_language.ml
+++ lib/bap_language/bap_language.ml
@@ -1,26 +1,68 @@
 open Core_kernel.Std
+open Regular.Std
+open Bap_future.Std
+open Bap_plugins.Std
+open Format
 
 module Std = struct
-  module Tid = Type_equal.Id
-  type 'a language = 'a Tid.t
-  module Language = struct
-    type 'a t = 'a language
 
-    let create ~name = Tid.create ~name sexp_of_opaque
+  module Tid = Type_equal.Id
+
+  type name = ..
+
+  type language = {
+    name : name;
+    repr : string;
+  }
+
+  type 'a key = 'a Tid.t
+
+  let chosen,choose = Future.create ()
+
+
+  let name_to_string = ref (fun _ -> "<language>")
+
+  module Key = struct
+    type 'a t = 'a key
+
+    let create {repr;name} =
+      name_to_string := (fun n ->
+          if n = name then repr else !name_to_string n);
+      Tid.create ~name:repr sexp_of_opaque
 
     let name = Tid.name
 
-    module type Property = sig
+    include Printable(struct
+        type t = language
+        let module_name = Some "Bap_language.Std.Language"
+        let pp ppf {repr} =
+          pp_print_string ppf repr
+      end)
+
+    module Name = struct
+      type t = name = ..
+      include Printable(struct
+          type t = name
+          let module_name = Some "Bap_language.Std.Language.Name"
+          let pp ppf n =
+            pp_print_string ppf (!name_to_string n)
+        end)
+    end
+
+
+    module type Table = sig
       type t
       type 'a data
 
       val create : unit -> t
-      val set : t -> 'a language -> 'a data -> unit
-      val get : t -> 'a language -> 'a data option
+      val set : t -> 'a key -> 'a data -> unit
+      val get : t -> 'a key -> 'a data option
     end
 
 
-    module Property(T : T1) : Property with type 'a data = 'a T.t = struct
+
+
+    module Table(T : T1) : Table with type 'a data = 'a T.t = struct
       module U = Univ_map.Make(struct
           type 'a t = 'a T.t
           let sexp_of_t  _ = sexp_of_opaque
@@ -31,8 +73,8 @@ module Std = struct
       let empty = U.empty
 
       let create () = ref empty
-      let set t lang data = t := U.set !t lang data
-      let get t lang = U.find !t lang
+      let set t key data = t := U.set !t key data
+      let get t key = U.find !t key
     end
   end
 end
--- lib/bap_language/bap_language.mli
+++ lib/bap_language/bap_language.mli
@@ -1,6 +1,64 @@
 open Core_kernel.Std
+open Regular.Std
+open Bap_future.Std
+open Bap.Std
 
-(** A programming language abstraction.
+(** A high level programming language.
+
+    Sometimes it is useful to know in which high-level language the
+    program was originally written. Based on this knowledge it is
+    possible to recover some abstractions from the binary and make the
+    analysis more precise. This abstraction allows to write analysis,
+    that are dependent on the particular language. This module is also
+    a place, where language detection algorithms meet and decide in
+    which language the program was written.
+
+    {2 Writing an analysis that depends on the language}
+
+    A common way to write an analysis that depends on the language is
+    demonstrated in the following example, that will add an automatic
+    a hypothetical pass that resolves [applyN] function calls into
+    regular calls:
+
+    {[
+
+      open Language.Std
+      open OCaml.Std
+
+      let resolve_apply proj = (* do something with proj *) proj
+
+      let () = Future.upon Language.chosen @@ fun lang ->
+        match Language.name lang with
+        | OCaml ->
+          Project.register_pass ~autorun:true ~f:resolve_apply
+        | _ -> ()
+    ]}
+
+    The example, assumes that there exists a library that defines
+    module [OCaml.Std], that extends the set of language names with
+    constructor OCaml, e.g.,
+    {[
+      module Std : sig
+        type Language.name += OCaml
+
+        val ocaml : language
+        module OCaml : sig
+          type t = ocaml
+          (* implementation specific to the OCaml language *)
+        end
+      end
+    ]}
+
+
+    {2 Detecting the language}
+
+    First of all there should be only one system component, that is
+    responsible for selecting the language. In the Platform it is the
+    [language] platform the provides a user interface for choosing the
+    language. The library by itself doesn't decide the language, it is
+    only the common ground, where different plugins meet. So, if you
+    wrote an analysis that detects program language, then you need to
+
 
     A ['a language] is high level language represented with abstraction
     ['a]. It is possible to associate properties that depends on the type
@@ -49,25 +107,65 @@ open Core_kernel.Std
     framework, that maps API languages to ABI implementations.
 *)
 
+(** The promise to set the propery. This promise should be
+    taken by only one system component, if there is more than
+    one component, trying to decide the language, then it will
+    provoke a runtime error.  *)
+
 module Std : sig
-  type 'a language
 
-  module Language : sig
-    type 'a t = 'a language
+  module type Property = sig
+    type t
+    type 'a key
 
-    val create : name:string -> 'a t
+    val decided : t future
+    val decide : t promise
 
-    val name : 'a t -> string
+    module Key : sig
+      val create : t -> 'a key
+      val propery : 'a key -> t
+    end
 
-    module type Property = sig
+    module type Table = sig
       type t
       type 'a data
 
       val create : unit -> t
-      val set : t -> 'a language -> 'a data -> unit
-      val get : t -> 'a language -> 'a data option
+      val set : t -> 'a key -> 'a data -> unit
+      val get : t -> 'a key -> 'a data option
     end
-
-    module Property(T : T1) : Property with type 'a data = 'a T.t
+    module Table(T : T1) : Table with type 'a data = 'a T.t
   end
+
+  module type Named = sig
+    type t
+    type name = ..
+
+    val create : string -> name -> t
+    val name : t -> name
+
+    include Printable with type t := t
+    module Name : sig
+      type t = name = ..
+      include Printable with type t := t
+    end
+  end
+
+  module type Named_property = sig
+    include Property
+    include Named with type t := t
+  end
+
+  type language
+  type compiler
+  type platform
+
+  module Arch : Property with type t = arch
+
+  module Language : Named_property with type t = language
+  module Compiler : Named_property with type t = compiler
+  module Platform : Named_property with type t = platform
+
+
+
 end
