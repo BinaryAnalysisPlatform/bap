46f40390beb4424a890bc76562c2563bf0a3599e
Jay Bosamiya <jaybosamiya@gmail.com>
2016-05-27 10:25:31 -0400

    Add a DoNothing to bap_utils to prevent IDA complain

--- plugins/ida/python/bap_utils.py
+++ plugins/ida/python/bap_utils.py
@@ -1,5 +1,7 @@
 """Commonly used utilities for BAP IDAPython plugins to use."""
 
+import idaapi
+
 
 def sexp2list(s):
     """Convert S-Expression to List"""
@@ -64,3 +66,45 @@ def add_to_comment_string(comm, key, value):
             kv.append(e)
 
     return comm[:start_loc] + list2sexp(kv) + comm[end_loc:]
+
+
+class DoNothing(idaapi.plugin_t):
+    """
+    Do Nothing.
+
+    This plugin does absolutely nothing. It is created for the sole purpose of
+    being able to keep multiple non-plugin Python files which may then be used
+    as utilities by other plugins.
+
+    Usage:
+        import bap_utils
+
+        class DoNothing<SomeUniqueIdentifier>(bap_utils.DoNothing):
+            pass
+
+        def PLUGIN_ENTRY():
+            return DoNothing<SomeUniqueIdentifier>()
+    """
+
+    flags = idaapi.PLUGIN_UNL
+    comment = "Does Nothing"
+    help = "Does Nothing"
+    wanted_name = "Do Nothing"
+    wanted_hotkey = ""
+
+    def init(self):
+        """Skip plugin."""
+        return idaapi.PLUGIN_OK
+
+    def term(self):
+        """Do nothing."""
+        pass
+
+    def run(self, arg):
+        """Do nothing."""
+        pass
+
+
+def PLUGIN_ENTRY():
+    """Do not count this file as a plugin."""
+    return DoNothing()
