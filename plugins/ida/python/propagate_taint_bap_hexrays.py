7d24203cb373775c56ac4331e095d1f9c3df2207
Jay Bosamiya <jaybosamiya@gmail.com>
2016-05-27 11:44:34 -0400

    Use SimpleLine_Modifier for BAP_Taint_Pseudocode

--- plugins/ida/python/propagate_taint_bap_hexrays.py
+++ plugins/ida/python/propagate_taint_bap_hexrays.py
@@ -16,25 +16,30 @@ bap_color = {
     'gray':    0xEAEAEA,
 }
 
+from simpleline_modifier_hexrays import SimpleLine_Modifier_Hexrays
 
-class BAP_Taint_Pseudocode(idaapi.plugin_t):
+
+class BAP_Taint_Pseudocode(SimpleLine_Modifier_Hexrays):
     """Propagate taint information from Text/Graph view to Pseudocode view."""
 
-    def _autocolorize_function(self, cfunc):
-        simplelinevec = cfunc.get_pseudocode()
+    comment = "BAP Taint Plugin for Pseudocode View"
+    help = "BAP Taint Plugin for Pseudocode View"
+    wanted_name = "BAP Taint Pseudocode"
 
-        def ea_from_addr_tag(addr_tag):
-            return cfunc.treeitems.at(addr_tag).ea
+    @classmethod
+    def _simpleline_modify(cls, cfunc, sl):
+        cls._color_line(sl, bap_color['gray'])
+        # Ready to be painted over by other colors
+        for ea in cls.get_ea_list(cfunc, sl):
+            new_color = cls._get_new_color(sl.bgcolor, ea)
+            cls._color_line(sl, new_color)
 
-        for simpleline in simplelinevec:
-            self._color_line(simpleline, bap_color['gray'])
-                # Ready to be painted over by other colors
-            self._autocolorize_line(simpleline, ea_from_addr_tag)
+    @staticmethod
+    def _color_line(sl, color):
+        sl.bgcolor = color
 
-    def _color_line(self, simpleline, color):
-        simpleline.bgcolor = color
-
-    def _get_new_color(self, current_color, ea):
+    @staticmethod
+    def _get_new_color(current_color, ea):
         coloring_order = [
             bap_color[c] for c in [
                 'gray',
@@ -62,47 +67,10 @@ class BAP_Taint_Pseudocode(idaapi.plugin_t):
         else:
             return current_color
 
-    def _autocolorize_line(self, simpleline, ea_from_tag):
-        anchor = idaapi.ctree_anchor_t()
-        line = simpleline.line[:]  # Copy
-
-        def is_addr_code(l):
-            return (l[0] == idaapi.COLOR_ON and
-                    l[1] == chr(idaapi.COLOR_ADDR))
-
-        while len(line) > 0:
-            skipcode_index = idaapi.tag_skipcode(line)
-            if skipcode_index == 0:  # No code found
-                line = line[1:]  # Skip one character ahead
-            else:
-                if is_addr_code(line):
-                    addr_tag = int(line[2:skipcode_index], 16)
-                    anchor.value = addr_tag
-                    if anchor.is_citem_anchor():
-                        line_ea = ea_from_tag(addr_tag)
-                        if line_ea != idaapi.BADADDR:
-                            new_color = self._get_new_color(simpleline.bgcolor,
-                                                            line_ea)
-                            self._color_line(simpleline, new_color)
-                line = line[skipcode_index:]  # Skip the colorcodes
-
-    flags = idaapi.PLUGIN_FIX
-    comment = "BAP Taint Plugin for Pseudocode View"
-    help = "BAP Taint Plugin for Pseudocode View"
-    wanted_name = "BAP Taint Pseudocode"
-    wanted_hotkey = ""
-
     def init(self):
         """Initialize Plugin."""
         try:
             if idaapi.init_hexrays_plugin():
-                def hexrays_event_callback(event, *args):
-                    if event == idaapi.hxe_text_ready:
-                        vu, = args
-                        self._autocolorize_function(vu.cfunc)
-                    return 0
-
-                idaapi.install_hexrays_callback(hexrays_event_callback)
 
                 def cfunc_from_ea(ea):
                     func = idaapi.get_func(ea)
@@ -116,7 +84,7 @@ class BAP_Taint_Pseudocode(idaapi.plugin_t):
                     cfunc = cfunc_from_ea(ea)
                     if cfunc is None:
                         return
-                    self._autocolorize_function(cfunc)
+                    self.run_over_cfunc(cfunc)
 
                 idaapi.load_and_run_plugin('bap_propagate_taint.py', 0)
                 BAP_Taint.install_callback(autocolorize_callback)
@@ -130,22 +98,7 @@ class BAP_Taint_Pseudocode(idaapi.plugin_t):
         except AttributeError:
             print "init_hexrays_plugin() not found. Skipping Hex-Rays plugin."
 
-        return idaapi.PLUGIN_KEEP
-
-    def term(self):
-        """Terminate Plugin."""
-        try:
-            idaapi.term_hexrays_plugin()
-        except AttributeError:
-            pass
-
-    def run(self, arg):
-        """
-        Run Plugin.
-
-        Ignored since callbacks are installed.
-        """
-        pass
+        return SimpleLine_Modifier_Hexrays.init(self)  # Call superclass init()
 
 
 def PLUGIN_ENTRY():
