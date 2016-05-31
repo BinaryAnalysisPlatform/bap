6481f8744b50deb9b2c23cd866e6bbbf958fcfd1
Jay Bosamiya <jaybosamiya@gmail.com>
2016-05-31 15:56:19 -0400

    Plugin for Hex-Rays Decompiler (BAP comments + abstract base plugin + refactor) (#446)
    * Add utilities for BAP IDAPython plugins to use
    
    * Abstract Base Plugin Class to modify simplelines
    
    * Add a DoNothing to bap_utils to prevent IDA complain
    
    * PEP257
    
    * Add a DoNothing to simpleline_modifier
    
    * Fix typo
    
    * Add PLUGIN_KEEP wherever necessary
    
    * Fix minor inconveniences in hexrays taint prop
    
    * Use SimpleLine_Modifier for BAP_Taint_Pseudocode
    
    * Refactor add_to_comment_string
    
    * Add comment propagation for pseudocode view
    
    * Also allow (k v0 v1 ...) type expressions
    
    * Change output format
    
    * Move cfunc_from_ea
    
    * Ensure block comments do not cause problems
    
    * Use block comments
    
    * Ensure file opened & hexrays ready before loading
    
    * Make the block comments more stable
    
    Cleanup unnecessary comments
    
    * Revert "Use block comments"
    
    This reverts commit fdb935db85cda2c85ca99db79ca46b37d107ba1e.
    
    * Ensure correct coloring of comments
    
    * Allow quoting using " in s-expressions
    
    * Allow ( and ) in quotes in BAP_comment

---
+++ plugins/ida/python/bap_utils.py
@@ -0,0 +1,148 @@
+"""Commonly used utilities for BAP IDAPython plugins to use."""
+
+import idaapi
+
+
+def sexp2list(s):
+    """Convert S-Expression to List."""
+    sexp = [[]]
+    word = ''
+    in_str = False
+    for c in s:
+        if c == '(' and not in_str:
+            sexp.append([])
+        elif c == ')' and not in_str:
+            if word:
+                sexp[-1].append(word)
+                word = ''
+            temp = sexp.pop()
+            sexp[-1].append(temp)
+        elif c in (' ', '\n', '\t') and not in_str:
+            if word:
+                sexp[-1].append(word)
+            word = ''
+        elif c == '\"':
+            in_str = not in_str
+        else:
+            word += c
+    return sexp[0]
+
+
+def list2sexp(l):
+    """Convert List to S-Expression."""
+    if isinstance(l, str):
+        for special_char in (' ', '\n', '\t', '(', ')', '\"'):
+            if special_char in l:
+                return '\"' + l + '\"'
+        return l
+    return '(' + ' '.join(list2sexp(e) for e in l) + ')'
+
+
+def get_bap_comment(comm):
+    """
+    Get '(BAP )' style comment from given string.
+
+    Returns tuple (BAP_dict, start_loc, end_loc)
+        BAP_dict: The '(BAP )' style comment
+        start_loc: comm[:start_loc] was before the BAP comment
+        end_loc: comm[end_loc:] was after the BAP comment
+    """
+    if '(BAP ' in comm:
+        start_loc = comm.index('(BAP ')
+        bracket_count = 0
+        in_str = False
+        for i in range(start_loc, len(comm)):
+            if comm[i] == '(' and not in_str:
+                bracket_count += 1
+            elif comm[i] == ')' and not in_str:
+                bracket_count -= 1
+                if bracket_count == 0:
+                    end_loc = i + 1
+                    BAP_dict = comm[start_loc:end_loc]
+                    break
+            elif comm[i] == '\"':
+                in_str = not in_str
+        else:
+            # Invalid bracketing.
+            # Someone messed up the dict.
+            # Correct by inserting enough close brackets.
+            end_loc = len(comm)
+            BAP_dict = comm[start_loc:end_loc] + (')' * bracket_count)
+    else:
+        start_loc = len(comm)
+        end_loc = len(comm)
+        BAP_dict = '(BAP )'
+
+    return (BAP_dict, start_loc, end_loc)
+
+
+def get_bap_list(BAP_dict):
+    """Return a list containing all the values in the BAP comment."""
+    outer_removed = BAP_dict[5:-1]  # Remove outermost '(BAP', ')'
+    return sexp2list(outer_removed)
+
+
+def add_to_comment_string(comm, key, value):
+    """Add key:value to comm string."""
+    BAP_dict, start_loc, end_loc = get_bap_comment(comm)
+
+    kv = ['BAP', [key, value]]
+    for e in get_bap_list(BAP_dict):
+        if isinstance(e, list) and len(e) == 2:  # It is of the '(k v)' type
+            if e[0] != key:  # Don't append if same as required key
+                kv.append(e)
+        else:
+            kv.append(e)
+
+    return comm[:start_loc] + list2sexp(kv) + comm[end_loc:]
+
+
+def cfunc_from_ea(ea):
+    """Get cfuncptr_t from EA."""
+    func = idaapi.get_func(ea)
+    if func is None:
+        return None
+    cfunc = idaapi.decompile(func)
+    return cfunc
+
+
+class DoNothing(idaapi.plugin_t):
+    """
+    Do Nothing.
+
+    This plugin does absolutely nothing. It is created for the sole purpose of
+    being able to keep multiple non-plugin Python files which may then be used
+    as utilities by other plugins.
+
+    Usage:
+        import bap_utils
+
+        class DoNothing<SomeUniqueIdentifier>(bap_utils.DoNothing):
+            pass
+
+        def PLUGIN_ENTRY():
+            return DoNothing<SomeUniqueIdentifier>()
+    """
+
+    flags = idaapi.PLUGIN_UNL
+    comment = "Does Nothing"
+    help = "Does Nothing"
+    wanted_name = "Do Nothing"
+    wanted_hotkey = ""
+
+    def init(self):
+        """Skip plugin."""
+        return idaapi.PLUGIN_OK
+
+    def term(self):
+        """Do nothing."""
+        pass
+
+    def run(self, arg):
+        """Do nothing."""
+        pass
+
+
+def PLUGIN_ENTRY():
+    """Do not count this file as a plugin."""
+    return DoNothing()
---
+++ plugins/ida/python/propagate_comments_bap_hexrays.py
@@ -0,0 +1,42 @@
+"""Hex-Rays Plugin to propagate comments to Pseudocode View."""
+
+from simpleline_modifier_hexrays import SimpleLine_Modifier_Hexrays
+import bap_utils
+
+
+class BAP_Comment_Pseudocode(SimpleLine_Modifier_Hexrays):
+    """Propagate comments from Text/Graph view to Pseudocode view."""
+
+    @classmethod
+    def _simpleline_modify(cls, cfunc, sl):
+        sl_dict = {}
+
+        for ea in set(cls.get_ea_list(cfunc, sl)):
+            ea_comm = GetCommentEx(ea, repeatable=0)
+            if ea_comm is None:
+                continue
+            ea_BAP_dict, _, _ = bap_utils.get_bap_comment(ea_comm)
+            for e in bap_utils.get_bap_list(ea_BAP_dict):
+                if isinstance(e, list) and len(e) >= 2:  # i.e. '(k v)' type
+                    val_list = sl_dict.get(e[0], [])
+                    sexp = bap_utils.list2sexp(e[1:])
+                    if sexp not in val_list:
+                        val_list.append(sexp)
+                    sl_dict[e[0]] = val_list
+
+        if len(sl_dict) > 0:
+            BAP_dict = ['BAP']
+            for k, v in sl_dict.items():
+                BAP_dict += [[k] + v]
+            sl.line += '\x01\x0c // \x01\x0c'  # start comment coloring
+            sl.line += bap_utils.list2sexp(BAP_dict)
+            sl.line += '\x02\x0c\x02\x0c'  # stop comment coloring
+
+    comment = "BAP Comment on Pseudocode"
+    help = "BAP Comment on Pseudocode"
+    wanted_name = "BAP Comment on Pseudocode"
+
+
+def PLUGIN_ENTRY():
+    """Install BAP_Comment_Pseudocode upon entry."""
+    return BAP_Comment_Pseudocode()
