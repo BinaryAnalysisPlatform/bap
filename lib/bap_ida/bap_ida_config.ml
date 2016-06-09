8eac6658f03055050bdbd17ba15dd3ee277e3ba6
Jay Bosamiya <jaybosamiya@gmail.com>
2016-06-09 15:14:00 -0400

    Read ~ from env, and use algebraic types for the sexpr

--- lib/bap_ida/bap_ida_config.ml
+++ lib/bap_ida/bap_ida_config.ml
@@ -1,22 +1,37 @@
 open Core_kernel.Std
 
-let config_file = "~/.bap/config"
+let config_file =
+  let (/) = Filename.concat in
+  Sys.getenv "HOME" / ".bap" / "config"
+
+type bap_dict =
+  | BAP of string
+  | KV of string*string
+
+let bap_dict_of_sexp sexp = match sexp with
+  | Sexp.Atom atom -> BAP atom
+  | Sexp.List [Atom k; Atom v] -> KV (k,v)
+  | Sexp.List _ -> Sexplib.Conv_error.no_variant_match "bap_dict" sexp
+
+let stripkv kv_list =
+  List.map kv_list ~f:(
+    fun x -> match x with
+      | BAP _ -> assert false
+      | KV (k,v) -> (k,v)
+  )
 
 let config_read filename =
-  let s_pair_of_sexp = pair_of_sexp string_of_sexp string_of_sexp in
-  let s_pair_list_of_sexp = list_of_sexp s_pair_of_sexp in
   let config_pair = In_channel.with_file
       filename ~f:(fun ch -> Sexp.input_sexp ch
-                             |> pair_of_sexp string_of_sexp
-                               s_pair_list_of_sexp) in
-  let alist = match config_pair with
-    | ("BAP", x) -> x
+                             |> list_of_sexp bap_dict_of_sexp) in
+  let kv_list = match config_pair with
+    | (BAP "BAP") :: x -> x
     | _ -> assert false
   in
+  let alist = stripkv kv_list in
   match Map.of_alist String.comparator alist with
   | `Ok map -> map
   | _ -> assert false
-
 
 let config = config_read config_file
 
