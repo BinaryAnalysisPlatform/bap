bb4db4933a96b6a8bc3faed70b5348be24a065a6
Jay Bosamiya <jaybosamiya@gmail.com>
2016-06-09 16:27:11 -0400

    Use standard exceptions

--- lib/bap_ida/bap_ida_config.ml
+++ lib/bap_ida/bap_ida_config.ml
@@ -4,27 +4,23 @@ let config_file =
   let (/) = Filename.concat in
   Sys.getenv "HOME" / ".bap" / "config"
 
-exception Sexp_Error_BAP_Dict
-
 let config_alist_of_sexp sexp =
   let kv_of_sexp = pair_of_sexp string_of_sexp string_of_sexp in
   let alist_of_sexp = list_of_sexp kv_of_sexp in
   match sexp with
   | Sexp.List ((Sexp.Atom "BAP")::kv_list) -> alist_of_sexp sexp
-  | Sexp.Atom _ -> raise Sexp_Error_BAP_Dict
-  | Sexp.List _ -> raise Sexp_Error_BAP_Dict
+  | Sexp.Atom _ -> of_sexp_error "config must be a list" sexp
+  | Sexp.List _ -> of_sexp_error "config must start with \"(BAP\"" sexp
 
 let config_read filename =
   try
-    try
-      let alist = In_channel.with_file
-          filename ~f:(fun ch -> Sexp.input_sexp ch
-                                 |> config_alist_of_sexp)
-      in
-      match Map.of_alist String.comparator alist with
-      | `Ok map -> Some map
-      | _ -> None
-    with Sexp_Error_BAP_Dict -> None
+    let alist = In_channel.with_file
+        filename ~f:(fun ch -> Sexp.input_sexp ch
+                               |> config_alist_of_sexp)
+    in
+    match Map.of_alist String.comparator alist with
+    | `Ok map -> Some map
+    | _ -> None
   with Sys_error _ -> None
 
 
