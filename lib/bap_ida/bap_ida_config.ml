7ef04a102509397073c808c3d09ecbfa483f3644
Jay Bosamiya <jaybosamiya@gmail.com>
2016-06-09 14:14:17 -0400

    Use map instead of Hashtbl

--- lib/bap_ida/bap_ida_config.ml
+++ lib/bap_ida/bap_ida_config.ml
@@ -2,20 +2,29 @@ open Core_kernel.Std
 
 let config_file = "~/.bap/config"
 
-let config_to_hashtbl filename =
-  let sexp_to_hashtbl_str =
-    Sexplib.Conv.hashtbl_of_sexp string_of_sexp string_of_sexp in
-  let config_pair = In_channel.with_file filename ~f:(fun ch ->
-      Sexp.input_sexp ch
-      |> pair_of_sexp string_of_sexp sexp_to_hashtbl_str) in
-  match config_pair with
-  | ("BAP", hashtbl) -> hashtbl
+let config_read filename =
+  let s_pair_of_sexp = pair_of_sexp string_of_sexp string_of_sexp in
+  let s_pair_list_of_sexp = list_of_sexp s_pair_of_sexp in
+  let config_pair = In_channel.with_file
+      filename ~f:(fun ch -> Sexp.input_sexp ch
+                             |> pair_of_sexp string_of_sexp
+                               s_pair_list_of_sexp) in
+  let alist = match config_pair with
+    | ("BAP", x) -> x
+    | _ -> assert false
+  in
+  match Map.of_alist String.comparator alist with
+  | `Ok map -> map
   | _ -> assert false
 
-let config_hashtbl = config_to_hashtbl config_file
 
-let ida_path = match Hashtbl.find config_hashtbl "ida_path" with
-  | Some path -> path
+let config = config_read config_file
+
+let get_config key =
+  match Map.find config key with
+  | Some v -> v
   | None -> assert false
+
+let ida_path = get_config "ida_path"
 
 let is_headless = false (* TODO Read this from config *)
---
+++ lib/bap_ida/bap_ida_config.mli
@@ -0,0 +1,3 @@
+val ida_path : string
+
+val is_headless : bool
