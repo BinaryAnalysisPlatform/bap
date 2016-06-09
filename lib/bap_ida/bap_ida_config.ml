40a77013092d2d63bd14ec3eecf8243b598b892a
Jay Bosamiya <jaybosamiya@gmail.com>
2016-06-09 12:25:16 -0400

    Read config from file
    Note: This current implementation gives a Hashtbl/0.t Hashtbl/-1.t
          error. Hence, it isn't even compilable. Further changes should
          fix this.

--- lib/bap_ida/bap_ida_config.ml
+++ lib/bap_ida/bap_ida_config.ml
@@ -1,2 +1,21 @@
-let ida_path = "/home/jay/ida-6.9" (* TODO Read this from config *)
+open Core_kernel.Std
+
+let config_file = "~/.bap/config"
+
+let config_to_hashtbl filename =
+  let sexp_to_hashtbl_str =
+    Sexplib.Conv.hashtbl_of_sexp string_of_sexp string_of_sexp in
+  let config_pair = In_channel.with_file filename ~f:(fun ch ->
+      Sexp.input_sexp ch
+      |> pair_of_sexp string_of_sexp sexp_to_hashtbl_str) in
+  match config_pair with
+  | ("BAP", hashtbl) -> hashtbl
+  | _ -> assert false
+
+let config_hashtbl = config_to_hashtbl config_file
+
+let ida_path = match Hashtbl.find config_hashtbl "ida_path" with
+  | Some path -> path
+  | None -> assert false
+
 let is_headless = false (* TODO Read this from config *)
