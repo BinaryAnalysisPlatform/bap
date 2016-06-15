274954e418463e810b266e3a35a64e6905465209
Jay Bosamiya <jaybosamiya@gmail.com>
2016-06-15 17:54:50 -0400

    Remove old stuff

--- lib/bap/bap_self.mli
+++ lib/bap/bap_self.mli
@@ -11,30 +11,4 @@ module Create() : sig
   val warning : ('a,formatter,unit) format -> 'a
   val error   : ('a,formatter,unit) format -> 'a
 
-  module Config : sig
-    (** Version number  *)
-    val version : string
-
-    (** A directory for bap specific read-only architecture
-        independent data files.  *)
-    val datadir : string
-
-    (** A directory for bap specific object files, libraries, and
-        internal binaries that are not intended to be executed directly
-        by users or shell scripts *)
-    val libdir : string
-
-    (** A directory for bap specific configuaration files  *)
-    val confdir : string
-
-    (** Get configuration value for a plugin that calls it *)
-    val get : string -> string option
-
-    (** Set configuration value for a plugin that calls it *)
-    val set : name:string -> data:string -> unit
-
-    (** List all known configuration values for a plugin that calls it *)
-    val options : unit -> (string * string) list
-  end
-
 end
