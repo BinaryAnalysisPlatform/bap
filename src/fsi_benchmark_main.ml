5d4ef2c06fb34f84076eee02f03d3e4ae5381537
Jay Bosamiya <jaybosamiya@gmail.com>
2016-06-23 11:41:34 -0400

    Better error handling

--- src/fsi_benchmark_main.ml
+++ src/fsi_benchmark_main.ml
@@ -86,29 +86,34 @@ let print formatter tool result print_metrics : unit =
   output_metric_value formatter result print_metrics
 
 let compare_against bin tool_name truth_name print_metrics : unit =
-  let module EF = Monad.T.Or_error.Make(Future) in
-  let open EF in
-  (Func_start.of_tool tool_name ~testbin:bin >>| fun tool ->
-   Func_start.of_truth truth_name ~testbin:bin >>| fun truth ->
-   let result =
-     let to_set seq = Seq.fold seq ~init:Addr.Set.empty
-         ~f:Addr.Set.add in
-     let tool = to_set tool in
-     let truth = to_set truth in
-     let false_positive = Set.(length (diff tool truth)) in
-     let false_negative = Set.(length (diff truth tool)) in
-     let true_positive = Set.length truth - false_negative in
-     let ratio x = Float.(of_int true_positive / (of_int true_positive + of_int x)) in
-     let prec = ratio false_positive in
-     let recl = ratio false_negative in
-     let f_05 = 1.5 *. prec *. recl /. (0.5 *. prec +. recl) in
-     {false_positive;false_negative;true_positive;prec;recl;f_05} in
-   print std_formatter tool_name result print_metrics)
-  |> (fun x -> Future.upon x (function
-      | Ok _ -> ()
-      | Error err ->
-        printf "Function start is not recognized properly due to \
-                the following error:\n %a" Error.pp err))
+  let rooters = Rooter.Factory.list () in
+  let valid_tool = List.mem rooters tool_name in
+  if not valid_tool
+  then printf "Error: Cannot use tool %S.\n" tool_name
+  else
+    let module EF = Monad.T.Or_error.Make(Future) in
+    let open EF in
+    (Func_start.of_tool tool_name ~testbin:bin >>| fun tool ->
+     Func_start.of_truth truth_name ~testbin:bin >>| fun truth ->
+     let result =
+       let to_set seq = Seq.fold seq ~init:Addr.Set.empty
+           ~f:Addr.Set.add in
+       let tool = to_set tool in
+       let truth = to_set truth in
+       let false_positive = Set.(length (diff tool truth)) in
+       let false_negative = Set.(length (diff truth tool)) in
+       let true_positive = Set.length truth - false_negative in
+       let ratio x = Float.(of_int true_positive / (of_int true_positive + of_int x)) in
+       let prec = ratio false_positive in
+       let recl = ratio false_negative in
+       let f_05 = 1.5 *. prec *. recl /. (0.5 *. prec +. recl) in
+       {false_positive;false_negative;true_positive;prec;recl;f_05} in
+     print std_formatter tool_name result print_metrics)
+    |> (fun x -> Future.upon x (function
+        | Ok _ -> ()
+        | Error err ->
+          printf "Function start is not recognized properly due to \
+                  the following error:\n %a" Error.pp err))
 
 
 let compare_against_t = Term.(pure compare_against $bin $tool $truth $print_metrics)
