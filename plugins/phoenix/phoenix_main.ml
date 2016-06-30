77d0279e11fd355221eef31da168dbe22f67d87c
Jay Bosamiya <jaybosamiya@gmail.com>
2016-06-30 12:57:16 -0400

    Update phoenix plugin

--- plugins/phoenix/phoenix_main.ml
+++ plugins/phoenix/phoenix_main.ml
@@ -1,6 +1,5 @@
 open Core_kernel.Std
 open Bap.Std
-open Cmdliner
 open Format
 
 include Self()
@@ -17,55 +16,42 @@ let run options project =
   let dest = Phoenix.store () in
   printf "Stored data to %s@." dest
 
-let cfg_format : 'a list Term.t =
-  Arg.(value & vflag_all [`with_name] [
-      `with_name, info ["labels-with-name"]
-        ~doc: "Put block name on graph labels";
-      `with_asm, info ["labels-with-asm"]
-        ~doc:"Put assembler instructions on graph labels";
-      `with_bil, info ["labels-with-bil"]
-        ~doc:"Put bil instructions on graph labels";
-    ])
+let label_with x y =
+  let name = sprintf "labels-with-%s" x in
+  let doc = sprintf "Put %s on graph labels" y in
+  Config.(flag name ~doc)
 
-let output_folder : _ Term.t =
+let with_name = label_with "name" "block name"
+let with_asm  = label_with "asm"  "assembler instructions"
+let with_bil  = label_with "bil"  "bil instructions"
+
+let output_folder : string Config.param =
   let doc = "Output data into the specified folder" in
-  let default = "phoenix" in
-  Arg.(value & opt string default & info ["output-folder"] ~doc)
+  Config.(param string ~default:"phoenix" "output-folder" ~doc)
 
-
-let no_resolve : bool Term.t =
+let no_resolve : bool Config.param =
   let doc = "Do not resolve addresses to symbolic names" in
-  Arg.(value & flag & info ["no-resolve"] ~doc)
+  Config.(flag "no-resolve" ~doc)
 
-let keep_alive : bool Term.t =
+let keep_alive : bool Config.param =
   let doc = "Keep alive unused temporary variables" in
-  Arg.(value & flag & info ["keep-alive"] ~doc)
+  Config.(flag "keep-alive" ~doc)
 
-let no_inline : bool Term.t =
+let no_inline : bool Config.param =
   let doc = "Disable inlining temporary variables" in
-  Arg.(value & flag & info ["no-inline"] ~doc)
+  Config.(flag "no-inline" ~doc)
 
-let keep_consts : bool Term.t =
+let keep_consts : bool Config.param =
   let doc = "Disable constant folding" in
-  Arg.(value & flag & info ["keep-const"] ~doc)
+  Config.(flag "keep-const" ~doc)
 
-let no_optimizations : bool Term.t =
+let no_optimizations : bool Config.param =
   let doc = "Disable all kinds of optimizations" in
-  Arg.(value & flag & info ["no-optimizations"] ~doc)
+  Config.(flag "no-optimizations" ~doc)
 
 let create
     a b c d e f g = Phoenix_options.Fields.create
     a b c d e f g
-
-
-let options = Term.(const create
-                    $output_folder
-                    $cfg_format
-                    $no_resolve
-                    $keep_alive
-                    $no_inline
-                    $keep_consts
-                    $no_optimizations)
 
 let man = [
   `S "DESCRIPTION";
@@ -76,10 +62,23 @@ let man = [
       target file will be used as a directory name."
 ]
 
-let info = Term.info ~man ~doc ~version name
-
 let () =
-  match Term.eval ~argv ~catch:false (options,info) with
-  | `Ok options -> Project.register_pass' (run options)
-  | `Version | `Help -> exit 0
-  | `Error _ -> exit 1
+  Config.manpage man;
+  Config.when_ready (fun {Config.get=(!)} ->
+      let cfg_format =
+        let self = [] in
+        let self = if !with_name then `with_name :: self else self in
+        let self = if !with_asm  then `with_asm  :: self else self in
+        let self = if !with_bil  then `with_bil  :: self else self in
+        match self with
+        | [] -> [`with_name]
+        | _ as x -> x in
+      let options = create
+          !output_folder
+          cfg_format
+          !no_resolve
+          !keep_alive
+          !no_inline
+          !keep_consts
+          !no_optimizations in
+      Project.register_pass' (run options))
