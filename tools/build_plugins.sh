6aa6ed1f57f34ef70201f5d8dc1597ac58408092
Ivan Gotovchits <ivg@ieee.org>
2017-02-16 07:43:40 -0500

    speeds up the compilation
    a service commit, that will build plugins in parallel and employ more
    threads when building the main program.

--- tools/build_plugins.sh
+++ tools/build_plugins.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
 
-# TODO rewrite it as an ocaml script for portability
-
 set -ue
+
+build_plugin() {
+    plugin=$1
+    if ocamlfind query bap-plugin-$plugin 2>/dev/null
+    then
+        TMPDIR=`mktemp --directory`
+        cd $TMPDIR
+        touch $plugin.ml
+        bapbuild -package bap-plugin-$plugin $plugin.plugin
+        bapbundle update -desc "`ocamlfind query -format "%D" bap-plugin-$plugin`" $plugin.plugin
+        bapbundle install $plugin.plugin
+        cd -
+        rm -rf $TMPDIR
+    fi
+
+}
+
 
 cd plugins
 
 for plugin in `ls`; do
-    if ocamlfind query bap-plugin-$plugin 2>/dev/null
-    then
-        touch $plugin.ml
-        bapbuild -package bap-plugin-$plugin $plugin.plugin
-        bapbundle update -desc "`ocamlfind query -format "%D" bap-plugin-$plugin`" $plugin.plugin
-        if [ -f $plugin/resources ]; then
-            cd $plugin
-            for line in `cat resources`; do
-                bapbundle update -add-resources $line ../$plugin.plugin
-            done
-            cd ..
-        fi
-        bapbundle install $plugin.plugin
-        bapbuild -clean
-        rm $plugin.ml
-    fi
+    build_plugin $plugin &
 done
 
 cd ..
+wait
