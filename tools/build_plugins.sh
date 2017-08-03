85707434ae5559b4c54f804d5b0582c1fbb2be79
gitoleg <forown@yandex.ru>
2017-08-03 07:34:18 -0400

    fixed *.cmxs names in bap plugins (#683)
    fix https://github.com/BinaryAnalysisPlatform/bap/issues/678

--- tools/build_plugins.sh
+++ tools/build_plugins.sh
@@ -4,16 +4,16 @@ set -ue
 
 
 build_plugin() {
-    plugin=$1
-    if ocamlfind query bap-plugin-$plugin 2>/dev/null
+    plugin=bap_plugin_$1
+    if ocamlfind query bap-plugin-$1 2>/dev/null
     then
         TMPDIR=`mktemp -d`
         cd $TMPDIR
         touch $plugin.ml
-        bapbuild -package bap-plugin-$plugin $plugin.plugin
-        DESC=`ocamlfind query -format "%D" bap-plugin-$plugin`
-        CONS=`ocamlfind query -format "%(constraints)" bap-plugin-$plugin`
-        TAGS=`ocamlfind query -format "%(tags)" bap-plugin-$plugin`
+        bapbuild -package bap-plugin-$1 $plugin.plugin
+        DESC=`ocamlfind query -format "%D" bap-plugin-$1`
+        CONS=`ocamlfind query -format "%(constraints)" bap-plugin-$1`
+        TAGS=`ocamlfind query -format "%(tags)" bap-plugin-$1`
         if [ ! -z "$CONS" ]; then
             bapbundle update -cons "$CONS" $plugin.plugin
         fi
@@ -21,8 +21,10 @@ build_plugin() {
             bapbundle update -tags "$TAGS" $plugin.plugin
         fi
         bapbundle update -desc "$DESC" $plugin.plugin
+        bapbundle update -name $1 $plugin.plugin
 
-        bapbundle install $plugin.plugin
+        mv $plugin.plugin $1.plugin
+        bapbundle install $1.plugin
         cd -
         rm -rf $TMPDIR
     fi
