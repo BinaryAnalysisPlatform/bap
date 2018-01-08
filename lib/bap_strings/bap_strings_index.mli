343bd55094916e878090e19962d05cb776ec5d71
Ivan Gotovchits <ivg@ieee.org>
2018-01-08 14:42:47 -0500

    adds some docstrings to the index module

--- lib/bap_strings/bap_strings_index.mli
+++ lib/bap_strings/bap_strings_index.mli
@@ -9,10 +9,26 @@ module Persistent : sig
   module type S = sig
     type t 
     type key
+
+    (** [string idx key] returns data associated with the
+        provided [key]. Returns an empty string if there are no data.
+    *)
     val string : t -> key -> string
+
+    (** [key idx data] returns the key associated with [data], if no
+        key is associated then [Key.null] is returned *)
     val key : t -> string -> key
+
+    (** [register idx data] registers data in the index [idx]. If
+        data is already registered, then the index is returned
+        unchanged. Otherwise returns an index where a fresh new key is
+        associated with [data].  *)
     val register : t -> string -> t
+
+    (** [registered idx data returns *)
     val registered : t -> string -> bool
   end
+
+  (** Make(Key) creates an index with the given [Key]. *)
   module Make (Key : Key) : S with type key := Key.t
 end
