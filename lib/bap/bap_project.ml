dc729f266214889203d7b79d54cc4ed0545d09d4
Oleg <forown@yandex.ru>
2018-01-24 16:18:08 -0500

    minor

--- lib/bap/bap_project.ml
+++ lib/bap/bap_project.ml
@@ -119,6 +119,7 @@ module Input = struct
     let mem = Memory.create (Arch.endian arch) base big |> ok_exn in
     let section = Value.create Image.section "bap.user" in
     let data = Memmap.add Memmap.empty mem section in
+    Signal.send Info.got_img None;
     {arch; data; code = data; file = filename; finish = ident;}
 
   let available_loaders () =
