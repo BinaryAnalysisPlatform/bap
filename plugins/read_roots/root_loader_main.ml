b031841530d6831e27bf4f1966d095ef742c50fd
Oleg <forown@yandex.ru>
2018-01-25 16:10:16 -0500

    edited readme

--- plugins/read_roots/root_loader_main.ml
+++ plugins/read_roots/root_loader_main.ml
@@ -6,10 +6,9 @@ include Self ()
 
 let extract filename arch =
   let width = Arch.addr_size arch |> Size.in_bits in
-  let addr = Addr.of_int64 ~width in
   In_channel.with_file filename ~f:(fun ch ->
       Sexp.input_sexps ch |> List.map ~f:Int64.t_of_sexp |>
-      List.map ~f:addr |>
+      List.map ~f:(Addr.of_int64 ~width) |>
       Seq.of_list)
 
 let register filename =
@@ -20,17 +19,11 @@ let register filename =
 let () =
   let () = Config.manpage [
       `S "DESCRIPTION";
-      `P
-        "Read roots information from a file and provide a rooter under
-     the name $(b,file). So print $(b,--rooter=file) to use only this
-     rooter. Input file format is [<name>.<fmt>[-<ver>]]. If fmt is not
-     specified the default reader will be used, otherwise the `<fmt>`
-     part should match with some reader, registered for Addr module.
-     The `<ver>` part is optional, and defines format version.
-     Example: \"functions.sexp\" or \"functions.sexp-1.0.
-     The example above will use sexp serializer to read data from file.";
+      `P "Read roots information from a file and provide a rooter under
+          the name $(b,file). So print $(b,--rooter=file) to use only this
+          rooter.";
       `S "SEE ALSO";
-      `P "$(b,bap-plugin-objdump)(1), $(b,bap-plugin-byteweight)(1), $(b,bap-plugin-ida)(1)";
+      `P "$(b,bap-plugin-read-symbols)(1)";
     ] in
   let file = Config.(param (some non_dir_file) "from" ~docv:"SYMS"
                        ~doc:"Use this file as roots source") in
