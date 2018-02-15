be15cbd2b3ff56eec65586db0d1cc4ba62e1b11f
Ivan Gotovchits <ivg@ieee.org>
2018-02-15 11:36:22 -0500

    handles mips32 startup procedure more correctly
    Problem: is that main returns to the address that is stored in the
    RA register, which is a nop instruction, that is implemented as a `b
    0` operation, that doesn't terminate.
    
    Solution: abuse `__libc_csu_fini` as a termination procedure, and set
    RA to its address.

--- plugins/primus_lisp/lisp/libc-init.lisp
+++ plugins/primus_lisp/lisp/libc-init.lisp
@@ -14,8 +14,6 @@
   (declare (external "__libc_init"))
   (invoke-subroutine main args (ptr+1 ptr_t args)))
 
-
-
 (defun security-init-cookie ()
   "Windows CRT buffer overrun protection"
   (declare (external "__security_init_cookie")
@@ -29,3 +27,24 @@
   (let ((argc (read-word int32_t stack_on_entry))
         (argv (ptr+1 int32_t stack_on_entry)))
     (invoke-subroutine (read-word int32_t (+ stinfo 4)) argc argv)))
+
+
+(defun fini ()
+  (declare (external "__libc_csu_fini"))
+  (exit-with 0))
+
+(defun init (main argc argv auxv)
+  "GNU libc initialization stub"
+  (declare (external "__libc_start_main")
+           (context (arch "mips")))
+  (set T9 main)
+  (set RA @__libc_csu_fini)
+  (exit-with (invoke-subroutine main argc argv)))
+
+(defun init (main argc argv auxv)
+  "GNU libc initialization stub"
+  (declare (external "__libc_start_main")
+           (context (arch "mips64") (abi "mips64")))
+  (set T9 main)
+  (set RA @__libc_csu_fini)
+  (exit-with (invoke-subroutine main argc argv)))
