81319c19371d33d075ce2db09d23d7f397782815
Ivan Gotovchits <ivg@ieee.org>
2018-03-12 15:48:44 -0400

    produces correct callgraphs if there are no calls in a program (#791)
    An empty call graph was produced in a degenerative case, when there
    are no calls at all, i.e., when there are no edges in the callgraph.
    Now it will produce a graph that will have only nodes, in that case.

--- lib/bap_types/bap_ir_callgraph.ml
+++ lib/bap_types/bap_ir_callgraph.ml
@@ -9,7 +9,8 @@ module G = Graphlib.Make(Tid)(struct type t = jmp term list end)
 let create program =
   Term.enum sub_t program |> Seq.fold ~init:G.empty ~f:(fun g sub ->
       let src = Term.tid sub in
-      Term.enum blk_t sub |> Seq.fold ~init:g ~f:(fun g blk ->
+      let init = G.Node.insert src g in
+      Term.enum blk_t sub |> Seq.fold ~init ~f:(fun g blk ->
           Term.enum jmp_t blk |>
           Seq.fold ~init:Tid.Map.empty ~f:(fun dsts jmp ->
               match Ir_jmp.kind jmp with
