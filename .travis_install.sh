4b60f18a937dd9157817cb82bb6e430f57222238
Oleg <forown@yandex.ru>
2018-03-21 16:24:38 -0400

    updated qerry for installes pkg

--- .travis_install.sh
+++ .travis_install.sh
@@ -6,21 +6,17 @@ if [ "$WITH_BUILD_CACHE" == "true" ]; then
     export POST_INSTALL_HOOK='
 git clone https://github.com/BinaryAnalysisPlatform/bap-veri.git
 opam pin add bap-veri bap-veri/ -n
-x=`ocamlfind query bap-veri`
-if [ $x == "" ]; then
+x=`ocamlfind list | grep bap-veri`
+if [ $x = "" ]; then
    opam install bap-veri -y
 fi
-
-x=`ocamlfind query bap`
-if [ $x != "" ]; then
-   echo START TO PACK BAP
-   OPAM_SWITCH=`opam config var switch`
-   mkdir -p $HOME/save_opam
-   mkdir -p $HOME/save_opam/lib
-   cp -r $HOME/.opam/$OPAM_SWITCH/lib/bap* $HOME/save_opam/lib/
-   cp -r $HOME/.opam/$OPAM_SWITCH/bin/ $HOME/save_opam/
-   cp -r $HOME/.opam/$OPAM_SWITCH/share $HOME/save_opam/
-fi
+echo START TO PACK BAP
+OPAM_SWITCH=`opam config var switch`
+mkdir -p $HOME/save_opam
+mkdir -p $HOME/save_opam/lib
+cp -r $HOME/.opam/$OPAM_SWITCH/lib/bap* $HOME/save_opam/lib/
+cp -r $HOME/.opam/$OPAM_SWITCH/bin/ $HOME/save_opam/
+cp -r $HOME/.opam/$OPAM_SWITCH/share $HOME/save_opam/
 '
 fi
 
