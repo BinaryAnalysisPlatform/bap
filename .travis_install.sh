89201b8c1d8293cfd0ecf544e2ab24d7c5e35a0d
oleg <forown@yandex.ru>
2018-03-22 20:30:38 -0400

    updated

--- .travis_install.sh
+++ .travis_install.sh
@@ -5,17 +5,16 @@ export TESTS=false
 if [ "$WITH_BUILD_CACHE" == "true" ]; then
     export POST_INSTALL_HOOK='
 rm -rf bap-veri
-opam remove bap-veri -y
 git clone https://github.com/BinaryAnalysisPlatform/bap-veri.git
-opam pin add bap-veri bap-veri/ -n
-opam install bap-veri -y
-
+opam pin add bap-veri bap-veri/ -y
 OPAM_SWITCH=`opam config var switch`
 mkdir -p $HOME/save_opam
 mkdir -p $HOME/save_opam/lib
 cp -r $HOME/.opam/$OPAM_SWITCH/lib/bap* $HOME/save_opam/lib/
 cp -r $HOME/.opam/$OPAM_SWITCH/bin/ $HOME/save_opam/
 cp -r $HOME/.opam/$OPAM_SWITCH/share $HOME/save_opam/
+opam remove bap-veri -y
+rm -r $HOME/save_opam/lib/bap-veri
 '
 fi
 
