675f94ae874d397dd61d629dfc0717a0272f9b84
Oleg <forown@yandex.ru>
2018-04-03 10:28:51 -0400

    just fix doc

--- lib/bap/bap_inject_externals.mli
+++ lib/bap/bap_inject_externals.mli
@@ -3,7 +3,7 @@ open Bap_image_std
 open Bap_sema.Std
 open Bap_disasm_std
 
-(** [run prg insn spec] - looks for relocations in [spec] and
-    add synthetic subroutines for each external function. Also
+(** [run prg insn spec] performs search for relocations in [spec] and
+    add inject synthetic subroutines for each external function. Also
     fixups calls to externals in [prg] *)
 val run : program term -> (mem * insn) seq -> Ogre.doc -> program term
