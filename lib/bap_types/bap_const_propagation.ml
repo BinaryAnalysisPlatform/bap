937058c65db81d4b6f6de90ff27bf601e098114f
Oleg <forown@yandex.ru>
2018-04-27 09:40:45 -0400

    refactored a little

--- lib/bap_types/bap_const_propagation.ml
+++ lib/bap_types/bap_const_propagation.ml
@@ -8,35 +8,6 @@ module Var = Bap_var
 module Word = Bitvector
 module Exp_helpers = Bap_helpers.Exp
 
-let is_assigned = Bap_helpers.is_assigned
-
-(** [remove_forward_defs bil defs]
-    for every [x := exp] in [bil], where [exp] depends on some
-    [y1 .. yN] redefined later then [x],
-    removes both [x] and [y1 .. yN] from defs *)
-let remove_forward_defs bil defs =
-  (object
-    inherit [exp Var.Map.t] bil_visitor
-    method! enter_move var e defs =
-      let redefined = Set.filter (Exp_helpers.free_vars e)
-          ~f:(fun v -> is_assigned v succs) in
-      if Set.is_empty redefined then defs
-      else
-        Set.fold ~init:(Map.remove defs var) redefined
-          ~f:(fun defs v ->
-              if is_assigned v succs then Map.remove defs v
-              else defs)
-  end)#run bil defs
-
-let substitute defs e =
-  let mapper defs = object
-    inherit bil_mapper as super
-    method! map_var v = match Map.find defs v with
-      | Some e -> e
-      | None -> Var v
-  end in
-  Exp_helpers.fold_consts @@ (mapper defs)#map_exp e
-
 let inline bil =
   (object
     inherit bil_mapper
@@ -54,40 +25,74 @@ let inline bil =
 
   end)#run bil
 
+let is_assigned = Bap_helpers.is_assigned
+
+(** [remove_forward_defs bil defs]
+    for every [x := exp] in [bil], where [exp] depends on some
+    [y1 .. yN] redefined later then [x],
+    removes both [x] and [y1 .. yN] from defs *)
+let remove_forward_defs bil defs =
+  (object
+    inherit [word Var.Map.t] bil_visitor
+    method! enter_move var e defs =
+      let redefined = Set.filter (Exp_helpers.free_vars e)
+          ~f:(fun v -> is_assigned v succs) in
+      if Set.is_empty redefined then defs
+      else
+        Set.fold ~init:(Map.remove defs var) redefined
+          ~f:(fun defs v ->
+              if is_assigned v succs then Map.remove defs v
+              else defs)
+  end)#run bil defs
+
+let substitute defs e =
+  let free = Exp_helpers.free_vars e in
+  let mapper defs = object
+    inherit bil_mapper as super
+    method! map_var v =
+      if Set.mem free v then
+        match Map.find defs v with
+        | Some x -> Int x
+        | None -> Var v
+      else Var v
+  end in
+  Exp_helpers.fold_consts @@ (mapper defs)#map_exp e
+
 let propagate_consts bil =
-  let remove_unequal defs defs' =
-    let diff = Map.symmetric_diff defs defs' ~data_equal:Bap_exp.equal in
-    Sequence.fold diff ~init:defs ~f:(fun defs (v,diff) -> match diff with
-        | `Unequal _ -> Map.remove defs v
-        | _ -> defs) in
-  let rec run ss defs = function
-    | [] -> List.rev ss, defs
+  let remove_unequal env env' =
+    Map.merge env env' ~f:(fun ~key:var -> function
+        | `Left w -> Some w
+        | `Both (w1, w2) when Word.equal w1 w2 -> Some w1
+        | `Both (w1, w2) when not (Word.equal w1 w2) -> None
+        | _ -> None) in
+  let rec run ss env = function
+    | [] -> List.rev ss, env
     | Move (v,e) :: bil ->
-      let defs, e = match substitute defs e with
-        | Int _ as e -> Map.add defs v e, e
-        | e -> Map.remove defs v, e in
-      run (Move (v,e) :: ss) defs bil
+      let env, e = match substitute env e with
+        | Int w as e -> Map.add env v w, e
+        | e -> Map.remove env v, e in
+      run (Move (v,e) :: ss) env bil
     | If (cond,yes,no) :: bil ->
-      let yes,defs_yes = run [] defs yes in
-      let no, defs_no  = run [] defs no  in
-      let cond = substitute defs cond in
-      let defs' = match cond with
-        | Int w when Word.is_one  w -> defs_yes
-        | Int w when Word.is_zero w -> defs_no
+      let yes,env_yes = run [] env yes in
+      let no, env_no  = run [] env no  in
+      let cond = substitute env cond in
+      let env' = match cond with
+        | Int w when Word.is_one  w -> env_yes
+        | Int w when Word.is_zero w -> env_no
         | _ ->
-          let defs = remove_unequal defs defs_yes in
-          remove_unequal defs defs_no in
-      run (If (cond,yes,no) :: ss) defs' bil
+          let env = remove_unequal env env_yes in
+          remove_unequal env env_no in
+      run (If (cond,yes,no) :: ss) env' bil
     | While (cond,body) :: bil ->
-      let defs = remove_forward_defs body defs in
-      let body, defs' = run [] defs body in
-      let cond = substitute defs cond in
-      let defs' = match cond with
-        | Int w when Word.is_one w -> defs'
-        | _ -> remove_unequal defs defs' in
-      run (While (cond,body) :: ss) defs' bil
+      let env = remove_forward_defs body env in
+      let body, env' = run [] env body in
+      let cond = substitute env cond in
+      let env' = match cond with
+        | Int w when Word.is_one w -> env'
+        | _ -> remove_unequal env env' in
+      run (While (cond,body) :: ss) env' bil
     | Jmp dst :: bil ->
-      let dst = substitute defs dst in
-      run (Jmp dst :: ss) defs bil
-    | s :: bil -> run (s :: ss) defs bil in
+      let dst = substitute env dst in
+      run (Jmp dst :: ss) env bil
+    | s :: bil -> run (s :: ss) env bil in
   inline @@ fst @@ run [] Var.Map.empty bil
