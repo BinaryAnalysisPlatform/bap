100fd4daa514b9f1568c8c89b0b75a081728f413
Oleg <forown@yandex.ru>
2018-05-24 13:07:29 -0400

    refactored bil-pipe

--- plugins/bil_pipe/bil_pipe_main.ml
+++ plugins/bil_pipe/bil_pipe_main.ml
@@ -2,7 +2,7 @@ open Core_kernel
 open Bap.Std
 include Self ()
 
-type switch = Enable | Disable [@@deriving sexp]
+type switch = Enable | Disable
 
 let parse = function
   | "enable" -> `Ok Enable
@@ -11,27 +11,27 @@ let parse = function
     `Error (sprintf "unknown value %s, possible are: disable | enable" s)
 
 let print fmt s =
-  Format.fprintf fmt "%s" @@  Sexp.to_string (sexp_of_switch s)
+  Format.fprintf fmt "%s" @@
+  match s with
+  | Enable  -> "enable"
+  | Disable -> "disable"
 
-let switcher = Config.converter parse print Enable
+let switcher =
+  Config.(param (converter parse print Enable) ~default:Enable)
 
 let switch f s = Option.some_if (s = Enable) f
-let norm = switch (Stmt.normalize ~normalize_exp:false)
-let simpl = switch Bil.fold_consts
-let subst = switch Bil.reduce_consts
 
-let add norm simpl reduce =
-  let norm = switch (Stmt.normalize ~normalize_exp:false) norm in
+let add norml simpl reduce =
+  let norml = switch (Stmt.normalize ~normalize_exp:false) norml in
   let simpl = switch Bil.fold_consts simpl in
-  let reduce = if Option.is_none norm then None
+  let subst = if Option.is_none norml then None
     else switch Bil.reduce_consts reduce in
   let apply bil = function
     | None -> bil
     | Some f -> f bil in
   let (>>=) = apply in
-  let f bil =
-    Ok (apply bil norm >>= simpl >>= reduce) in
-  register_bass "internal" f
+  register_bass "internal" @@
+  fun bil -> Ok (apply bil norml >>= simpl >>= subst)
 
 let () =
   let () = Config.manpage [
@@ -47,13 +47,9 @@ Constant Substitution
       `P "$(b,bap.mli)"
     ] in
   let norm =
-    let doc = "Produces a normalized BIL program" in
-    Config.(param switcher ~default:Enable ~doc "norm") in
+    switcher "norm" ~doc:"Produces a normalized BIL program" in
   let simpl =
-    let doc = "Applies expressions simplification." in
-    Config.(param switcher ~default:Enable ~doc "simpl") in
+    switcher "simpl" ~doc:"Applies expressions simplification." in
   let subst =
-    let doc = "Substitutes constant expressions." in
-    Config.(param switcher ~default:Enable ~doc "subst") in
-  Config.when_ready (fun {Config.get=(!)} ->
-      add !norm !simpl !subst)
+    switcher "subst" ~doc:"Substitutes constant expressions." in
+  Config.when_ready (fun {Config.get=(!)} -> add !norm !simpl !subst)
