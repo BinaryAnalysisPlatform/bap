e24a0d54acdbe82e618973388171b0a6c4c88306
Ivan Gotovchits <ivg@ieee.org>
2018-06-01 15:00:15 -0400

    fixes a bug in the primus's warn-unused analysis
    also extends it a bit.

--- plugins/primus_test/lisp/warn-unused.lisp
+++ plugins/primus_test/lisp/warn-unused.lisp
@@ -1,15 +1,35 @@
 (require check-value)
 
 (defmethod call-return (name _ ret)
-  (when (= name
-           'malloc
-           'getenv
-           'mkdtemp
-           'mkstemp
-           'posix_openpt)
+  (when (is-in name
+           'malloc 'getenv 'mkdtemp 'mkstemp
+           'posix_openpt 'system 'gets 'fgets 'feof
+           'ferror 'fileno 'chdir 'pipe 'setuid)
+    (check-value ret)))
+
+(defmethod call-return (name _ _ ret)
+  (when (is-in name 'calloc 'realoc 'fdopen 'fopen
+               'realpath 'listen 'shutdown 'access 'getcwd
+               'symlink 'truncate 'popen)
+    (check-value ret)))
+
+(defmethod call-return (name _ _ _ ret)
+  (when (is-in name
+               'freopen 'strxfrm 'accept 'bind 'connect
+               'recvmsg 'sendmsg 'socket 'chown 'readv
+               'readlink 'write)
+    (check-value ret)))
+
+(defmethod call-return (name _ _ _ _ ret)
+  (when (is-in name 'fread 'fwrite 'recv 'send 'socketpair
+               'pread 'pwrite 'read)
+    (check-value ret)))
+
+(defmethod call-return (name _ _ _ _ _ _ _ ret)
+  (when (is-in name 'recvfrom 'sendto 'setsockopt)
     (check-value ret)))
 
 
-(defmethod call-return (name _ _ ret)
-  (when (= name 'realpath)
+(defmethod call-return (name _ _ _ _ _ _ ret)
+  (when (is-in name 'recvfrom 'sendto)
     (check-value ret)))
