530091884022510a28cca17a3af42151b34d9313
Oleg <forown@yandex.ru>
2018-08-15 09:17:57 -0400

    removed unused

--- lib/bap/bap_inject_externals.mli
+++
@@ -1,9 +0,0 @@
-open Bap_types.Std
-open Bap_image_std
-open Bap_sema.Std
-open Bap_disasm_std
-
-(** [run prg insn spec] performs search for relocations in [spec] and
-    add inject synthetic subroutines for each external function. Also
-    fixups calls to externals in [prg] *)
-val run : program term -> (mem * insn) seq -> Ogre.doc -> program term
