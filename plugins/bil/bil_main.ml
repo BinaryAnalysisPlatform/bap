2f9e1f32673f454e112337156c1eaf85bab20429
Oleg <forown@yandex.ru>
2018-08-31 18:38:24 -0400

    added memo

--- plugins/bil/bil_main.ml
+++ plugins/bil/bil_main.ml
@@ -3,23 +3,56 @@ open Bap.Std
 open Bil_const_propagation
 include Self ()
 
+module Memo = struct
+  let max = 500
+  let bils = String.Table.create ()
+  let actual = Int.Table.create ()
+
+  let min = ref 0
+  let pnt = ref 0
+
+  let digest_of_bil b = Digest.string (Bil.to_string b)
+
+  let clean () =
+    if !pnt > max then
+      let d = Hashtbl.find_exn actual !min in
+      Hashtbl.remove actual !min;
+      Hashtbl.remove bils d;
+      incr min
+
+  (* todo: add in actual *)
+  let add digest bil =
+    Hashtbl.set bils digest bil;
+    Hashtbl.set actual !pnt digest;
+    incr pnt;
+    clean ()
+
+  let find = Hashtbl.find bils
+end
+
 
 let simpl = Bil.fold_consts
 let norml = Stmt.normalize ~keep_ites:false ~normalize_exp:false
-let propg = propagate_consts
+let propg = opropagate_consts
+
+let apply bil =
+  List.fold ~init:bil ~f:(fun bil f -> f bil)
 
 let process pipe =
   let pipe = List.filter_map pipe
       ~f:(fun (test, f) -> Option.some_if test f) in
   fun bil ->
-    let rec run bil = function
-      | [] -> bil
-      | f :: fs ->
-        run (f bil) fs in
-    run bil pipe
+    let digest = Memo.digest_of_bil bil in
+    match Memo.find digest with
+    | Some bil -> bil
+    | None ->
+      let bil' = apply bil pipe in
+      if Bil.compare bil bil' <> 0 then
+        Memo.add digest bil';
+      bil'
 
-let run if_norml if_simpl if_propg =
-  register_bass "internal" @@
+let run if_norml if_simpl =
+  provide_bil_transformation "internal" @@
   process [
     if_simpl,simpl;
     if_norml,norml;
