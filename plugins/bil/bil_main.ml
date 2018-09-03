eaed9d8610cf5c420e250ca93c16a07b0bf04680
Oleg <forown@yandex.ru>
2018-09-03 15:53:26 -0400

    refactored a little

--- plugins/bil/bil_main.ml
+++ plugins/bil/bil_main.ml
@@ -1,38 +1,24 @@
 open Core_kernel
 open Bap.Std
+open Regular.Std
 
 include Self ()
 
-module Memo = struct
-  let max = 500
-  let bils = String.Table.create ()
-  let actual = Int.Table.create ()
-
-  let min = ref 0
-  let pnt = ref 0
-
-  let digest_of_bil b = Digest.string (Bil.to_string b)
-
-  let clean () =
-    if !pnt > max then
-      let d = Hashtbl.find_exn actual !min in
-      Hashtbl.remove actual !min;
-      Hashtbl.remove bils d;
-      incr min
-
-  let add digest bil =
-    Hashtbl.set bils digest bil;
-    Hashtbl.set actual !pnt digest;
-    incr pnt;
-    clean ()
-
-  let find = Hashtbl.find bils
-end
-
+(* variable is considred dead if it's a virtual and equal to const and
+   assigned only once. Assuming const propagation was ran before *)
 let eliminate_dead_code bil =
   let open Bil.Types in
-  let free = Bil.free_vars bil in
-  let is_dead v = Var.is_virtual v && not (Set.mem free v) in
+  let assigned_once =
+    fst @@
+    ((object
+      inherit [Var.Set.t * Var.Set.t] Stmt.visitor
+      method! enter_move var _ (once, many) =
+        if Set.mem once var then
+          Set.remove once var, Set.add many var
+        else if Set.mem many var then once,many
+        else Set.add once var, many
+    end)#run bil (Var.Set.empty, Var.Set.empty)) in
+  let is_dead v = Var.is_virtual v && Set.mem assigned_once v in
   let rec loop acc = function
     | [] -> List.rev acc
     | st :: bil ->
@@ -51,17 +37,6 @@ let eliminate_dead_code bil =
 let apply bil =
   List.fold ~init:bil ~f:(fun bil f -> f bil)
 
-let process pipe =
-  fun bil ->
-    let digest = Memo.digest_of_bil bil in
-    match Memo.find digest with
-    | Some bil -> bil
-    | None ->
-      let bil' = apply bil pipe in
-      if Bil.compare bil bil' <> 0 then
-        Memo.add digest bil';
-      bil'
-
 let norml level =
   if level = 0 then ident
   else
@@ -75,7 +50,7 @@ let simpl level =
 
 let run norml_level simpl_level =
   provide_bil_transformation "internal" @@
-  process [
+  fun bil -> apply bil [
     norml norml_level;
     simpl simpl_level;
   ]
