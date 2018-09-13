63d61b9cb8cf41c5899191739ca922291b8a99e4
Ivan Gotovchits <ivg@ieee.org>
2018-09-13 11:43:02 -0400

    provides information sensitivity analysis (#871)
    The analysis is based on the Primus Taint Engine and is fully
    implemented in Primus Lisp and consists of two modules:
    
    - taint-sources: is a module that automatically detects untrusted
    user inputs and correspondingly introduces taint into the system;
    
    - sensitive-sinks: monitors functions that are considered sensitive
    to untrusted/unsanitized user inputs, and reports policy violations
    if such functions are called with tainted arguments.
    
    It is possible to specify arbitrary sanitization procedures (and in
    general extend the policy with your own rules).
    
    The code is thoroughly documented, so it should be more or less
    obvious on how to use it.

---
+++ plugins/primus_taint/lisp/sensitive-sinks.lisp
@@ -0,0 +1,80 @@
+;; defines arguments to certain functions as sensitive and establishes
+;; a policy that they should either be checked (must/check) if they
+;; are tainted with the untrusted input, or never be tainted at all
+;; (must/trust)
+
+
+;; mark all taints that affected branch control, this gives at least
+;; a hope that the untrusted input was checked and validated.
+(defmethod eval-cond (c)
+  (let ((t (taint-get-direct 'untrusted c)))
+    (when t
+      (dict-add 'untrusted/checked t (incident-location)))))
+
+
+(defun must/check (v)
+  (let ((t (taint-get-direct 'untrusted v))
+        (c (dict-get 'untrusted/checked t)))
+    (when (and t (not c))
+      (incident-report 'unchecked-untrusted-argument
+                       (incident-location)
+                       (dict-get 'taint-sources/untrusted t)))))
+
+(defun must/trust (v)
+  (let ((t (taint-get-direct 'untrusted v)))
+    (when t
+      (incident-report 'untrusted-argument
+                       (incident-location)
+                       (dict-get 'taint-sources/untrusted t)))))
+
+(defun must/trust-string (s)
+  (while (not (points-to-null s))
+    (must/trust (memory-read s))
+    (incr s)))
+
+(defmethod call (name m n)
+  (when (= name 'calloc)
+    (must/check m)
+    (must/check n)))
+
+(defmethod call (name n)
+  (when (is-in name 'malloc)
+    (must/check n)))
+
+(defmethod call (name _ n)
+  (when (is-in name 'div 'ldiv 'lldiv)
+    (must/check n)))
+
+(defmethod call (name ptr aln len)
+  (when (= name 'posix_memalign)
+    (must/check len)))
+
+(defmethod call (name ptr len)
+  (when (= name 'realloc)
+    (must/check len)))
+
+(defmethod call (name x y len)
+  (when (is-in name
+               'memchr 'memcmp 'memcpy 'memmove 'memset
+               'strncasecmp 'strpcpy)
+    (must/check len)))
+
+(defmethod call (name cmd)
+  (when (is-in name 'system 'popen)
+    (must/trust-string cmd)))
+
+
+(defmethod call (name path arg)
+  (when (is-in name 'execl 'execle 'execlp 'execv 'execve 'execvp)
+    (must/trust-string path)
+    (must/trust-string arg)))
+
+(defun must/trust-args (args)
+  (while args
+    (must/trust-string (read_word ptr_t args))
+    (ptr+1 ptr_t args)))
+
+(defmethod call (name pid path fa attr args envp)
+  (must/trust-string path)
+  (must/trust-args args)
+  (must/trust-envp envp))
