55aed0636ce8261f9573062791432511159c9d33
Oleg <forown@yandex.ru>
2018-11-06 09:26:28 -0500

    refactoring

--- plugins/ida/bap_ida_info.ml
+++ plugins/ida/bap_ida_info.ml
@@ -2,10 +2,10 @@ open Core_kernel
 open Bap.Std
 include Self()
 
-type version = Vold | V7 [@@deriving sexp]
+type version = Vold | Vnew [@@deriving sexp]
 type old_kind = [ `idal | `idal64 | `idaq | `idaq64 ] [@@deriving sexp, enumerate]
-type v7_kind  = [ `idat | `idat64 | `ida  | `ida64  ] [@@deriving sexp, enumerate]
-type ida_kind = [ old_kind | v7_kind ] [@@deriving sexp]
+type new_kind = [ `idat | `idat64 | `ida  | `ida64  ] [@@deriving sexp, enumerate]
+type ida_kind = [ old_kind | new_kind ] [@@deriving sexp]
 
 type ida = {
     headless  : ida_kind;
@@ -28,7 +28,7 @@ type failure =
   | Non_dir_path
   | File_not_found of string
   | Unexpected_kind
-  | Headless_not_allowed
+  | Headless_win32
   | Ida_python_missed
 
 let (/) = Filename.concat
@@ -36,19 +36,19 @@ let (/) = Filename.concat
 let string_of_kind k = Sexp.to_string (sexp_of_ida_kind k)
 
 let code_of_failure = function
-  | Ida_not_exists -> 0
-  | Non_dir_path -> 1
+  | Ida_not_exists   -> 0
+  | Non_dir_path     -> 1
   | File_not_found _ -> 2
-  | Unexpected_kind -> 3
-  | Headless_not_allowed -> 4
-  | Ida_python_missed -> 5
+  | Unexpected_kind  -> 3
+  | Headless_win32   -> 4
+  | Ida_python_missed-> 5
 
 let string_of_failure = function
   | Ida_not_exists -> "IDA path not exists"
   | Non_dir_path -> "IDA path is not a directory"
-  | File_not_found k -> k ^ " not found "
+  | File_not_found k -> k ^ " must exist"
   | Unexpected_kind -> "can't infer IDA version. Check IDA installation"
-  | Headless_not_allowed -> "can't use headless on windows"
+  | Headless_win32 -> "can't use headless on windows"
   | Ida_python_missed -> "bap-ida-python is not installed"
 
 module Check = struct
@@ -69,18 +69,19 @@ module Check = struct
     require_path ~error:Ida_python_missed
       (path / "plugins" / "plugin_loader_bap.py")
 
+  let require_kind path k =
+    let file = path / string_of_kind k in
+    require_path ~error:(File_not_found file) file
+
   let run {path; ida} =
-    let require_kind k =
-      let file = path / string_of_kind k in
-      require_path ~error:(File_not_found file) file in
+    let require_kind = require_kind path in
     Result.all_ignore [
         require_ida path;
+        require_ida_python path;
         require_kind ida.graphical;
         require_kind ida.graphical64;
         require_kind ida.headless;
-        require_kind ida.headless64;
-        require_ida_python path ]
-
+        require_kind ida.headless64; ]
 
   let check_integrity ida =
     let files = [ida.graphical; ida.headless;
@@ -89,12 +90,12 @@ module Check = struct
     let check version = List.for_all files ~f:(is_of version) in
     let expected = match ida.headless with
       | `idal -> (all_of_old_kind :> ida_kind list)
-      | _ -> (all_of_v7_kind :> ida_kind list) in
+      | _ -> (all_of_new_kind :> ida_kind list) in
     let checked = check expected in
     require checked ~error:Unexpected_kind
 
   let check_headless is_headless =
-    require (is_headless ==> not Sys.win32) ~error:Headless_not_allowed
+    require (is_headless ==> not Sys.win32) ~error:Headless_win32
 
 end
 
@@ -124,7 +125,7 @@ let create_ida path =
          Error (File_not_found files) in
   let version_of_headless = function
     | `idal -> Vold
-    | _ -> V7 in
+    | _ -> Vnew in
   Result.(
     find_kind ~is_headless:true  `m32 >>= fun headless ->
     find_kind ~is_headless:false `m32 >>= fun graphical ->
@@ -137,12 +138,11 @@ let create' path is_headless =
   let open Check in
   Result.(
     require_ida path >>= fun () ->
-    create_ida path >>= fun ida ->
+    require_ida_python path >>= fun () ->
     check_headless is_headless >>= fun () ->
+    create_ida path >>= fun ida ->
     check_integrity ida >>= fun () ->
-    let info = {ida; path; is_headless} in
-    Check.run info >>= fun () ->
-    Ok info)
+    Ok {ida; path; is_headless})
 
 let create path is_headless =
   match create' path is_headless with
@@ -153,13 +153,13 @@ let create path is_headless =
 
 (* Note, we always launch headless ida in case of IDA Pro 7 *)
 let ida32 info = match info.ida.version with
-  | V7 -> info.ida.headless
+  | Vnew -> info.ida.headless
   | Vold ->
      if info.is_headless then info.ida.headless
      else info.ida.graphical
 
 let ida64 info = match info.ida.version with
-  | V7 -> info.ida.headless64
+  | Vnew -> info.ida.headless64
   | Vold ->
      if info.is_headless then info.ida.headless64
      else info.ida.graphical64
