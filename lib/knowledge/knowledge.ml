280efe615978cce42ef3a370e95271b57a8bdfa4
Ivan Gotovchits <ivg@ieee.org>
2018-11-27 14:26:11 -0500

    makes the promises collection a little bit more elaborated
    1. we should be able to handle arbitrary cycles
    2. we should find the maximal data

--- lib/knowledge/knowledge.ml
+++ lib/knowledge/knowledge.ml
@@ -13,14 +13,22 @@ module Conflict = struct
   type t = conflict = ..
 end
 
+module Pid = Int63
+type pid = Pid.t
+
+
 module Base = struct
   type t = {
     data : semantics Label.Map.t;
+    reqs : Label.Set.t Pid.Map.t;
   }
 end
 
 type state = Base.t
-let empty = {Base.data = Label.Map.empty}
+let empty : Base.t = {
+  data = Label.Map.empty;
+  reqs = Pid.Map.empty;
+}
 
 module State = struct
   include Monad.State.T1(Base)(Monad.Ident)
@@ -43,20 +51,53 @@ let update f = Knowledge.lift (State.update f)
 
 
 let provide tag id info =
-  update @@ fun {data} -> {
-    data = Map.update data id ~f:(function
+  update @@ fun s -> {
+    s with
+    data = Map.update s.data id ~f:(function
         | None -> Semantics.put tag Semantics.empty info
         | Some sema -> Semantics.put tag sema info)
   }
 
-let promise tag p =
-  Semantics.promise tag p
+type 'a promise = {
+  get : label -> 'a Knowledge.t;
+  pid : pid;
+}
 
-let collect tag id =
-  (* we can memoize here, but let's it do later *)
-  Knowledge.List.iter (Semantics.promises tag) ~f:(fun p ->
-      p id >>= fun info ->
-      provide tag id info) >>= fun () ->
+let promises = ref Pid.zero
+let promise tag get =
+  Pid.incr promises;
+  let pid = !promises in
+  Semantics.promise tag {get; pid}
+
+let is_active reqs pid id = match Map.find reqs pid with
+  | None -> false
+  | Some ids -> Set.mem ids id
+
+let activate reqs pid id = Map.update reqs pid ~f:(function
+    | None -> Label.Set.singleton id
+    | Some ids -> Set.add ids id)
+
+let deactivate req pid id = Map.change req pid ~f:(function
+    | None -> None
+    | Some ids ->
+      let ids = Set.remove ids id in
+      if Set.is_empty ids then None else Some ids)
+
+let collect (type t) tag id =
+  let module Domain =
+    (val Semantics.domain tag : Domain.S with type t = t) in
+  Semantics.promises tag |>
+  Knowledge.List.fold ~init:Domain.empty ~f:(fun curr req ->
+      get () >>= fun s ->
+      if is_active s.reqs req.pid id
+      then Knowledge.return curr
+      else put {s with reqs = activate s.reqs req.pid id} >>= fun () ->
+        req.get id >>= fun next ->
+        put {s with reqs = deactivate s.reqs req.pid id} >>= fun () ->
+        match Domain.partial curr next with
+        | GE -> Knowledge.return curr
+        | LE | EQ | NC -> Knowledge.return next) >>= fun max ->
+  provide tag id max >>= fun () ->
   gets @@ fun {data} ->
   let sema = Map.find data id |> function
     | None -> Semantics.empty
@@ -65,7 +106,7 @@ let collect tag id =
 
 include Knowledge
 type 'a knowledge = 'a t
-type 'a data = ('a,Label.t -> 'a t) Semantics.data
+type 'a data = ('a,'a promise) Semantics.data
 
 let declare = Semantics.declare
 
