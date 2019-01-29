65acae60cf06800039bbba96a97862ea423dfddb
Oleg <forown@yandex.ru>
2019-01-29 15:16:57 -0500

    wrote docs

--- lib/bap_elementary/bap_elementary.ml
+++ lib/bap_elementary/bap_elementary.ml
@@ -5,7 +5,8 @@ open Bap_knowledge
 open Bap_core_theory
 open Knowledge.Syntax
 
-module Elementary = struct
+module Elementary (Theory : Theory.Core) = struct
+  open Theory
 
   type 'a t = 'a knowledge
 
@@ -16,111 +17,104 @@ module Elementary = struct
   let name op sort rank =
     String.concat ~sep:"/" [op; Sort.name sort; string_of_int rank]
 
-  let scheme tab =
-    match String.split ~on:'/' (Var.name tab) with
+  let scheme ident =
+    match String.split ~on:'/' (Var.Ident.to_string ident) with
     | [name; sort; rank] -> Some (name, sort, rank)
     | _ -> None
 
-  let operation tab = match scheme tab with
+  let operation ident = match scheme ident with
     | Some (name,_,_) -> name
     | None -> raise Not_a_table
 
   let table = name
-  let is_table tab = Option.is_some (scheme tab)
+  let is_table ident = Option.is_some (scheme ident)
 
-  module Make(Theory : Theory.Core) = struct
-    open Theory
+  let bind a body =
+    a >>= fun a ->
+    let sort = Value.sort a in
+    Var.scoped sort @@ fun v ->
+    let_ v !!a (body v)
 
-    let bind a body =
-      a >>= fun a ->
-      let sort = Value.sort a in
-      Var.scoped sort @@ fun v ->
-      let_ v !!a (body v)
+  let (>>>=) = bind
 
-    let (>>>=) = bind
+  let (>>->) x f =
+    x >>= fun x ->
+    f (Value.sort x) x
 
-    let (>>->) x f =
-      x >>= fun x ->
-      f (Value.sort x) x
+  let approximate ~rank ~reduce ~extract ~coefs x rmode =
+    x >>-> fun fsort x ->
+    reduce !!x >>>= fun key ->
+    load (var coefs) (var key) >>>= fun value ->
+    let coef i = float fsort (extract i (var value)) in
+    let rec sum i y =
+      if i >= 0 then
+        fmul rmode !!x y >>>= fun y ->
+        coef i >>>= fun c ->
+        fadd rmode (var y) (var c) >>>= fun y ->
+        sum (i - 1) (var y)
+      else y in
+    coef rank >>>= fun cr ->
+    if rank = 0 then var cr
+    else sum (rank - 1) (var cr)
 
-    let approximate ~rank ~reduce ~extract ~coefs ?(rmode=rne) x =
-      x >>-> fun fsort x ->
-      reduce !!x >>>= fun key ->
-      load (var coefs) (var key) >>>= fun value ->
-      let coef i = float fsort (extract i (var value)) in
-      let rec sum i y =
-        if i >= 0 then
-          fmul rmode !!x y >>>= fun y ->
-          coef i >>>= fun c ->
-          fadd rmode (var y) (var c) >>>= fun y ->
-          sum (i - 1) (var y)
-        else y in
-      coef rank >>>= fun cr ->
-      if rank = 0 then var cr
-      else sum (rank - 1) (var cr)
+  let of_int sort x = int sort (Word.of_int ~width:(Bits.size sort) x)
 
-    let of_int sort x = int sort (Word.of_int ~width:(Bits.size sort) x)
+  let nth fsort n bitv =
+    bitv >>-> fun sort bitv ->
+    let index = of_int sort (n * Bits.size (bits fsort)) in
+    lshift !!bitv index >>>= fun bitv ->
+    high (bits fsort) (var bitv)
 
-    let nth fsort n bitv =
-      bitv >>-> fun sort bitv ->
-      let index = of_int sort (n * Bits.size (bits fsort)) in
-      lshift !!bitv index >>>= fun bitv ->
-      high (bits fsort) (var bitv)
-
-    let tabulate op ~rank ~interval x =
-      x >>-> fun fsort x ->
-      let keys = Bits.define interval in
-      let values = Bits.define ((rank + 1) * Bits.size (bits fsort)) in
-      let mems = Mems.define keys values in
-      let name = name op fsort rank in
-      let coefs = Var.define mems name in
-      let reduce x = high keys (fbits x) in
-      let extract = nth fsort in
-      approximate ~rank ~coefs ~reduce ~extract !!x
-
-  end
-
-  module Base = Make(Theory.Manager)
-
-  let approximate = Base.approximate
-  let tabulate = Base.tabulate
+  let tabulate op ~rank ~size x rmode =
+    x >>-> fun fsort x ->
+    let keys = Bits.define size in
+    let values = Bits.define ((rank + 1) * Bits.size (bits fsort)) in
+    let mems = Mems.define keys values in
+    let name = name op fsort rank in
+    let coefs = Var.define mems name in
+    let reduce x = high keys (fbits x) in
+    let extract = nth fsort in
+    approximate ~rank ~coefs ~reduce ~extract !!x rmode
 
   module Scheme = struct
-    let pow = name "pow"
-    let powr = name "powr"
-    let compound = name "compound"
-    let rootn = name "rootn"
-    let pownn = name "pownn"
-    let rsqrt = name "rsqrt"
-    let hypot = name "hypot"
-    let exp  = name "exp"
-    let expm1 = name "expm1"
-    let exp2 = name "exp2"
-    let exp2m1 = name "exp2m1"
-    let exp10 = name "exp10"
-    let exp10m1 = name "exp10m1"
-    let log = name "log"
-    let log2 = name "log2"
-    let log10 = name "log10"
-    let logp1 = name "logp1"
-    let log2p1 = name "log2p1"
-    let log10p1 = name "log10p1"
-    let sin = name "sin"
-    let cos = name "cos"
-    let tan = name "tan"
-    let sinpi = name "sinpi"
-    let cospi = name "cospi"
-    let atanpi = name "atanpi"
-    let atan2pi = name "atan2pi"
-    let asin = name "asin"
-    let acos = name "acos"
-    let atan = name "atan"
-    let atan2 = name "atan2"
-    let sinh = name "sinh"
-    let cosh = name "cosh"
-    let tanh = name "tanh"
-    let asinh = name "asinh"
-    let acosh = name "acosh"
-    let atanh = name "atanh"
+    type 'a t = 'a sort -> int -> string
+
+    let pow s = name "pow" s
+    let powr s = name "powr" s
+    let compound  s = name "compound" s
+    let rootn s = name "rootn" s
+    let pownn s = name "pownn" s
+    let rsqrt s = name "rsqrt" s
+    let hypot s = name "hypot" s
+    let exp  s = name "exp" s
+    let expm1 s = name "expm1" s
+    let exp2 s = name "exp2" s
+    let exp2m1 s = name "exp2m1" s
+    let exp10 s = name "exp10" s
+    let exp10m1 s = name "exp10m1" s
+    let log s = name "log" s
+    let log2 s = name "log2" s
+    let log10 s = name "log10" s
+    let logp1 s = name "logp1" s
+    let log2p1 s = name "log2p1" s
+    let log10p1 s = name "log10p1" s
+    let sin s = name "sin" s
+    let cos s = name "cos" s
+    let tan s = name "tan" s
+    let sinpi s = name "sinpi" s
+    let cospi s = name "cospi" s
+    let atanpi s = name "atanpi" s
+    let atan2pi s = name "atan2pi" s
+    let asin s = name "asin" s
+    let acos s = name "acos" s
+    let atan s = name "atan" s
+    let atan2 s = name "atan2" s
+    let sinh s = name "sinh" s
+    let cosh s = name "cosh" s
+    let tanh s = name "tanh" s
+    let asinh s = name "asinh" s
+    let acosh s = name "acosh" s
+    let atanh s = name "atanh" s
   end
+
 end
--- lib/bap_elementary/bap_elementary.mli
+++ lib/bap_elementary/bap_elementary.mli
@@ -3,73 +3,102 @@ open Bap.Std
 open Bap_knowledge
 open Bap_core_theory
 
-module Elementary : sig
+(** Elementary is a library that provides few primitives for
+    approximations of floating point operations via table methods.  *)
+module Elementary (Theory : Theory.Core) : sig
 
   type 'a t = 'a knowledge
 
   exception Not_a_table
 
+  (** [approximate ~rank ~reduce ~extract ~coefs x rmode]
+      returns a function f(x,rmode) that is defined by a polynomial
+      of rank [rank], which coefficients are stored in a table [coefs].
+
+      @param coefs is a table, where keys are some points in integer
+      space, in which floating point values could be mapped by
+      [reduce] function. And values of [coefs] are some point in
+      integer namespace from which floating point coefficients
+      could be restored by [extract] function with a respect to a
+      rank of each coefficient. *)
   val approximate :
     rank : int ->
     reduce : (('a,'s) format float value t -> 'r bitv value t) ->
     extract : (int -> 'd bitv value t -> 's bitv value t) ->
     coefs : ('r, 'd) mem var ->
-    rmode : rmode value t ->
     ('a, 's) format float value t ->
+    rmode value t ->
     ('a, 's) format float value t
 
+  (** [tabulate op ~rank ~size x rmode] defines a subset of
+      functions that can be created by [approximate], s.t.
+      each value in a table is a concatenation of coefficients
+      (from one with the most rank to one with the least),
+      and each key is first [size] bits of floating point value.
+
+      @param op is a name of floating point operation to approximate. *)
   val tabulate :
     string ->
     rank:int ->
-    interaval:int ->
+    size:int ->
     ('a, 's) format float value t ->
+    rmode value t ->
     ('a, 's) format float value t
 
-  val is_table : ('r, 'd) mem var -> bool
+  (** [table operation sort rank] defines a naming scheme for
+      approximation of [rank] of an [operation] for values of [sort].  *)
+  val table : string -> ('r, 's) format float sort -> int -> string
 
-  val operation : ('r, 'd) mem var -> string
+  (** [is_table ident] returns true if [ident] is a table *)
+  val is_table  : Var.ident -> bool
 
-  val table : string -> 'a sort -> int -> string
+  (** [operation ident] returns the name of a function,
+      which polynomial coefficients reside in a table
+      referenced by [ident].
+      Raise Not_a_table if [ident] doesn't match to a naming
+      scheme *)
+  val operation : Var.ident -> string
 
+  (** module contains naming schemes for different math functions *)
   module Scheme : sig
     type 'a t = 'a sort -> int -> string
 
-    val pow : t
-    val powr : t
-    val compound : t
-    val rootn : t
-    val pownn : t
-    val rsqrt : t
-    val hypot : t
-    val exp  : t
-    val expm1 : t
-    val exp2 : t
-    val exp2m1 : t
-    val exp10 : t
-    val exp10m1 :t
-    val log : t
-    val log2 : t
-    val log10 : t
-    val logp1 : t
-    val log2p1 : t
-    val log10p1 : t
-    val sin : t
-    val cos : t
-    val tan : t
-    val sinpi : t
-    val cospi : t
-    val atanpi : t
-    val atan2pi : t
-    val asin : t
-    val acos : t
-    val atan : t
-    val atan2 : t
-    val sinh : t
-    val cosh : t
-    val tanh : t
-    val asinh : t
-    val acosh : t
-    val atanh : t
-
+    val pow : 'a t
+    val powr : 'a t
+    val compound : 'a t
+    val rootn : 'a t
+    val pownn : 'a t
+    val rsqrt : 'a t
+    val hypot : 'a t
+    val exp  : 'a t
+    val expm1 : 'a t
+    val exp2 : 'a t
+    val exp2m1 : 'a t
+    val exp10 : 'a t
+    val exp10m1 : 'a t
+    val log : 'a t
+    val log2 : 'a t
+    val log10 : 'a t
+    val logp1 : 'a t
+    val log2p1 : 'a t
+    val log10p1 : 'a t
+    val sin : 'a t
+    val cos : 'a t
+    val tan : 'a t
+    val sinpi : 'a t
+    val cospi : 'a t
+    val atanpi : 'a t
+    val atan2pi : 'a t
+    val asin : 'a t
+    val acos : 'a t
+    val atan : 'a t
+    val atan2 : 'a t
+    val sinh : 'a t
+    val cosh : 'a t
+    val tanh : 'a t
+    val asinh : 'a t
+    val acosh : 'a t
+    val atanh : 'a t
   end
+
 end
