4d52b23924c0e38dd3ccc47bfd269216206c33d8
gitoleg <forown@yandex.ru>
2019-01-30 15:25:56 -0500

    fixed building on travis (#915)

--- .travis_install.sh
+++ .travis_install.sh
@@ -8,13 +8,14 @@ rm -rf bap-veri
 git clone https://github.com/BinaryAnalysisPlatform/bap-veri.git
 opam pin add bap-veri bap-veri/ -y
 OPAM_SWITCH=`opam config var switch`
-mkdir -p $HOME/save_opam
-mkdir -p $HOME/save_opam/lib
-cp -r $HOME/.opam/$OPAM_SWITCH/bin/ $HOME/save_opam/
-cp -r $HOME/.opam/$OPAM_SWITCH/share $HOME/save_opam/
+mkdir -p $HOME/save_opam/$OPAM_SWITCH/lib/bap
+cp -r $HOME/.opam/$OPAM_SWITCH/bin/ $HOME/save_opam/$OPAM_SWITCH/
+cp -r $HOME/.opam/$OPAM_SWITCH/share $HOME/save_opam/$OPAM_SWITCH/
+cp -r $HOME/.opam/$OPAM_SWITCH/lib/bap/*.plugin $HOME/save_opam/$OPAM_SWITCH/lib/bap
+
 opam remove bap-veri -y
-cp -r $HOME/.opam/$OPAM_SWITCH/lib/bap* $HOME/save_opam/lib/
 '
+
 fi
 
 bash -ex .travis-opam.sh
--- plugins/x86/x86_mov_offset.ml
+++ plugins/x86/x86_mov_offset.ml
@@ -194,6 +194,7 @@ module T = Make(Ver_common)
 module Self = Self ()
 
 let () =
+  let llvm_version = String.sub llvm_version 0 3 in
   if llvm_version = "3.4" then T_34.register ()
   else
   if List.mem ["3.8";"4.0";"5.0";"6.0";"7.0"] llvm_version ~equal:String.equal
