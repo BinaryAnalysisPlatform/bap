c2e8de5a5287ba8b275634fe59f590ea66ba5691
turnersr <steven.rafael.turner@gmail.com>
2019-02-19 12:42:51 -0800

    reverse coefficients order

--- plugins/primus_approximation/lisp/test.lisp
+++ plugins/primus_approximation/lisp/test.lisp
@@ -1,4 +1,8 @@
 (require math)
 
 (defmethod init ()
-  (msg "hello from math $0" (sin 4611686018427387904:64)))
+  (msg "hello from math $0" (sin 4611686018427387904:64))
+  (msg "hello from math - sin(316.159265358979326) = $0" (sin 4644269548807471931:64))
+  (msg "hello from math sin(7853981635.97448254) = $0" (sin 4755029503896426363:64)))
+
+
