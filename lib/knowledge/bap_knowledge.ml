ab1a73924439687808f5030e9a61c72d12050f2a
Ivan Gotovchits <ivg@ieee.org>
2019-02-22 15:38:51 -0500

    nearly finished the implementation
    only serialization is left

--- lib/knowledge/bap_knowledge.ml
+++ lib/knowledge/bap_knowledge.ml
@@ -1,34 +1,136 @@
 open Core_kernel
 open Monads.Std
 
-module Domain = Bap_knowledge_domain
-module L = Bap_knowledge_label
-module Semantics = Bap_knowledge_semantics
-
-type semantics = Semantics.t [@@deriving bin_io, compare, sexp]
-type label = L.t
-type conflict = ..
-
-module Conflict = struct
-  type t = conflict = ..
+module Order = struct
+  type partial = LT | EQ | GT | NC
 end
 
-module Pid = Int63
-type pid = Pid.t
+module Id = Int63
+
+module Domain = struct
+  type 'a t = {
+    serialize : (module Binable.S with type t = 'a) option;
+    inspect : 'a -> Sexp.t;
+    empty : 'a;
+    order : 'a -> 'a -> Order.partial;
+    name : string;
+  }
+
+  let empty_serializer (type a) empty =
+    let module S = struct
+      type t = a
+      include Binable.Of_binable(Unit)(struct
+          type t = a
+          let to_binable _ = ()
+          let of_binable () = empty
+        end)
+    end  in
+    (module S : Binable.S with type t = a)
+
+end
+
+type 'a obj = Id.t
+type 'a ord = Id.comparator_witness
+
+module Class = struct
+  type +'a t = {
+    id : Id.t;
+    name : string;
+  }
+
+
+  type info = {
+    desc : string option;
+    slots : (string * string option) Hashtbl.M(String).t;
+  }
+
+  let classes = ref Int63.zero
+
+  let info desc = {
+    desc;
+    slots = Hashtbl.create (module String);
+  }
+
+  let public_classes = Hashtbl.create (module String)
+
+  let register_class ?desc name =
+    match Hashtbl.add public_classes ~key:name ~data:(info desc) with
+    | `Ok -> ()
+    | `Duplicate -> invalid_argf "Failed to register public class %s\
+                                  - the name is not unique" name ()
+
+  let register_slot ?desc cls name =
+    let {slots} = Hashtbl.find_exn public_classes cls.name in
+    match Hashtbl.add slots ~key:name ~data:(name,desc) with
+    | `Ok -> ()
+    | `Duplicate ->
+      invalid_argf "Failed to register slot %s of class %s - \
+                    the slot name is not unique, but made public"
+        name cls.name ()
+
+  let newclass name =
+    Int63.incr classes;
+    {id = !classes; name}
+
+  let declare ?(public=false) ?desc name =
+    if public then register_class ?desc name;
+    newclass name
+
+  let derived ?desc name parent =
+    let name = parent.name ^ "/" ^ name in
+    if Hashtbl.mem public_classes parent.name
+    then register_class ?desc name;
+    newclass name
+
+
+  let comparator : type a. a t ->
+    (module Comparator.S
+      with type t = a obj
+       and type comparator_witness = a ord) = fun _ ->
+    let module R = struct
+      type t = a obj
+      type comparator_witness = Id.comparator_witness
+      let comparator = Id.comparator
+    end in
+    (module R)
+end
+
+module Object = struct
+  type +'a t = Id.t
+end
+
+module Record = Univ_map
 
 module Knowledge = struct
+  type 'a value = Record.t
+  type +'a cls = 'a Class.t
+  type +'a obj = 'a Object.t
+  type 'p domain = 'p Domain.t
+
+  module Pid = Int63
+  type pid = Pid.t
+
+  type conflict = ..
+
+  module Conflict = struct
+    type t = conflict = ..
+  end
+
   module Base = struct
-    type t = {
-      data : semantics Map.M(L).t;
-      reqs : Set.M(L).t Map.M(Pid).t;
+    type objects = {
+      data : Record.t Map.M(Id).t;
+      reqs : Set.M(Id).t Map.M(Pid).t
     }
+    let empty_class = {
+      data = Map.empty (module Id);
+      reqs = Map.empty (module Pid);
+    }
+
+    type t = objects Map.M(Id).t
   end
 
   type state = Base.t
-  let empty : Base.t = {
-    data = Map.empty (module L);
-    reqs = Map.empty (module Pid);
-  }
+  let empty : Base.t = Map.empty (module Id)
 
   module State = struct
     include Monad.State.T1(Base)(Monad.Ident)
@@ -40,31 +142,40 @@ module Knowledge = struct
     include Monad.Result.Make(Conflict)(State)
   end
 
+  type 'a knowledge = 'a Knowledge.t
+
   open Knowledge.Syntax
 
-  type 'a promise = {
-    get : label -> 'a Knowledge.t;
-    pid : pid;
-  }
+  module Slot = struct
+    type 'p promise = {
+      get : Id.t -> 'p Knowledge.t;
+      pid : pid;
+    }
 
-  type 'a content = {
-    domain : 'a Semantics.domain;
-    name : string;
-    info : string option;
-    mutable promises : 'a promise list;
-  }
+    type ('a,'p) t = {
+      cls : 'a Class.t;
+      dom : 'p Domain.t;
+      key : 'p Type_equal.Id.t;
+      name : string;
+      desc : string option;
+      mutable promises : 'p promise list;
+    }
 
-  let registry = Hashtbl.create (module String)
+    let declare ?desc cls name (dom : 'a Domain.t) =
+      let key = Type_equal.Id.create ~name dom.inspect in
+      {cls; dom; key; name; desc; promises = []}
+  end
 
-  let register_public_service name desc =
-    if Hashtbl.mem registry name
-    then invalid_argf "Content name is not uniqiue: %S" name ();
-    Hashtbl.set registry ~key:name ~data:desc
+  type ('a,'p) slot = ('a,'p) Slot.t
 
-  let declare ?(public=false) ?desc ~name domain =
-    if public
-    then register_public_service name desc;
-    { domain; name; info=desc; promises = []}
+  module Value = struct
+    type 'a t = 'a value
+    let empty _ = Record.empty
+    let put {Slot.key} v x = Record.set v key x
+    let get {Slot.key; dom} data = match Record.find data key with
+      | None -> dom.empty
+      | Some x -> x
+  end
 
   let get () = Knowledge.lift (State.get ())
   let put s = Knowledge.lift (State.put s)
@@ -72,27 +183,30 @@ module Knowledge = struct
   let update f = Knowledge.lift (State.update f)
 
 
-  let provide tag id info =
-    update @@ fun s -> {
-      s with
-      data = Map.update s.data id ~f:(function
-          | None -> Semantics.put tag.domain Semantics.empty info
-          | Some sema -> Semantics.put tag.domain sema info)
-    }
+  let provide : type a p. (a,p) slot -> a Object.t -> p -> unit Knowledge.t =
+    fun slot obj x ->
+      update @@ fun s ->
+      let {Base.data; reqs} = match Map.find s slot.cls.id with
+        | None -> Base.empty_class
+        | Some objs -> objs in
+      let data = Map.update data obj ~f:(function
+          | None -> Value.(put slot (empty slot.cls) x)
+          | Some v -> Value.put slot v x) in
+      Map.set s ~key:slot.cls.id ~data:{data; reqs}
 
+  let pids = ref Pid.zero
 
-  let promises = ref Pid.zero
-  let promise tag get =
-    Pid.incr promises;
-    let pid = !promises in
-    tag.promises <- {get;pid} :: tag.promises
+  let promise (s : _ slot) get =
+    Pid.incr pids;
+    let pid = !pids in
+    s.promises <- {get;pid} :: s.promises
 
   let is_active reqs pid id = match Map.find reqs pid with
     | None -> false
     | Some ids -> Set.mem ids id
 
   let activate reqs pid id = Map.update reqs pid ~f:(function
-      | None -> Set.singleton (module L) id
+      | None -> Set.singleton (module Pid) id
       | Some ids -> Set.add ids id)
 
   let deactivate req pid id = Map.change req pid ~f:(function
@@ -101,54 +215,43 @@ module Knowledge = struct
         let ids = Set.remove ids id in
         if Set.is_empty ids then None else Some ids)
 
-  let collect (type t) tag id =
-    let module Domain =
-      (val Semantics.domain tag.domain : Domain.S with type t = t) in
-    get () >>= fun {data} ->
-    let init = Map.find data id |> function
-      | None -> Domain.empty
-      | Some sema -> Semantics.get tag.domain sema in
-    tag.promises |>
-    Knowledge.List.fold ~init ~f:(fun curr req ->
-        get () >>= fun s ->
-        if is_active s.reqs req.pid id
-        then Knowledge.return curr
-        else put {s with reqs = activate s.reqs req.pid id} >>= fun () ->
-          req.get id >>= fun next ->
-          put {s with reqs = deactivate s.reqs req.pid id} >>= fun () ->
-          match Domain.partial curr next with
-          | GE -> Knowledge.return curr
-          | LE | EQ | NC -> Knowledge.return next) >>= fun max ->
-    provide tag id max >>| fun () -> max
+  let objects : _ slot -> _ = fun slot ->
+    get () >>| fun base ->
+    match Map.find base slot.cls.id with
+    | None -> Base.empty_class
+    | Some objs -> objs
+
+  let update_reqs : _ slot -> _ = fun slot reqs ->
+    update @@ fun s ->
+    Map.update s slot.cls.id ~f:(function
+        | None -> {Base.empty_class with reqs}
+        | Some objs -> {objs with reqs})
+
+  let collect : type a p. (a,p) slot -> a Object.t -> p Knowledge.t =
+    fun slot id ->
+      objects slot >>= fun {Base.data} ->
+      let init = match Map.find data id with
+        | None -> slot.dom.empty
+        | Some v -> Value.get slot v in
+      slot.promises |>
+      Knowledge.List.fold ~init ~f:(fun curr req ->
+          objects slot >>= fun {Base.reqs} ->
+          if is_active reqs req.pid id
+          then Knowledge.return curr
+          else
+            update_reqs slot (activate reqs req.pid id) >>= fun () ->
+            req.get id >>= fun next ->
+            update_reqs slot (deactivate reqs req.pid id) >>= fun () ->
+            match slot.dom.order curr next with
+            | GT -> Knowledge.return curr
+            | LT | EQ | NC -> Knowledge.return next) >>= fun max ->
+      provide slot id max >>| fun () -> max
 
 
   include Knowledge
-  let domain x = x.domain
 
   let run x s = match State.run x s with
     | Ok x,s -> Ok (x,s)
     | Error err,_ -> Error err
-end
 
-type state = Knowledge.state
-type 'a knowledge = 'a Knowledge.t
-type 'a domain = 'a Semantics.domain
-type 'a content = 'a Knowledge.content
-
-module Ident = struct
-end
-
-module Label = struct
-  include L
-  let label = Semantics.declare "label" (module Domain.Label)
-  let labels = Knowledge.declare ~name:"labels-generator" label
-
-  module Generator = struct
-    open Knowledge.Syntax
-    let fresh =
-      Knowledge.collect labels L.root >>= fun last ->
-      let next = L.succ last in
-      Knowledge.provide labels L.root next >>| fun () ->
-      next
-  end
 end
--- lib/knowledge/bap_knowledge.mli
+++ lib/knowledge/bap_knowledge.mli
@@ -2,7 +2,6 @@ open Core_kernel
 open Monads.Std
 
 type 'a knowledge
-
 module Knowledge : sig
   type 'a t = 'a knowledge
 
@@ -10,26 +9,28 @@ module Knowledge : sig
   type +'a obj
   type 'a value
   type ('a,'p) slot
-  type 'p property
+  type 'p domain
   type state
   type conflict = ..
+
+  val empty : state
+
 
   val collect : ('a,'p) slot -> 'a obj -> 'p t
   val provide : ('a,'p) slot -> 'a obj -> 'p -> unit t
   val promise : ('a,'p) slot -> ('a obj -> 'p t) -> unit t
 
+  val run : unit t -> (state, conflict) result
 
-  val run : 'a obj t -> state -> ('a value * state, conflict) result
-
-  val empty : state
+  val extract : state -> 'a cls -> 'a obj -> 'a value
 
   include Monad.S with type 'a t := 'a t
   include Monad.Fail.S with type 'a t := 'a t
                         and type 'a error = conflict
 
   module Class : sig
-    val declare : ?desc:string -> string -> 'a cls
-    val derive : ?desc:string -> string -> 'a cls -> ('b -> 'a) cls
+    val declare : ?public:bool -> ?desc:string -> string -> 'a cls
+    val derived : ?desc:string -> string -> 'a cls -> ('b -> 'a) cls
     val parent : (_ -> 'a) cls -> 'a cls
 
 
@@ -47,7 +48,7 @@ module Knowledge : sig
 
     (** [scoped scope] pass a fresh new object to [scope].
 
-        The extend of the passed object is limited with the extend
+        The extend of the created object is limited with the extend
         of the function [scope].*)
     val scoped : 'a cls -> ('a obj t -> 'b knowledge) -> 'b knowledge
 
@@ -59,11 +60,10 @@ module Knowledge : sig
   end
 
   module Slot : sig
-    val declare : ?public:bool -> ?desc:string ->
-      string -> 'a cls -> 'p property -> ('a,'p) slot
+    val declare : ?desc:string ->
+      'a cls -> string -> 'p domain -> ('a,'p) slot
 
     val get : ('a,'p) slot -> 'a value -> 'p
-
     val put : ('a,'p) slot -> 'a value -> 'p -> 'a value
   end
 
@@ -73,124 +73,54 @@ module Knowledge : sig
     val merge : 'a value -> 'a value -> 'a value
   end
 
+  module Order : sig
+    type partial = LT | EQ | GT | NC
+  end
+
   module Domain : sig
-    module Order : sig
-      type partial = LE | EQ | GE | NC
-    end
+    type 'a t = 'a domain
 
-    module type S = sig
-      type t
 
-      (** no information.
-          aka undefined, aka the bottom element of the domain.
-          For all values [x] of the domain it should be true that
-          [empty <= x], where (<=) defines the partial order *)
-      val empty : t
+    (** [define ~inspect ~empty ~order name] defines a domain for the
+        type ['a].
 
-      (** [partial x y] establishes a partial order between [x] and [y],
+        The [empty] value denotes the representation of an absence of
+        information, or an undefined value, or the default value, or
+        the least possible value in the chain, etc. It's only required
+        that for all possible values [x], [empty <= x], where [<=]
+        is the partial order defined by the [order] parameter
 
-          Given a partial order relation [<=],
-          - [partial x y = LE] iff [x<=y && not(y <= x)]
+        The [order] function defines the partial order for the given
+        domain, such that
+          - [partial x y = LT] iff [x<=y && not(y <= x)]
           - [partial x y = EQ] iff [x <= y && y <= x]
-          - [partial x y = GE] iff [not (x <= y) && (y <= x)]
-          - [partial x y = NC] iff [not (x <= y) && not (y <= x)]
+          - [partial x y = GT] iff [not (x <= y) && (y <= x)]
+          - [partial x y = NC] iff [not (x <= y) && not (y <= x)].
 
-          The induced partial order shall be:
-
-          - reflexive: a <= a
-          - transitive: a <= b /\ b <= c -> a <= c
-          - antisymmetric: a <= b /\ b <= a -> a = b
-      *)
-      val partial : t -> t -> Order.partial
-
-      (** [inspect x] returns an overview of information stored in [x],
-          for introspection purposes.  *)
-      val inspect : t -> Base.Sexp.t
-
-    end
-    type 'a t = (module S with type t = 'a)
-
-    val chain : ?inspect:('a -> Base.Sexp.t) -> ('a -> 'a -> int) -> 'a t
-
-    val map : equal:('d -> 'd -> bool) -> ('a,) Map.comparator ->
-      'd Map.M()
-  end
+        The optional [inspect] function enables introspection, and may
+        return any representation of the domain value.
+    *)
+    val define :
+      ?inspect:('a -> Base.Sexp.t) ->
+      ?serialize : (module Binable.S with type t = 'a) ->
+      empty:'a ->
+      order:('a -> 'a -> Order.partial) -> string -> 'a domain
 
 
-  module Property : sig
-    val property : ?public:bool -> ?desc:string -> name:string ->
-      'c Object.Class.t -> 'a domain -> ('c,'a) slot
+    val total :
+      ?inspect:('a -> Base.Sexp.t) ->
+      ?serialize : (module Binable.S with type t = 'a) ->
+      empty:'a ->
+      order:('a -> 'a -> int) ->
+      string -> 'a domain
 
-  end
+    val mapping : ?equal:('d -> 'd -> bool) ->
+      ('a,'e) Map.comparator ->
+      string ->
+      ('a,'d,'e) Map.t domain
 
-  module Delete_me : sig
-    module Map : sig
-      module type Eq = sig
-        type t
-        val equal : t -> t -> bool
-      end
-
-      module Make(K : Base.Comparator.S)(V : Eq) :
-        S with type t = V.t Base.Map.M(K).t
-    end
-
-    module Chain : sig
-      module type T = sig
-        type t
-        val empty : t
-        val inspect : t -> Base.Sexp.t
-        val compare : t -> t -> int
-      end
-
-      module Make(Chain : T) : S with type t = Chain.t
-    end
-
-    module Counter : sig
-      type t
-      val zero : t
-      val succ : t -> t
-      val pp : Format.formatter -> t -> unit
-      include S with type t := t
-      include Base.Comparable.S with type t := t
-    end
-
-    module Label : S with type t = label
-  end
-
-  module Semantics : sig
-    type t = semantics
-
-    val declare :
-      ?serializer:(module Binable.S with type t = 'a) ->
-      name:string -> (module Domain.S with type t = 'a) -> 'a domain
-
-    val empty : t
-    val create : 'a domain -> 'a -> t
-    val get : 'a domain -> t -> 'a
-    val put : 'a domain -> t -> 'a -> t
-    val merge : t -> t -> t
-
-    val sexp_of_t : t -> Sexp.t
-    val t_of_sexp : Sexp.t -> t
-
-    include Domain.S with type t := t
-    include Binable.S with type t := t
-
-    module Sorted(Sort : sig type 'a t end) : sig
-      type 'a t                      (* semantic value *)
-
-      val create : 'a Sort.t -> semantics -> 'a t
-      val empty : 'a Sort.t -> 'a t
-      val get : 'b domain -> 'a t -> 'b
-      val put : 'b domain -> 'a t -> 'b -> 'a t
-      val kind : 'a t -> 'a Sort.t
-      val partial : 'a t -> 'a t -> Domain.Order.partial
-      val merge : 'a t -> 'a t -> 'a t
-      val semantics : 'a t -> semantics
-    end
-
-    val domains : unit -> string list
-    val pp : Format.formatter -> t -> unit
-    val pp_domains : string list -> Format.formatter -> t -> unit
+    val int : int t
+    val int63 : Int63.t
+    val string : string domain
   end
 end
