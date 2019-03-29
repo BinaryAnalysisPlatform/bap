4747f8c3b55eacbebc9c5715930c58a4dd26dc0d
Oleg <forown@yandex.ru>
2019-03-29 12:43:03 -0400

    updated after review

--- lib/bap/bap_log.ml
+++ lib/bap/bap_log.ml
@@ -54,8 +54,12 @@ let print_message ppf msg =
   | Error -> eprintf "%s@\n%!" msg.message
   | _ -> ()
 
-let lock () =
-  let file = Filename.get_temp_dir_name () / "bap_lock" in
+let lock_filename logdir =
+  let digest = Md5.digest_string logdir in
+  let name = sprintf "bap-%s.lock" (Md5.to_hex digest) in
+  Filename.get_temp_dir_name () / name
+
+let lock file =
   let lock = Unix.openfile file Unix.[O_RDWR; O_CREAT] 0o666 in
   Unix.lockf lock Unix.F_LOCK 0;
   lock
@@ -68,7 +72,7 @@ let open_log_channel user_dir =
   try
     let log_folder = log_folder user_dir in
     let file = log_folder / "log" in
-    let lock = lock () in
+    let lock = lock (lock_filename log_folder) in
     protect ~f:(fun () ->
         mkdir log_folder;
         if Sys.file_exists file
