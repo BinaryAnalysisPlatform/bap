022e9cb3ea4e509be31017e976a627d742fefebb
Oleg <forown@yandex.ru>
2019-06-14 11:50:52 -0400

    adds x86 SP handling for external calls
    So far the stack pointer register wasn't properly handled by
    primus in case of external calls.
    
    The reason is in the asymmetry of the function calling in x86 ABI:
    the caller side allocates the stack slot for the returning address
    and callee sise is responsible for clearing it. But in case of
    external calls, dependless if a lisp stub exists or call remains
    unresolved, there wasn't any stack restoring. As a result
    stack pointer didn't point to a proper place.
    
    This PR solve this problem by incrementing stack pointer
    after lisp call returns. It works for unresolved calls as well.

--- plugins/primus_x86/primus_x86_loader.ml
+++ plugins/primus_x86/primus_x86_loader.ml
@@ -1,10 +1,13 @@
 open Core_kernel
 open Bap.Std
 open Bap_primus.Std
+open X86_cpu
 
 module Component(Machine : Primus.Machine.S) = struct
   open Machine.Syntax
   module Env = Primus.Env.Make(Machine)
+  module Value = Primus.Value.Make(Machine)
+  module Interpreter = Primus.Interpreter.Make(Machine)
 
   let zero = Primus.Generator.static 0
 
@@ -12,11 +15,22 @@ module Component(Machine : Primus.Machine.S) = struct
     Machine.Seq.iter (Set.to_sequence flags) ~f:(fun reg ->
         Env.add reg zero)
 
+  let correct_sp sp addend _ =
+    Env.get sp >>= fun x ->
+    Value.of_int ~width:(addend * 8) addend >>= fun addend ->
+    Interpreter.binop Bil.plus x addend >>= fun x ->
+    Env.set sp x
+
+  let correct_sp sp addend =
+    Primus.Linker.Trace.lisp_call_return >>> correct_sp sp addend
+
+  let seq = Machine.sequence
+
   let init () =
     Machine.get () >>= fun proj ->
     match Project.arch proj with
-    | `x86 -> initialize_flags (X86_cpu.IA32.flags)
-    | `x86_64 -> initialize_flags (X86_cpu.AMD64.flags)
+    | `x86 -> seq [initialize_flags IA32.flags; correct_sp IA32.sp 4]
+    | `x86_64 -> seq [initialize_flags AMD64.flags; correct_sp AMD64.sp 8]
     | _ -> Machine.return ()
 
 end
