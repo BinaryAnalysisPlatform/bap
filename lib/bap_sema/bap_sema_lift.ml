BinaryAnalysisPlatform/bap

commit 0352edfe42ba5560b178b6bf990cf15e81d78f57

prunes return edges for conditional calls (#987)
A predicated invocation of a procedure which is expected to return, must perform two actions under the same predicate - store the return address and actually do the control flow transfer.
E.g., assume the `bleq foo` instruction
```
if (ZF) {
    LR := 4;
    call foo
}
```

That will have the following IR
```
when ZF goto @take_call
goto @next

take_call:
LR :=   4
call @foo return @next

next:
```
When we see an interprocedural control flow which is predicated, then we can safely assume that there weren't any data effects under the same predicate, therefore this call is not expected to return.
Of course, it is possible to craft IR which will violate this property, e.g.,
```
LR := 4
when ZF call @foo return @next
call @bar return @next
next:
```
which corresponds to the following pseudocode `if (a = b) foo() else bar()`
But such representation could be generated only after a global optimization pass or if an architecture actually has an instruction that dispatches several calls based on a condition.
The global optimization is not possible in the context of the sema-lifter as it is applied on a per-instruction basis, so it always operates with the code produced for the given instruction.

lib/bap_sema/bap_sema_lift.ml
@@ -90,9 +90,16 @@ module IrBuilder = struct
     | [] -> []
     | b :: bs -> Term.set_attr b address addr :: bs
 
+  let is_conditional jmp = match Ir_jmp.cond jmp with
+    | Bil.Int _ ->  false
+    | _ -> true
+
   let turn_into_call ret blk =
-    Term.map jmp_t blk ~f:(update_jmp ~f:(fun dst _ jmp ->
-        jmp ~dst:ret ~alt:dst))
+    Term.map jmp_t blk ~f:(fun jmp ->
+        update_jmp jmp ~f:(fun dst _ make ->
+            if is_conditional jmp
+            then make ~dst:ret ~alt:None
+            else make ~dst:ret ~alt:dst))
 
   let landing_pad return jmp =
     match Ir_jmp.kind jmp with
@@ -122,10 +129,6 @@ module IrBuilder = struct
 
   let insert_inter_fall alt blk =
     Term.append jmp_t blk @@ Ir_jmp.reify ~alt ()
-
-  let is_conditional jmp = match Ir_jmp.cond jmp with
-    | Bil.Int _ ->  false
-    | _ -> true
 
   let is_last_jump_nonconditional blk =
     match Term.last jmp_t blk with

commit 96bb9f48cd0f35a1ae84565489a085ccaef58a3d

fixes return edges pruning for conditional calls
This PR fixes a bug introduced by #987
and finally, prunes return edges for conditional calls,
while the previous PR actually pruned call edges.

lib/bap_sema/bap_sema_lift.ml
@@ -98,7 +98,7 @@ module IrBuilder = struct
     Term.map jmp_t blk ~f:(fun jmp ->
         update_jmp jmp ~f:(fun dst _ make ->
             if is_conditional jmp
-            then make ~dst:ret ~alt:None
+            then make ~dst:None ~alt:dst
             else make ~dst:ret ~alt:dst))
 
   let landing_pad return jmp =