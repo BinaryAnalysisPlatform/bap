cccbdd516a752c859b62e73d81bf2fc3c37edf9c
Ivan Gotovchits <ivg@ieee.org>
2019-10-17 09:31:55 -0400

    pushes the raw lifter to the lower level
    It now operates on the Ogre level, so it is capable of creating both
    images and projects, therefore it could be used for the new objdump
    command.
    
    Another nice addition, is that now it is possible to specify file
    the offset and length, that enables extracting portions of big
    binaries, e.g., the following will extract the `main` function from
    our testing echo binary,
    
    ```
    bap testsuite/bin/x86_64-linux-gnu-echo --loader=raw \
       --raw-{base=0x40076b,offset=0x76b,length=542}
    ```
    
    The funny part is that our symbolizers will still work correctly and
    will be able to provide names.
    
    This features basically make the raw loader a swiss-knife unpacker,
    which could be used to run bap on arbitrary files. Besides, the
    disassembly of our testing echo is identical with the raw and llvm
    lifters :)

--- plugins/raw/raw_main.ml
+++ plugins/raw/raw_main.ml
@@ -4,6 +4,7 @@ let doc = {|
 Provides a loader for raw binaries. Raw binaries to not contain any
 meta information or other headers, so this input should be provided
 form the outside.
+
 |}
 
 open Bap.Std
@@ -13,7 +14,6 @@ open Extension.Syntax
 
 module Spec = struct
   open Extension
-
   let arch_t =
     let parse s = match Arch.of_string s with
       | None -> invalid_argf "Unknown architecture %s" s ()
@@ -38,17 +38,83 @@ module Spec = struct
   let base_address = Parameter.declare
       ~doc:"The address of the first byte"
       addr_t "base"
+
+  let offset = Parameter.declare
+      ~doc:"The offset of the first byte"
+      Type.int64 "offset"
+
+  let length = Parameter.declare
+      ~doc:"The length of the code section"
+      Type.(some int64) "length"
 end
 
+let doc_template = {|
+(declare arch (name str))
+(declare base-address (addr int))
+(declare entry-point (addr int))
+(declare mapped (addr int) (size int) (off int))
+(declare code-region (addr int) (size int) (off int))
+(declare named-region (addr int) (size int) (name str))
+(declare segment (addr int) (size int) (r bool) (w bool) (x bool))
+(declare section (addr int) (size int))
+(declare code-start (addr int))
+
+(arch $arch)
+(base-address $base)
+(entry-point $entry)
+(mapped $base $length $offset)
+(code-region $base $length $offset)
+(named-region $base $length code)
+(section $base $length)
+(segment $base $length true false true)
+|}
+
 let register_loader ctxt =
-  let arch = ctxt-->Spec.arch in
-  let to_word n = Addr.create n (Size.in_bits (Arch.addr_size arch)) in
-  let entries = ctxt-->Spec.entry_points |> Seq.of_list |>
-                Seq.map ~f:to_word |> Rooter.create
-  and base = to_word @@ ctxt-->Spec.base_address in
-  Rooter.provide entries;
-  Project.Input.register_loader "raw" @@ fun filename ->
-  Project.Input.binary ~base arch ~filename
+  Image.register_loader ~name:"raw" (module struct
+    let generate measure input =
+      let options = Spec.[
+          "arch", Arch.to_string (ctxt-->arch);
+          "offset", Int64.to_string @@ ctxt-->offset;
+          "base", Bitvec.to_string @@ ctxt-->base_address;
+          "entry", begin match ctxt-->entry_points with
+            | [] -> Bitvec.to_string @@ ctxt-->base_address;
+            | x :: _ -> Bitvec.to_string x
+          end;
+          "length", Int64.to_string @@ match ctxt-->length with
+          | None -> measure input
+          | Some n -> n;
+        ] |> String.Map.of_alist_exn in
+      let buf = Buffer.create 128 in
+      let ppf = Format.formatter_of_buffer buf in
+      doc_template |>
+      Buffer.add_substitute buf (fun var ->
+          match Map.find options var with
+          | None -> invalid_argf "bug: missed a var: %S" var ()
+          | Some v -> v);
+      ctxt-->Spec.entry_points |>
+      List.iter ~f:(Format.fprintf ppf "(code-start %a)@\n" Bitvec.pp);
+      Format.pp_print_flush ppf ();
+      Buffer.contents buf |>
+      Ogre.Doc.from_string |>
+      Or_error.ok_exn
+
+    let length_of_file filename =
+      let desc = Unix.openfile filename Unix.[O_RDONLY] 0o400 in
+      let {Unix.LargeFile.st_size} = Unix.LargeFile.fstat desc in
+      Unix.close desc;
+      st_size
+
+    let length_of_data str =
+      Int64.of_int (Bigstring.length str)
+
+    let from_file name =
+      Or_error.try_with @@ fun () ->
+      Some (generate length_of_file name)
+
+    let from_data data =
+      Or_error.try_with @@ fun () ->
+      Some (generate length_of_data data)
+  end)
 
 let () = Extension.declare ~provides:["loader"] ~doc @@ fun ctxt ->
   Ok (register_loader ctxt)
