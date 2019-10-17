e45516520e086e2a7291ed992c178b3087b38929
Oleg <forown@yandex.ru>
2019-10-17 10:10:40 -0400

    refactoring

--- plugins/primus_x86/primus_x86_loader.ml
+++ plugins/primus_x86/primus_x86_loader.ml
@@ -8,27 +8,12 @@ let is_section name v =
   | Some x -> String.(x = name)
   | _ -> false
 
-let address_range mem arch =
-  let step = Arch.addr_size arch |> Size.in_bytes in
-  let rec loop acc n =
-    if Addr.(n > Memory.max_addr mem)
-    then acc
-    else
-      let next = Addr.(n ++ step) in
-      loop (n :: acc) next in
-  loop [] (Memory.min_addr mem)
-
 module Make_unresolved(Machine : Primus.Machine.S) = struct
   module Linker = Primus.Linker.Make(Machine)
 
   let exec =
     Linker.exec (`symbol Primus.Linker.unresolved_handler)
 end
-
-let try_load mem size addr =
-  match Memory.get ~scale:size ~addr mem with
-  | Ok data -> Some data
-  | _ -> None
 
 module Plt_jumps(Machine : Primus.Machine.S) = struct
   module Interp = Primus.Interpreter.Make(Machine)
@@ -41,46 +26,27 @@ module Plt_jumps(Machine : Primus.Machine.S) = struct
     Machine.get () >>| fun proj ->
     Memmap.filter (Project.memory proj) ~f:(is_section sec_name) |>
     Memmap.to_sequence |>
-    Seq.map ~f:fst |>
-    Seq.to_list
-
-  let got_cells =
-    Machine.arch >>= fun arch ->
-    section_memory ".got.plt" >>| fun memory ->
-    List.fold memory ~init:[]
-      ~f:(fun acc mem -> acc @ address_range mem arch)
-
-  let load_word addr =
-    Machine.arch >>= fun arch ->
-    Value.of_word addr >>= fun addr ->
-    Interp.load addr (Arch.endian arch)
-      (Arch.addr_size arch :> size) >>|
-    Value.to_word
-
-  let load_table =
-    got_cells >>=
-    Machine.List.map ~f:load_word
+    Seq.map ~f:fst
 
   let load_table =
     Machine.arch >>= fun arch ->
     section_memory ".got.plt" >>| fun memory ->
-    List.fold memory ~init:[]
+    Seq.fold memory ~init:[]
       ~f:(fun acc mem ->
-        let range = address_range mem arch in
-        acc @ List.filter_map range
-                ~f:(try_load mem (Arch.addr_size arch :> size)))
+          let step = (Arch.addr_size arch :> size) in
+          Memory.fold mem ~word_size:step ~init:[] ~f:(fun a w -> a :: w))
 
   let filter_plt addrs =
     section_memory ".plt" >>= fun memory ->
     Machine.return @@
     List.filter addrs ~f:(fun a ->
-        List.exists memory ~f:(fun mem -> Memory.contains mem a))
+        Seq.exists memory ~f:(fun mem -> Memory.contains mem a))
 
   let unlink addrs =
     Machine.List.iter addrs ~f:(fun addr ->
         Linker.link ~addr (module Make_unresolved))
 
-  let unresolve =
+  let unresolve  =
     load_table >>=
     filter_plt >>=
     unlink
