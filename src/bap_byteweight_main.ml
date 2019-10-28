9a9e1cab8b4038018a1b4521268af46a5d5ae44a
Ivan Gotovchits <ivg@ieee.org>
2019-10-28 16:50:52 -0400

    removes stalled code
    The code is no longer used anymore or is a part of the
    bap-fsi-benchmark tool, which is no longer used or needed in the main
    code base. We will soon provide a corresonding functionality as a part
    of the bap-byteweight utility.

--- src/bap_byteweight_main.ml
+++ src/bap_byteweight_main.ml
@@ -198,6 +198,34 @@ let symbols loader print_name print_size input _ctxt =
       then printf " %4d" (Memory.length mem);
       printf "\n")
 
+
+module SymIO = struct
+  let read arch ic : (string * addr * addr) list =
+    let sym_of_sexp x = [%of_sexp:string * int64 * int64] x in
+    let addr_of_int64 x =
+      let width = Arch.addr_size arch |> Size.in_bits in
+      Addr.of_int64 ~width x in
+    List.(Sexp.input_sexps ic >>| sym_of_sexp >>| (fun (s, es, ef) ->
+        s, addr_of_int64 es, addr_of_int64 ef))
+
+  let read_addrs ic : addr list =
+    List.t_of_sexp Addr.t_of_sexp @@ Sexp.input_sexp ic
+
+  let write_addrs oc (addrs : addr list) : unit =
+    Sexp.output oc @@ List.sexp_of_t Addr.sexp_of_t addrs
+
+  let write oc (syms : (string * addr * addr) list) : unit =
+    let sexp_of_sym x = [%sexp_of:string * int64 * int64] x in
+    try
+      let syms = List.map syms ~f:(fun (s, es, ef) -> s, Addr.to_int64 es |> ok_exn,
+                                                      Addr.to_int64 ef |> ok_exn) in
+      List.iter syms ~f:(fun sym -> Sexp.output_hum oc @@ sexp_of_sym sym;
+                          Out_channel.output_char oc '\n')
+    with exn ->
+      printf "Output error: %a." Exn.pp exn;
+      ()
+end
+
 let dump loader comp info min_length max_length threshold path input _ctxt =
   create_image loader input >>= fun img ->
   let arch = Image.arch img in
@@ -210,14 +238,14 @@ let dump loader comp info min_length max_length threshold path input _ctxt =
       else BW.find_using_threshold bw threshold
           ~min_length ~max_length in
 
-    Symbols.write_addrs stdout @@ Addr.Set.to_list @@
+    SymIO.write_addrs stdout @@ Addr.Set.to_list @@
     Seq.fold (code_of_image img)
       ~init:Addr.Set.empty ~f:(fun starts mem ->
           List.fold (find mem) ~init:starts ~f:Set.add)
   | `SymTbl ->
     let syms = Image.symbols img in
     let mems = Table.regions syms in
-    Symbols.write_addrs stdout @@ Seq.to_list (Seq.map mems Memory.min_addr);
+    SymIO.write_addrs stdout @@ Seq.to_list (Seq.map mems Memory.min_addr);
     Ok ()
 
 let create_parent_dir dst =
