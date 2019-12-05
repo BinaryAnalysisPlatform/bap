5dbc1049cf4153ec9e5ecd17397b1e15f6aafd88
oleg <forown@yandex.ru>
2019-12-04 20:04:43 -0500

    revert changes in text-tags

--- lib/text_tags/text_tags.ml
+++ lib/text_tags/text_tags.ml
@@ -12,16 +12,10 @@ let expect_arg () = invalid_arg "Expect: arg := (<name> <val>)"
 
 let is_quoted = String.is_prefix ~prefix:"\""
 
-let string_of_stag = function
-    | String_tag tag -> tag
-    | _ -> invalid_arg "String tag expected"
-
-
 module Html = struct
   open Sexp
-
-  let mark_open_stag stag : string =
-    match Sexp.of_string @@ string_of_stag stag with
+  let mark_open_tag tag : string =
+    match Sexp.of_string tag with
     | List (Atom tag :: attrs) ->
       let attrs = List.map attrs ~f:(function
           | List [Atom name; Atom value] ->
@@ -33,31 +27,31 @@ module Html = struct
     | Atom tag -> sprintf "<%s>" tag
     | _ -> expect_tag ()
 
-  let mark_close_stag stag : string =
-    match Sexp.of_string @@ string_of_stag stag with
+  let mark_close_tag tag : string =
+    match Sexp.of_string tag with
     | List (Atom tag :: _) | Atom tag -> sprintf "</%s>" tag
     | _ -> expect_tag ()
 
 
-  let print_open_stag fmt  _ =  pp_open_vbox fmt 1; pp_print_cut fmt ()
-  let print_close_stag fmt _ =  pp_close_box fmt (); pp_print_cut fmt ()
+  let print_open_tag fmt  _ =  pp_open_vbox fmt 1; pp_print_cut fmt ()
+  let print_close_tag fmt _ =  pp_close_box fmt (); pp_print_cut fmt ()
 
   let install fmt =
     pp_set_mark_tags  fmt true;
     pp_set_print_tags fmt true;
-    pp_set_formatter_stag_functions fmt {
-      mark_open_stag;
-      mark_close_stag;
-      print_open_stag = print_open_stag fmt;
-      print_close_stag = print_close_stag fmt;
+    pp_set_formatter_tag_functions fmt {
+      mark_open_tag;
+      mark_close_tag;
+      print_open_tag = print_open_tag fmt;
+      print_close_tag = print_close_tag fmt;
     }
 end
 
 module Blocks = struct
   open Sexp
 
-  let filter_attrs stag : string option =
-    match Sexp.of_string @@ string_of_stag stag with
+  let filter_attrs tag : string option =
+    match Sexp.of_string tag with
     | List (Atom _ :: attrs) ->
       List.filter_map attrs ~f:(function
           | List [Atom "title"; Atom v] -> Some (`T v)
@@ -71,22 +65,22 @@ module Blocks = struct
     | Atom _ -> None
     | _ -> expect_tag ()
 
-  let mark_open_stag stag : string =
-    filter_attrs stag |>
+  let mark_open_tag tag : string =
+    filter_attrs tag |>
     Option.value_map ~default:"" ~f:(sprintf "begin(%s) ")
 
-  let mark_close_stag stag : string =
-    filter_attrs stag |>
+  let mark_close_tag tag : string =
+    filter_attrs tag |>
     Option.value_map ~default:"" ~f:(sprintf "end(%s)")
 
-  let print_open_stag fmt stag : unit =
-    if Option.is_some (filter_attrs stag) then begin
+  let print_open_tag fmt tag : unit =
+    if Option.is_some (filter_attrs tag) then begin
       pp_open_vbox fmt 1;
       pp_print_cut fmt ();
     end
 
-  let print_close_stag fmt stag =
-    if Option.is_some (filter_attrs stag) then begin
+  let print_close_tag fmt tag =
+    if Option.is_some (filter_attrs tag) then begin
       pp_close_box fmt ();
       pp_print_cut fmt ();
     end
@@ -94,11 +88,11 @@ module Blocks = struct
   let install fmt =
     pp_set_mark_tags fmt true;
     pp_set_print_tags fmt true;
-    pp_set_formatter_stag_functions fmt {
-      mark_open_stag;
-      mark_close_stag;
-      print_open_stag = print_open_stag fmt;
-      print_close_stag = print_close_stag fmt;
+    pp_set_formatter_tag_functions fmt {
+      mark_open_tag;
+      mark_close_tag;
+      print_open_tag = print_open_tag fmt;
+      print_close_tag = print_close_tag fmt;
     }
 end
 
@@ -138,8 +132,7 @@ module Attr = struct
     then pp_print_string ppf "\x1b[39;49m";
     clean := true
 
-  let print_open_tag ppf stag : unit =
-    let tag = string_of_stag stag in
+  let print_open_tag ppf tag : unit =
     if need_to_print tag then
       match ascii_color tag with
       | Some color when colorify.contents ->
@@ -151,10 +144,10 @@ module Attr = struct
 
   let install ppf =
     pp_set_print_tags ppf true;
-    let tags = pp_get_formatter_stag_functions ppf () in
-    pp_set_formatter_stag_functions ppf {
+    let tags = pp_get_formatter_tag_functions ppf () in
+    pp_set_formatter_tag_functions ppf {
       tags with
-      print_open_stag = print_open_tag ppf;
+      print_open_tag = print_open_tag ppf;
     };
     let out = pp_get_formatter_out_functions ppf () in
     pp_set_formatter_out_functions ppf {
@@ -185,13 +178,13 @@ let install fmt mode =
   | None -> raise (Unknown_mode mode)
 
 let with_mode fmt mode =
-  let g = pp_get_formatter_stag_functions fmt () in
+  let g = pp_get_formatter_tag_functions fmt () in
   let mark = pp_get_mark_tags fmt () in
   let print = pp_get_print_tags fmt () in
   let finally () =
     pp_set_mark_tags fmt mark;
     pp_set_print_tags fmt print;
-    pp_set_formatter_stag_functions fmt g in
+    pp_set_formatter_tag_functions fmt g in
   install fmt mode;
   Exn.protect ~finally
 
