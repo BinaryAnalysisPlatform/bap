10b0baf92eb2c68b0bda47dd85a896dd914dd4f3
Ivan Gotovchits <ivg@ieee.org>
2020-01-28 13:28:36 -0500

    cleans up the build_plugins tool
    to make codacy happy we switch from using bacticks to `$(...)`.

--- tools/build_plugins.sh
+++ tools/build_plugins.sh
@@ -16,7 +16,7 @@ built_plugins=_build/built_plugins
 compute_digest() {
     plugin_lib=$1
     output=$2
-    $md5sum `ocamlfind query -predicates byte -a-format -r $plugin_lib` > $output
+    $md5sum $(ocamlfind query -predicates byte -a-format -r $plugin_lib) > $output
 }
 
 build_plugin() {
@@ -33,15 +33,14 @@ build_plugin() {
 
     if cmp -s digest new_digest
     then
-        echo "$1: cache hit"
+        echo "$1: is up-to-date"
     else
-        echo "$1: cache miss"
         touch $plugin.ml
         bapbuild -clean
         bapbuild -package bap-plugin-$1 $plugin.plugin
-        DESC=`ocamlfind query -format "%D" bap-plugin-$1`
-        CONS=`ocamlfind query -format "%(constraints)" bap-plugin-$1`
-        TAGS=`ocamlfind query -format "%(tags)" bap-plugin-$1`
+        DESC=$(ocamlfind query -format "%D" bap-plugin-$1)
+        CONS=$(ocamlfind query -format "%(constraints)" bap-plugin-$1)
+        TAGS=$(ocamlfind query -format "%(tags)" bap-plugin-$1)
         if [ -z $CONS ]; then
             bapbundle update -name $1 -desc "$DESC" -tags "core,$TAGS" $plugin.plugin
         else
@@ -53,9 +52,8 @@ build_plugin() {
     mv new_digest digest
 }
 
-for plugin in `ls plugins`; do
+for plugin in $(ls plugins); do
     (build_plugin $plugin)&
-    echo `pwd`
 done
 wait
 echo "Finished updating plugins"
