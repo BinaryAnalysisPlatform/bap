2ddcab504242bb635b6c75bad9dd93e0e0e62c2f
Ivan Gotovchits <ivg@ieee.org>
2020-02-06 14:16:02 -0500

    specifies a missing size in `(taint-introduce 'indirectly)`
    Previously it was an ill-typed piece of code as we didn't pass the
    size. We employ parameters to set the size, and default to one word.
    
    Parameters are dynamically bound, so you can do as
    
    ```lisp
    (let ((*indirect-taint-size* 100))
       (taint-introduce 'indirectly k v))
    ```

--- plugins/primus_taint/lisp/taint.lisp
+++ plugins/primus_taint/lisp/taint.lisp
@@ -1,11 +1,14 @@
 ;;; Taint Analysis Framework
 ;;;
 
+(defparameter *indirect-taint-size* nil)
 
 (defun taint-introduce (r k v)
   "(taint-introduce REL KIND VAL) associates a fresh new taint with
    the value VAL using the specified relation REL, that must be either
-   'directly or 'indirectly"
+   'directly or 'indirectly. If taint is introduced indirectly, then
+   *indirect-taint-size* number of bytes are tainted."
   (if (= r 'directly)
       (taint-introduce-directly k v)
-    (taint-introduce-indirectly k v)))
+    (let ((n (or *indirect-taint-size* (/ (word-width) 8))))
+      (taint-introduce-indirectly k v n))))
