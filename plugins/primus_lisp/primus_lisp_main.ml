692dd7d88eda82c59120d2b7d2da8760a66fda85
Ivan Gotovchits <ivg@ieee.org>
2020-02-13 10:54:33 -0500

    prevents overwriting of a file in the input channel redirection (#1049)
    In Primus Lisp, we have channel redirection which makes files from the
    host system visible to the emulation layer. Underneath the hood, we
    use common OCaml channels to model files (and probably we should use
    Unix file descriptors), and when a file is mapped, we open both input
    and output channel. However, when we open the input channel, the file
    is truncated, with all the ramifications.
    
    The quickfix is to add the `append` flag. The long-term solution would
    be switching to Unix file desciptors and passing flags to the `open(2)`
    function.

--- plugins/primus_lisp/primus_lisp_main.ml
+++ plugins/primus_lisp/primus_lisp_main.ml
@@ -102,7 +102,7 @@ module Signals(Machine : Primus.Machine.S) = struct
         signal pc_change word one Type.(one int)
           {|(pc-change PC) is emitted when PC is updated|};
         signal eval_cond value one Type.(one bool)
-          {|(eval_cond V) is emitted after evaluating a conditional to V|};
+          {|(eval-cond V) is emitted after evaluating a conditional to V|};
         signal jumping (value,value) pair Type.(tuple [bool; int])
           {|(jumping C D) is emitted before jump to D occurs under the
           condition C|};
