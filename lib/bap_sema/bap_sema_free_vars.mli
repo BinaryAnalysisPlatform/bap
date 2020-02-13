9c2e0a394e9b38f681664ad2f65bf82b2bb2da2d
Ivan Gotovchits <ivg@ieee.org>
2020-02-13 10:02:03 -0500

    uses liveness analysis for free variable when no SSA available
    An approximation with dominators doesn't really work well, and since
    we have the fixpoint function for quite a few time there is no reason
    not to implement it using the classical dataflow.

--- lib/bap_sema/bap_sema_free_vars.mli
+++ lib/bap_sema/bap_sema_free_vars.mli
@@ -1,5 +1,8 @@
 open Bap_types.Std
+open Graphlib.Std
 open Bap_ir
 
+val start : tid
+val exit : tid
+val liveness : sub term -> (tid, Var.Set.t) Solution.t
 val free_vars_of_sub : sub term -> Var.Set.t
-val bind_args : sub term -> sub term
