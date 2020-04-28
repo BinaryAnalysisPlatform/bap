2910e764311a49482f5dc31a2077fced61924b42
ivg <ivg@ieee.org>
2020-04-28 17:30:37 -0400

    adds prototypes to the ctypes functions

--- plugins/api/api/c/posix.h
+++ plugins/api/api/c/posix.h
@@ -427,3 +427,20 @@ int *__errno_location(void);
 
 int brk(void *addr);
 void *sbrk(int increment);
+
+
+// ctypes.h
+int isalnum(int c);
+int isalpha(int c);
+int iscntrl(int c);
+int isdigit(int c);
+int isgraph(int c);
+int islower(int c);
+int isprint(int c);
+int ispunct(int c);
+int isspace(int c);
+int isupper(int c);
+int isxdigit(int c);
+
+int isascii(int c);
+int isblank(int c);
