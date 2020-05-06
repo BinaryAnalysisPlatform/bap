67283b92e52da4711de00bcdaa521b504798d553
ivg <ivg@ieee.org>
2020-05-06 17:59:35 -0400

    implements symbolic streams

--- plugins/primus_symbolic_executor/lisp/symbolic-io.lisp
+++ plugins/primus_symbolic_executor/lisp/symbolic-io.lisp
@@ -1,20 +1,20 @@
-(declare (static opened-descriptors))
+(declare (static opened-descriptors
+                 opened-streams))
 (declare (context (component bap:symbolic-computer)))
 
 (defparameter *symbolic-io-bufer-size* 4096)
-(defparameter *symbolic-stdin-size* 32768)
-(defparameter *symbolic-initial-file-size* 16)
+(defparameter *symbolic-stdin-size* 0x100)
+(defparameter *symbolic-initial-file-size* 0x100)
+(defconstant minimum-stream-number 0x8000)
 
-(defun symbolic-open (name flags)
-  (declare (external "open"))
-  (puts name)
-  (incr opened-descriptors)
-  (let ((name (symbol-of-string name))
-        (desc (symbolic-value (symbol-concat 'open-desc- name))))
+(defun symbolic-open-input (name)
+  (let ((next-desc (+1 opened-descriptors))
+        (desc (symbolic-value (symbol-concat name '-desc) next-desc)))
     (symbolic-assume (or (= desc -1)
-                         (= desc opened-descriptors)))
+                         (= desc next-desc)))
     (if (= desc -1) -1
-      (let ((size (symbolic-value  (symbol-concat 'open-size name)
+      (set opened-descriptors next-desc)
+      (let ((size (symbolic-value  (symbol-concat name '-size)
                                    (word-width)
                                    *symbolic-initial-file-size*))
             (data (symbolic-memory (symbol-concat name)
@@ -23,6 +23,11 @@
         (dict-add 'symbolic-open-data desc data)
         (dict-add 'symbolic-open-fpos desc 0)
         desc))))
+
+
+(defun open (name flags)
+  (declare (external "open"))
+  (symbolic-open-input (symbol-of-string name)))
 
 
 (defun symbolic-copy-to-concrete (memory cptr sptr len)
@@ -49,6 +54,31 @@
       (dict-add 'symbolic-open-fpos fd (+ fpos len))
       len)))
 
+(defun fopen (path mode)
+  (declare (external "fopen"))
+  (let ((name (symbol-of-string path))
+        (desc (symbolic-open-input name)))
+    (if (= desc -1) 0
+      (incr opened-streams)
+      (dict-add 'symbolic-streams opened-streams desc)
+      opened-streams)))
+
+(defun desc-of-stream (stream)
+  (if (= stream 0) -1
+    (or (dict-get 'symbolic-streams stream) 0)))
+
+(defun fgetc (file)
+  (declare (external "fgetc" "getc"))
+  (let ((fd (desc-of-stream file)))
+    (if (= fd -1) -1
+    (let ((fpos (dict-get 'symbolic-open-fpos fd))
+          (size (dict-get 'symbolic-open-size fd))
+          (data (dict-get 'symbolic-open-data fd)))
+      (if (= fpos size) -1
+        (msg "fgetc($0) -> fpos($1) < size($2)" fd fpos size)
+        (dict-add 'symbolic-open-fpos fd (+1 fpos))
+        (symbolic-memory-read data fpos))))))
+
 (defun init-symbolic-stdin ()
   (dict-add 'symbolic-open-fpos 0 0)
   (dict-add 'symbolic-open-size 0 *symbolic-initial-file-size*)
@@ -58,4 +88,5 @@
 
 
 (defmethod init ()
-  (init-symbolic-stdin))
+  (init-symbolic-stdin)
+  (set opened-streams minimum-stream-number))
