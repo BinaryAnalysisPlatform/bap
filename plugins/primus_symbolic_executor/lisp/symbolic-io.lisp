1a80fc504568f25be07c26134135ce87551f7c0d
ivg <ivg@ieee.org>
2020-05-15 14:18:24 -0400

    fixes tons of bugs in the symbolic stdio

--- plugins/primus_symbolic_executor/lisp/symbolic-io.lisp
+++ plugins/primus_symbolic_executor/lisp/symbolic-io.lisp
@@ -1,8 +1,10 @@
+(require types)
+
 (declare (static opened-descriptors
                  opened-streams))
 (declare (context (component bap:symbolic-computer)))
 
-(defparameter *symbolic-io-bufer-size* 4096)
+(defparameter *symbolic-io-buffer-size* 4096)
 (defparameter *symbolic-stdin-size* 0x100)
 (defparameter *symbolic-initial-file-size* 0x100)
 (defconstant minimum-stream-number 0x8000)
@@ -10,10 +12,14 @@
 
 (defun symbolic-open-input (name)
   (let ((next-desc (+1 opened-descriptors))
-        (desc (symbolic-value (symbol-concat name '-desc) next-desc)))
+        (desc (symbolic-value (symbol-concat name '-desc)
+                              (bitwidth int)
+                              next-desc)))
     (symbolic-assume (or (= desc -1)
                          (= desc next-desc)))
     (if (= desc -1) -1
+      (msg "opened-descriptors = $0, next-desc = $1, desc = $2"
+           opened-descriptors next-desc desc)
       (set opened-descriptors next-desc)
       (let ((size (symbolic-value  (symbol-concat name '-size)
                                    (word-width)
@@ -23,6 +29,7 @@
         (dict-add 'symbolic-open-size desc size)
         (dict-add 'symbolic-open-data desc data)
         (dict-add 'symbolic-open-fpos desc 0)
+        (dict-add 'symbolic-open-name desc name)
         desc))))
 
 
@@ -50,10 +57,16 @@
     (let ((fpos (dict-get 'symbolic-open-fpos fd))
           (size (dict-get 'symbolic-open-size fd))
           (data (dict-get 'symbolic-open-data fd))
-          (len (min len (- size fpos) *symbolic-io-bufer-size*)))
+          (len (min len (- size fpos) *symbolic-io-buffer-size*)))
       (symbolic-copy-to-concrete data buf fpos len)
       (dict-add 'symbolic-open-fpos fd (+ fpos len))
       len)))
+
+(defun fread (ptr size n stream)
+  (declare (external "fread"))
+  (let ((r (read (fileno stream) buf (* n size))))
+    (if (< r 0) r
+      (/ r size))))
 
 (defun fopen (path mode)
   (declare (external "fopen"))
@@ -61,19 +74,29 @@
         (desc (symbolic-open-input name)))
     (if (= desc -1) 0
       (incr opened-streams)
+      (msg "associating stream $0 with fd $1" opened-streams desc)
       (dict-add 'symbolic-streams opened-streams desc)
       opened-streams)))
+
+(defun fdopen (fd mode)
+  (declare (external "fdopen"))
+  (if (< fd opened-descriptors) 0
+    (incr opened-streams)
+    (dict-add 'symbolic-streams opened-streams fd)
+    opened-streams))
 
 ;; we return 0 for all unassociated streams thus treating
 ;; them as stdin. We could, instead, open some symbolic
 ;; file for an unknown stream.
-(defun desc-of-stream (stream)
+(defun fileno (stream)
+  (declare (external "fileno"))
   (if (= stream 0) -1
+    (msg "stream $0 has fd $1" stream (dict-get 'symbolic-streams stream))
     (or (dict-get 'symbolic-streams stream) 0)))
 
 (defun fgetc (file)
   (declare (external "fgetc" "getc"))
-  (let ((fd (desc-of-stream file)))
+  (let ((fd (fileno file)))
     (if (= fd -1) -1
     (let ((fpos (dict-get 'symbolic-open-fpos fd))
           (size (dict-get 'symbolic-open-size fd))
@@ -87,14 +110,15 @@
   (fgetc symbolic-stdin))
 
 (defun init-symbolic-stdin ()
+  (msg "initializing stdin")
   (dict-add 'symbolic-open-fpos 0 0)
   (dict-add 'symbolic-open-size 0 *symbolic-initial-file-size*)
   (dict-add 'symbolic-open-data 0
             (symbolic-memory 'stdin 0 (-1 *symbolic-initial-file-size*)))
-  (set opened-descriptors 2))
+  (set opened-descriptors 2)
+  (dict-add 'symbolic-streams opened-streams 0)
+  (set opened-streams minimum-stream-number))
 
 
 (defmethod init ()
-  (init-symbolic-stdin)
-  (dict-add 'symbolic-streams opened-streams 0)
-  (set opened-streams (+1 minimum-stream-number)))
+  (init-symbolic-stdin))
