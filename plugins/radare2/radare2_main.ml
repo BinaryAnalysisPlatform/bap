111df620dad329057b98a37aa81b5c4030219b2d
Phosphorus15 <steepout@qq.com>
2020-06-24 23:45:15 +0800

    clean-up refactor & indentation fix

--- plugins/radare2/radare2_main.ml
+++ plugins/radare2/radare2_main.ml
@@ -21,7 +21,31 @@ let provide_roots funcs =
   promise_property Theory.Label.is_valid;
   promise_property Theory.Label.is_subroutine
 
-exception Radare2_failed
+let extract_name (json : Yojson.t) = 
+  match json with
+  | Yojson.(`Assoc list) -> 
+    (match List.find list ~f:(fun (key, _) -> String.equal key "name") with
+     | Some (_, v) -> (match v with
+         | Yojson.(`String str) -> Some str
+         | _ -> None)
+     | _ -> None)
+  | _ -> None
+
+let extract_addr (json : Yojson.t) = 
+  match json with
+  | Yojson.(`Assoc list) -> 
+    (match List.find list ~f:(fun (key, _) -> String.equal key "vaddr") with
+     | Some (_, v) -> (match v with
+         | Yojson.(`Int i) -> Z.of_int i |> Some
+         | Yojson.(`Intlit s) -> Z.of_string s |> Some
+         | _ -> None)
+     | _ -> None)
+  | _ -> None
+
+let strip str = 
+  match String.chop_prefix str ~prefix:"sym.imp." with
+  | Some str -> str
+  | None -> str
 
 let provide_radare2 file = 
   let funcs = Hashtbl.create (module struct
@@ -29,51 +53,25 @@ let provide_radare2 file =
       let compare = Z.compare and hash = Z.hash
       let sexp_of_t x = Sexp.Atom (Z.to_string x)
     end) in
-    let accept name addr = Hashtbl.set funcs addr name in
-    let extract name json = let open Yojson in
-      match json with
-      | `Assoc list -> 
-        (match List.find list ~f:(fun (key, _) -> String.equal key name) with
-        | Some (_, v) -> Some v
-        | _ -> None)
-      | _ -> None in
-    try
-     let symbol_list = let open Yojson in
-      match R2.with_command_j "isj" file with
-      | `List list -> Some list
-      | _ -> None in
-     let strip str = let open String in
-      match chop_prefix str ~prefix:"sym.imp." with
-      | Some str -> str
-      | None -> str in
-     let open Yojson in
-     let to_string json = 
-      match json with
-      | `String str -> Some str
-      | _ -> None in
-     let to_int json = 
-      match json with
-      | `Int i -> Z.of_int i |> Some
-      | `Intlit s -> Z.of_string s |> Some
-      | _ -> None in
-     Option.(symbol_list >>| List.fold ~init:() ~f:(fun () symbol -> 
-            both
-            (extract "name" symbol >>= to_string >>| strip) 
-            (extract "vaddr" symbol >>= to_int) 
-            |> value_map ~default:() ~f:(fun (name, addr) -> accept name addr)
-     ) |> value ~default:());
-     if Hashtbl.length funcs = 0
-     then warning "failed to obtain symbols";
-     let symbolizer = Symbolizer.create @@ fun addr ->
-     Hashtbl.find funcs @@
-     Bitvec.to_bigint (Word.to_bitvec addr) in
-     Symbolizer.provide agent symbolizer;
-     provide_roots funcs
-    with _ -> warning "failed to use radare2";()
+  let accept name addr = Hashtbl.set funcs addr name in
+  let symbol_list = match R2.with_command_j "isj" file with
+    | Yojson.(`List list) -> Some list
+    | s -> warning "unexpected radare2 output: %a" Yojson.pp s; None
+    | exception _ -> warning "failed to get symbols - radare2 command failed"; None in
+  Option.iter symbol_list ~f:(List.iter ~f:(fun s -> match extract_name s, extract_addr s with
+      | Some name, Some addr -> accept (strip name) addr
+      | _ -> debug "skipping json item %a" Yojson.pp s));
+  if Hashtbl.length funcs = 0
+  then warning "failed to obtain symbols";
+  let symbolizer = Symbolizer.create @@ fun addr ->
+    Hashtbl.find funcs @@
+    Bitvec.to_bigint (Word.to_bitvec addr) in
+  Symbolizer.provide agent symbolizer;
+  provide_roots funcs
 
 
 
-let main = Stream.observe Project.Info.file @@ provide_radare2
+let main () = Stream.observe Project.Info.file @@ provide_radare2
 
 let () =
   Config.manpage [
@@ -87,4 +85,4 @@ let () =
     `S  "SEE ALSO";
     `P  "$(b,bap-plugin-objdump)(1)"
   ];
-  Config.when_ready (fun {get=_} -> main)
+  Config.when_ready (fun {get=_} -> main ())
