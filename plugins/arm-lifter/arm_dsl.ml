2b847c07a875eeef3f38f88258c6d1fb7fafcdc3
Phosphorus15 <steepout@qq.com>
2020-07-07 22:42:21 +0800

    simplified dsl extension

--- plugins/arm-lifter/arm_dsl.ml
+++ plugins/arm-lifter/arm_dsl.ml
@@ -5,38 +5,29 @@ open KB.Syntax
 module Env  = Arm_env.Env
 module Common = Dsl_common
 
+module Arm_cpu(Core : Theory.Core) = struct
+  open Core
+  exception Assert_error
+  include Env
+  let assert_val (op : operand) = match op with
+    | `Reg r -> load_reg r |> var
+    | `Imm i -> (int value (Bap.Std.Word.to_bitvec i))
+  let assert_var = function
+    | `Reg r -> load_reg r
+    | `Imm i -> raise Assert_error
+end
+
 module Make_Extend(Core : Theory.Core)(Holder : Common.ValueHolder) = struct
   open Core
-
-  module Arm_cpu = struct
-    exception Assert_error
-    include Env
-    let assert_val (op : operand) = match op with
-      | `Reg r -> load_reg r |> var
-      | `Imm i -> (int value (Bap.Std.Word.to_bitvec i))
-    let assert_var = function
-      | `Reg r -> load_reg r
-      | `Imm i -> raise Assert_error
-  end
-  module DSL = Dsl_common.DSL(Core)(Arm_cpu)(Holder)
+  module CPU = Arm_cpu(Core)
+  module DSL = Dsl_common.DSL(Core)(CPU)(Holder)
 
   include DSL
 end
 
 module Make(Core : Theory.Core) = struct
-  open Core
-
-  module Arm_cpu = struct
-    exception Assert_error
-    include Env
-    let assert_val (op : operand) = match op with
-      | `Reg r -> load_reg r |> var
-      | `Imm i -> (int value (Bap.Std.Word.to_bitvec i))
-    let assert_var = function
-      | `Reg r -> load_reg r
-      | `Imm i -> raise Assert_error
-  end
-  module DSL = Dsl_common.DSL(Core)(Arm_cpu)(Arm_cpu)
-
-  include DSL
+  module CPU_Holder = Arm_cpu(Core)
+  module Nested = Make_Extend(Core)(CPU_Holder)
+  
+  include Nested
 end
