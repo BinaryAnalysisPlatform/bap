BinaryAnalysisPlatform/bap

commit 32391a7444f4c66d9f9af47dee9724564d22b3be

enables ARM Thumb/Thumb2 and interworking  (#1178)
* full implementation of ARM thumb

* adds arch check

* + patch CMN instruction
+ removed debug output

* instruction `magic word` should be 10 bits

* + renamed grps
+ new pc resolution

* fix pc usage & assignment

* fix `bl` and `blx`

* top-level exception handling

* integrates the thumb plugin with BAP build system

Right now we put thumb as a separate plugin, but we will probably move
it into the ARM plugin before the release, as there is no
reason (logical) to have it in a separate plugin. But lets move in
small steps.

It is recommended to use [omake][1] for rapid builds.

[1]: https://github.com/BinaryAnalysisPlatform/bap/wiki/Experimental-OMake-backend

* removes exceptions and implements a proper plugin registration

Removes exceptions, we don't use them in general, only to indicate a
programmers error. Turned everything into failwith and we will,
eventually, get rid of all of them.

The lifter is now using encodings instead of architectures. That
ensures that it will be called in proper times. Also, makes the loader
invocation a little bit more straightforward.

Finally, implements a proper plugin extension registration - we shall
not register the lifter until we get the permission.

* reimplements the Memory theory

* rewrote the memory operations

* enables predicates when the disassembler is switched

When the disassembler is switched it may not have the predicates set
correctly. Ideally, we should track predicates per disassembler/per
state instead of resetting it on every switch, but benchmarking
doesn't show any significant performance impact, so we will just keep
it simple.

Also fixes a bug in the `with_preds` function, in which the condition
was flipped, i.e., predicates should be reset if there are predicates
to drop not the other way around.

* stops arm lifter from lifting unknown instructions

it was producing specials.

lib/bap_image/bap_memory.ml
@@ -212,34 +212,28 @@ module Input = struct
   let int256 = read `r256
 end
 
-let sub copy ?(word_size=`r8) ?from ?words  t : t or_error =
+let view_exn ?(word_size=`r8) ?from ?words  t =
   let amin = Option.value from ~default:(min_addr t) in
-  let amax =
-    Option.map words
-      ~f:(fun w -> Addr.(amin ++ Int.(w * Size.in_bytes word_size - 1))) |>
-    Option.value ~default:(max_addr t) in
-  Validate.(result @@ name "view must not be empty" @@
-            Addr.validate_lbound amax ~min:(Incl amin)) >>= fun () ->
-  Addr.Int_err.(!$amax - !$amin >>= Addr.to_int) >>= fun diff ->
-  let size = diff + 1 in
-  Addr.Int_err.(!$amin - !$(t.addr) >>= Addr.to_int) >>= fun off ->
-  let off = t.off + off in
-  let check_preconditions = Validate.(name_list "preconditions" [
-      name "offset in bounds" @@ Int.validate_bound off
-        ~min:(Incl t.off)
-        ~max:(Excl (t.off + t.size));
-      name_list "size fits" [
-        Int.validate_bound size
-          ~min:(Incl 1) ~max:(Incl t.size);
-        Int.validate_ubound (off + size) ~max:(Incl (t.off + t.size))
-      ];
-    ]) in
-  Validate.result check_preconditions >>= fun () ->
-  if size = 1 then return (make_byte t amin off)
-  else return { t with size; data = t.data; addr = amin; off}
+  let amax = match words with
+    | None -> max_addr t
+    | Some w -> Addr.(amin ++ Int.(w * Size.in_bytes word_size - 1)) in
+  if Word.(amax < amin) then invalid_arg "out-of-bounds"
+  else
+    let size = Addr.(to_int_exn @@ amax - amin) + 1 in
+    let off = Addr.(to_int_exn @@ amin - t.addr) + t.off in
+    if off >= t.off && off < t.off + t.size then match size with
+      | 0 -> invalid_arg "empty view"
+      | 1 -> make_byte t amin off
+      | n when n <= t.size ->
+        { t with size; data = t.data; addr = amin; off}
+      | _ -> invalid_arg "out-of-bounds"
+    else invalid_arg "out-of-bounds"
 
-let view = sub ident
-let copy = sub Bigstring.subo
+let view ?word_size ?from ?words mem =
+  try Ok (view_exn ?word_size ?from ?words mem)
+  with exn -> Or_error.of_exn exn
+
+let copy = view
 
 let range mem a1 a2 =
   Addr.Int_err.(!$a2 - !$a1) >>= Addr.to_int >>= fun bytes ->

commit 5ff6b605254b12e0c5df904ea1cf0bec53222f0d

fixes two bugs in the Memory module
The first bug was breaking the raw loader at least. It bug was
introduced in #1178 during the `Memory.view` optimization. A check was
missed that allowed for creation of invalid views. The invalid views
later were caught with an assertion check, (easily reproduced with
`bap /bin/ls --loader=raw`), e.g.,

```
("Assert_failure bap_memory.ml:72:2"
  "Raised at file \"bap_memory.ml\", line 72, characters 2-31\
 \nCalled from file \"bap_memory.ml\", line 196, characters 2-18\
 \nCalled from file \"bap_memory.ml\", line 410, characters 26-52\
 \nCalled from file \"bap_trie.ml\", line 46, characters 34-53\
 \nCalled from file \"bap_trie.ml\", line 62, characters 4-130\
 \nCalled from file \"bap_byteweight.ml\", line 78, characters 12-39\
 \nCalled from file \"bap_byteweight.ml\", line 129, characters 12-39\
 \nCalled from file \"byteweight_main.ml\", line 50, characters 44-56\
 \nCalled from file \"src/sequence.ml\", line 123, characters 29-36\
 \nCalled from file \"byteweight_main.ml\" (inlined), line 49, characters 4-116\
 \nCalled from file \"byteweight_main.ml\", line 48, characters 4-146\
...
```

The second bug was probably all the time in the library and concerns
`Memory.find_map` and `Memory.find_if`, which are talking an optional
`word_size` parameter that was ignored and functions were always
iterating over bytes.

lib/bap_image/bap_memory.ml
@@ -213,7 +213,7 @@ module Input = struct
   let int256 = read `r256
 end
 
-let view_exn ?(word_size=`r8) ?from ?words  t =
+let view_exn ?(word_size=`r8) ?from ?words t =
   let amin = Option.value from ~default:(min_addr t) in
   let amax = match words with
     | None -> max_addr t
@@ -225,7 +225,7 @@ let view_exn ?(word_size=`r8) ?from ?words  t =
     if off >= t.off && off < t.off + t.size then match size with
       | 0 -> invalid_arg "empty view"
       | 1 -> make_byte t amin off
-      | n when n <= t.size ->
+      | n when n <= t.size && off + size <= t.off + t.size ->
         { t with size; data = t.data; addr = amin; off}
       | _ -> invalid_arg "out-of-bounds"
     else invalid_arg "out-of-bounds"
@@ -354,12 +354,12 @@ module Make_iterators( M : Monad.S) = struct
 
   let find_map ?word_size t ~f =
     with_return (fun s ->
-        iteri t ~f:(fun a w -> f a w >>= function
+        iteri ?word_size t ~f:(fun a w -> f a w >>= function
           | None -> return ()
           | some -> s.return (return some)) >>| fun () -> None)
 
   let find_if ?word_size t ~f =
-    find_map t ~f:(fun a w -> f a w >>| function
+    find_map ?word_size t ~f:(fun a w -> f a w >>| function
       | true  -> Some w
       | false -> None)
 end