f9b028f0f870ce6988dd1f628d82c4ff8f324768
ivg <ivg@ieee.org>
2021-04-01 20:00:50 -0400

    uses the new ABI to implement riscv abi

--- plugins/riscv/riscv_main.ml
+++ plugins/riscv/riscv_main.ml
@@ -1,4 +1,4 @@
-open Base
+open Core_kernel
 open Bap_main
 open Bap.Std
 open Bap_core_theory
@@ -46,79 +46,65 @@ let enable_loader () =
 module Abi = struct
   open Bap_c.Std
   open Bap.Std
-  open Monads.Std
-  open Monad.Option.Syntax
-  open Monad.Option.Let
 
-  let name = "riscv"            (* is there an official name? *)
-
-  let (.:()) file num = match Set.nth file num with
-    | None -> failwith "a wrong number of registers"
-    | Some v -> Var.reify v
-
-  let (.%()) file num = Bil.var file.:(num)
+  module Arg = C.Abi.Arg
+  open Arg.Let
+  open Arg.Syntax
 
   let is_floating = function
     | `Basic {C.Type.Spec.t=#C.Type.real} -> true
     | _ -> false
 
-  (* even x = x if x is even otherwise x+1 *)
-  let even x = x + x land 1
-
   let data_model t =
     let bits = Theory.Target.bits t in
     new C.Size.base (if bits = 32 then `ILP32 else `LP64)
 
-  let insert_args t _sub _attrs {C.Type.Proto.return; args} =
-    let bits = Theory.Target.bits t in
-    let a = Theory.Target.regs t ~roles:Theory.Role.Register.[
-        integer; function_argument;
-      ] in
-    let fa = Theory.Target.regs t ~roles:Theory.Role.Register.[
-        floating; function_argument;
-      ] in
-    let regs = Set.length a in
-    let mem = Bil.var @@ Var.reify @@ Theory.Target.data t in
-    let* sp = Theory.Target.reg t Theory.Role.Register.stack_pointer >>| Var.reify in
-    let size = data_model t in
-    let stack t n =
-      size#bits t >>= Size.of_int_opt >>| fun sz ->
-      C.Abi.data size t,
-      Bil.load ~mem LittleEndian sz
-        ~addr:Bil.(var sp + int (Word.of_int ~width:bits n)) in
-    let param t n =
-      if n > regs then stack t (n - regs)
-      else
-        size#bits t >>= fun s ->
-        Monad.Option.guard (s <= 2 * bits) >>| fun () ->
-        C.Abi.data size t, match is_floating return,s <= bits with
-        | true,true -> fa.%(n)
-        | true,false -> Bil.concat fa.%(even n) fa.%(even n + 1)
-        | false,true -> a.%(n)
-        | false,false -> Bil.concat a.%(even n) a.%(even n + 1) in
-    let return = param return 0 in
-    let+ (_,params) = Monad.Option.List.fold args
-        ~init:(0,[]) ~f:(fun (used,pars) (_name,arg) ->
-            size#bits arg >>= fun argsz ->
-            param arg used >>| fun par ->
-            let used = if argsz <= bits then used + 1
-              else used + 2 + used land 1 in
-            used,par::pars) in
-    {C.Abi.return; params = List.rev params; hidden=[]}
+  let define t =
+    let model = data_model t in
+    C.Abi.define t model @@ fun _ {C.Type.Proto.return=r; args} ->
+    let* iargs = Arg.Arena.iargs t in
+    let* irets = Arg.Arena.irets t in
+    let* fargs = Arg.Arena.fargs t in
+    let* frets = Arg.Arena.frets t in
 
-  let apply_headers proj =
-    let t = Project.target proj in
-    if Theory.Target.belongs Target.parent t then
-      let abi = C.Abi.{
-          insert_args = insert_args t;
-          apply_attrs = fun _ x -> x;
-        } in
-      C.Abi.register name abi;
-      let size = data_model t in
-      let apply_headers = C.Abi.create_api_processor size abi in
-      Bap_api.process apply_headers;
-      Project.set proj Bap_abi.name name
-    else proj
+    (* integer calling convention *)
+    let integer regs t =
+      Arg.count regs t >>= function
+      | None -> Arg.reject ()
+      | Some 1 -> Arg.choice [
+          Arg.register regs t;
+          Arg.memory t;
+        ]
+      | Some 2 -> Arg.choice [
+          Arg.sequence [
+            Arg.align_even regs;
+            Arg.registers ~limit:2 regs t;
+          ];
+          Arg.split_with_memory regs t;
+          Arg.memory t;
+        ]
+      | Some _ -> Arg.reference regs t in
+
+    (* floating-point calling convention *)
+    let float iregs fregs t =
+      Arg.count fregs t >>= function
+      | Some 1 -> Arg.choice [
+          Arg.register fregs t;
+          Arg.register iregs t;
+          Arg.memory t;
+        ]
+      | _ -> integer iregs t in
+
+    let arg iregs fregs r =
+      if is_floating r
+      then float iregs fregs r
+      else integer iregs r in
+
+    Arg.define ?return:(match r with
+        | `Void -> None
+        | r -> Some (arg irets frets r))
+      (Arg.List.iter args ~f:(fun (_,t) ->
+           arg iargs fargs t));
 end
 
 
@@ -127,7 +113,8 @@ let main _ctxt =
   enable_llvm Target.llvm32 "riscv32";
   enable_loader ();
   provide_decoding ();
-  Bap_abi.register_pass Abi.apply_headers;
+  Abi.define Target.riscv32;
+  Abi.define Target.riscv64;
   Ok ()
 
 let () = Bap_main.Extension.declare main
