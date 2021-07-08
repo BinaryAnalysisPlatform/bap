9f8e2c58ca444f72f5a442843b8c760e82170c5b
ivg <ivg@ieee.org>
2021-07-08 12:43:20 -0400

    implements AVR ABI and refines AVR targets

--- lib/bap_avr/bap_avr_target.ml
+++ lib/bap_avr/bap_avr_target.ml
@@ -8,6 +8,8 @@ let package = "bap"
 type r16 and r8
 
 type 'a bitv = 'a Theory.Bitv.t Theory.Value.sort
+type reg = r8 Theory.Bitv.t Theory.var
+
 
 let r16 : r16 bitv = Theory.Bitv.define 16
 let r8  : r8  bitv = Theory.Bitv.define 8
@@ -15,17 +17,29 @@ let bool = Theory.Bool.t
 
 let reg t n = Theory.Var.define t n
 
-let array ?(index=string_of_int) t pref size =
-  List.init size ~f:(fun i -> reg t (pref ^ index i))
+let array ?(rev=false) ?(start=0) ?(index=string_of_int) t pref size =
+  let stop = if rev then start-size else start+size in
+  let stride = if rev then -1 else 1 in
+  List.range ~stride start stop |>
+  List.map ~f:(fun i -> reg t (pref ^ index i))
 
 let untyped = List.map ~f:Theory.Var.forget
 let (@<) xs ys = untyped xs @ untyped ys
 
-let gpr = array r8 "R" 32
+let regs t = List.map ~f:(reg t)
+let nums = array r8 "R" 24
+let wxyz = regs r8 [
+    "Wlo"; "Whi";
+    "Xlo"; "Xhi";
+    "Ylo"; "Yhi";
+    "Zlo"; "Zhi";
+  ]
+let gpr = nums @< wxyz
 let sp = reg r16 "SP"
-let flags = List.map ~f:(reg bool) [
+let flags = regs bool [
     "CF"; "ZF"; "NF"; "VF"; "SF"; "HF"; "TF"; "IF"
   ]
+
 
 let datas = Theory.Mem.define r16 r8
 let codes = Theory.Mem.define r16 r16
@@ -33,16 +47,47 @@ let codes = Theory.Mem.define r16 r16
 let data = reg datas "data"
 let code = reg codes "code"
 
-let parent = Theory.Target.declare ~package "avr"
+let parent = Theory.Target.declare ~package "avr8"
     ~bits:8
     ~byte:8
     ~endianness:Theory.Endianness.le
 
-let atmega328 = Theory.Target.declare ~package "ATmega328"
+
+let tiny = Theory.Target.declare ~package "avr8-tiny"
     ~parent
     ~data
     ~code
     ~vars:(gpr @< [sp] @< flags @< [data] @< [code])
+
+let mega = Theory.Target.declare ~package "avr8-mega"
+    ~parent:tiny
+
+let xmega = Theory.Target.declare ~package "avr8-xmega"
+    ~parent:mega
+
+module Gcc = struct
+  let abi = Theory.Abi.declare ~package "avr-gcc"
+  let wreg = regs r8 ["Whi"; "Wlo"]
+  let args = wreg @ array ~rev:true ~start:23 r8 "R" 16
+  let rets = wreg @ array ~rev:true ~start:23 r8 "R" 6
+  let regs = Theory.Role.Register.[
+      [general; integer], gpr;
+      [function_argument], untyped args;
+      [function_return], untyped rets;
+      [stack_pointer], untyped [sp];
+      [caller_saved], rets @< regs r8 ["R0"; "Xlo"; "Xhi"; "Zlo"; "Zhi"];
+      [callee_saved], array ~start:1 r8 "R" 17 @< regs r8 ["Ylo"; "Yhi"];
+    ]
+
+  let target parent name =
+    Theory.Target.declare ~package name ~regs ~parent ~abi
+
+  let tiny = target tiny "avr8-tiny-gcc"
+  let mega = target mega "avr8-mega-gcc"
+  let xmega = target xmega "avr8-xmega-gcc"
+end
+
+
 
 let pcode =
   Theory.Language.declare ~package:"bap" "pcode-avr"
@@ -69,7 +114,7 @@ let enable_loader () =
     | Ok arch -> arch in
   KB.promise Theory.Unit.target @@ fun unit ->
   KB.collect Image.Spec.slot unit >>| request_arch >>| function
-  | Some "avr" -> atmega328
+  | Some "avr" -> Gcc.mega
   | _ -> Theory.Target.unknown
 
 
