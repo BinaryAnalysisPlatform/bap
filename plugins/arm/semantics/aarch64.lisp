cf95551784e9c01493fecaf8e09e479cedb0f120
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-02-04 02:03:02 +0000

    add MADD, MSUB, SDIV, UDIV and conditional select

--- plugins/arm/semantics/aarch64.lisp
+++ plugins/arm/semantics/aarch64.lisp
@@ -147,6 +147,33 @@
 (defun ADDXrs (rd rn rm off)
   (set$ rd (+ rn (shifted rm off))))
 
+
+(defmacro Mop*rrr (set op rd rn rm ra)
+  "(Mop*rrr set op rd rn rm ra) implements multiply-add, multiply-subtract
+   etc with W or X registers. op is the binary operation used after *."
+  (set rd (op ra (* rn rm))))
+
+;; MUL*rr is alias of MADD*rrr and gets converted
+(defun MADDWrrr (rd rn rm ra) (Mop*rrr setw + rd rn rm ra))
+(defun MADDXrrr (rd rn rm ra) (Mop*rrr set$ + rd rn rm ra))
+;; MNEG*rr is alias of MSUB*rrr and gets converted
+(defun MSUBWrrr (rd rn rm ra) (Mop*rrr setw - rd rn rm ra))
+(defun MSUBXrrr (rd rn rm ra) (Mop*rrr set$ - rd rn rm ra))
+
+
+(defmacro *DIV*r (set div rd rn rm)
+  "(*DIV*r set div rd rn rm) implements the SDIV or UDIV instructions
+   on W or X registers, with div set to s/ or / respectively."
+  (if (= rm 0)
+    (set rd 0)
+    (set rd (div rn rm))))
+
+(defun SDIVWr (rd rn rm) (*DIV*r setw s/ rd rn rm))
+(defun SDIVXr (rd rn rm) (*DIV*r set$ s/ rd rn rm))
+(defun UDIVWr (rd rn rm) (*DIV*r setw /  rd rn rm))
+(defun UDIVXr (rd rn rm) (*DIV*r set$ /  rd rn rm))
+
+
 (defun shifted (rm off)
   (declare (visibility :private))
   (let ((typ (extract 7 6 off))
@@ -213,6 +240,25 @@
 (defun LDRXroX (rt rn rm _ shift)
   (set$ rt (load-word (+ rn (lshift rm (* shift 3))))))
 
+
+(defmacro CSop*r (set op rd rn rm cnd)
+  "(CSop*r set op rd rn rm cnd) implements the conditional select
+   instruction on W or X registers, with op being applied to rm
+   when cnd is false."
+  (if (condition-holds cnd)
+    (set rd rn)
+    (set rd (op rm))))
+
+(defun id (arg) (declare (visibility :private)) arg)
+
+(defun CSELWr  (rd rn rm cnd) (CSop*r setw id   rd rn rm cnd))
+(defun CSELXr  (rd rn rm cnd) (CSop*r set$ id   rd rn rm cnd))
+(defun CSINCWr (rd rn rm cnd) (CSop*r setw +1   rd rn rm cnd))
+(defun CSINCXr (rd rn rm cnd) (CSop*r set$ +1   rd rn rm cnd))
+(defun CSINVWr (rd rn rm cnd) (CSop*r setw lnot rd rn rm cnd))
+(defun CSINVXr (rd rn rm cnd) (CSop*r set$ lnot rd rn rm cnd))
+(defun CSNEGWr (rd rn rm cnd) (CSop*r setw neg  rd rn rm cnd))  ;; 2's complement negation
+(defun CSNEGXr (rd rn rm cnd) (CSop*r set$ neg  rd rn rm cnd))  ;; 2's complement negation
 
 
 (defun STRBBui (src reg off)
