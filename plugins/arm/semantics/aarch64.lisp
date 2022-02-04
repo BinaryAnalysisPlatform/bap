51ad92c3c25630e361334275812c17db1c6bd803
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-02-04 01:59:24 +0000

    Add barrier instructions via the special primitive

--- plugins/arm/semantics/aarch64.lisp
+++ plugins/arm/semantics/aarch64.lisp
@@ -259,9 +259,21 @@
   (when (condition-holds cnd)
     (relative-jump off)))
 
+
+(defun DMB (option)
+  (special (barrier-option-to-symbol :dmb option)))
+
+(defun DSB (option)
+  (special (barrier-option-to-symbol :dsb option)))
+
+(defun ISB (option)
+  ;; strictly speaking, only the sy option is valid and is
+  ;; the default option (it can be omitted from the mnemonic).
+  ;; still including option here though
+  (special (barrier-option-to-symbol :dmb option)))
+
 (defun HINT (_)
   (empty))
-
 
 (defun UDF (exn)
   (special :undefined-instruction))
--- plugins/arm/semantics/arm-bits.lisp
+++ plugins/arm/semantics/arm-bits.lisp
@@ -103,3 +103,38 @@
         (immr (extract 11 6 mask))
         (imms (extract 5 0 mask)))
     (decode-bit-masks N imms immr true)))
+
+(defun barrier-option-to-symbol (barrier-type option)
+  (case barrier-type
+    :dmb
+      (case option
+        0b1111 :barrier-dmb-sy
+        0b1110 :barrier-dmb-st
+        0b1101 :barrier-dmb-ld
+        0b1011 :barrier-dmb-ish
+        0b1010 :barrier-dmb-ishst
+        0b1001 :barrier-dmb-ishld
+        0b0111 :barrier-dmb-nsh
+        0b0110 :barrier-dmb-nshst
+        0b0101 :barrier-dmb-nshld
+        0b0011 :barrier-dmb-osh
+        0b0010 :barrier-dmb-oshst
+        0b0001 :barrier-dmb-oshld
+        :barrier-dmb-unknown)
+    :dsb
+      (case option
+        0b1111 :barrier-dsb-sy
+        0b1110 :barrier-dsb-st
+        0b1101 :barrier-dsb-ld
+        0b1011 :barrier-dsb-ish
+        0b1010 :barrier-dsb-ishst
+        0b1001 :barrier-dsb-ishld
+        0b0111 :barrier-dsb-nsh
+        0b0110 :barrier-dsb-nshst
+        0b0101 :barrier-dsb-nshld
+        0b0011 :barrier-dsb-osh
+        0b0010 :barrier-dsb-oshst
+        0b0001 :barrier-dsb-oshld
+        :barrier-dsb-unknown)
+    :isb
+      :barrier-isb-sy))
