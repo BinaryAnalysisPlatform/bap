28dc108a2be56ffa6c0351da60542fae2acd73b4
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-04-11 03:53:11 +0000

    implement CAS and friends for X registers llvm mnemonics most likely incorrect, will investigate why bap's llvm doesn't disassemble these insns

--- plugins/arm/semantics/aarch64-atomic.lisp
+++ plugins/arm/semantics/aarch64-atomic.lisp
@@ -4,6 +4,27 @@
 
 ;;; ATOMIC OPERATIONS
 
+;; WARNING: these llvm mnemonics are very likely incorrect,
+;; need to investigate why bap's llvm can't disassemble any instructions
+;; from armv8.1 or later.
+
+(defmacro CASordXr (rs rt rn acquire-ordering release-ordering)
+  "(CASord*r set load store rs rt rn acquire-ordering release-ordering)
+   implements a generic compare-and-swap instruction on a X register.
+   acquire-ordering and release-ordering are booleans indicating whether
+   load-acquire and store-release ordering is to be enforced."
+   (let ((data) (load-word rn))
+    (if (acquire-ordering) (special :load-acquire))
+    (if (= data rs)
+      (if (release-odering) (special :store-release))
+      (store-word rn rt))
+    (set$ rs data)))
+
+(defun CASXr   (rs rt rn) (CASordXr rs rt rn false false))
+(defun CASAXr  (rs rt rn) (CASordXr rs rt rn true  false))
+(defun CASLXr  (rs rt rn) (CASordXr rs rt rn false true))
+(defun CASALXr (rs rt rn) (CASordXr rs rt rn true  true))
+
 (defmacro CSop*r (set op rd rn rm cnd)
   "(CSop*r set op rd rn rm cnd) implements the conditional select
    instruction on W or X registers, with op being applied to rm
