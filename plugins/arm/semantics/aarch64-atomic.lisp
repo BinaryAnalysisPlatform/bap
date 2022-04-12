1dd5ed41b996024fcde9a9c0562ba4cb3197ff30
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-04-12 11:48:38 +0000

    generalise CAS and friends to 8-bit and 16-bit

--- plugins/arm/semantics/aarch64-atomic.lisp
+++ plugins/arm/semantics/aarch64-atomic.lisp
@@ -18,28 +18,48 @@
       (store rn rt))
     (set rs data)))
 
+(defmacro store-hword (dst src) (store-word dst (cast-low 32 src)))
+(defmacro load-quarter-word (addr) (load-bits 16 addr))
+(defmacro store-quarter-word (dst src) (store-word dst (cast-low 16 src)))
+
 (defmacro CASordX (rs rt rn acquire-ordering release-ordering)
   "Specialisation of CASord* for X registers."
   (CASord* set$ load-word store-word rs rt rn acquire-ordering release-ordering))
-
-(defmacro store-hword (dst src) (store-word dst (cast-low 32 src)))
 
 (defmacro CASordW (rs rt rn acquire-ordering release-ordering)
   "Specialisation of CASord* for W registers."
   (CASord* setw load-hword store-hword rs rt rn acquire-ordering release-ordering))
 
+(defmacro CASordB (rs rt rn acquire-ordering release-ordering)
+  "Specialisation of CASord* operating on individual bytes."
+  (CASord* setw memory-read store-byte rs rt rn acquire-ordering release-ordering))
+
+(defmacro CASordH (rs rt rn acquire-ordering release-ordering)
+  "Specialisation of CASord* for 16-bit values."
+  (CASord* setw load-quarter-word store-quarter-word rs rt rn acquire-ordering release-ordering))
+
 ;; not sure why llvm returns 4 arguments.
 ;; when i've tested it, the first and second arguments are always the same value
 ;; so i'm just assuming they're the same and ignoring the second.
 (defun CASX   (rs _ rt rn) (CASordX rs rt rn false false))
-(defun CASAX  (rs _ rt rn) (CASordX rs rt rn true false))
+(defun CASAX  (rs _ rt rn) (CASordX rs rt rn true  false))
 (defun CASLX  (rs _ rt rn) (CASordX rs rt rn false true))
 (defun CASALX (rs _ rt rn) (CASordX rs rt rn true  true))
 
 (defun CASW   (rs _ rt rn) (CASordW rs rt rn false false))
-(defun CASAW  (rs _ rt rn) (CASordW rs rt rn true false))
+(defun CASAW  (rs _ rt rn) (CASordW rs rt rn true  false))
 (defun CASLW  (rs _ rt rn) (CASordW rs rt rn false true))
 (defun CASALW (rs _ rt rn) (CASordW rs rt rn true  true))
+
+(defun CASB   (rs _ rt rn) (CASordB rs rt rn false false))
+(defun CASAB  (rs _ rt rn) (CASordB rs rt rn true  false))
+(defun CASLB  (rs _ rt rn) (CASordB rs rt rn false true))
+(defun CASALB (rs _ rt rn) (CASordB rs rt rn true  true))
+
+(defun CASH   (rs _ rt rn) (CASordH rs rt rn false false))
+(defun CASAH  (rs _ rt rn) (CASordH rs rt rn true  false))
+(defun CASLH  (rs _ rt rn) (CASordH rs rt rn false true))
+(defun CASALH (rs _ rt rn) (CASordH rs rt rn true  true))
 
 
 (defmacro CSop*r (set op rd rn rm cnd)
