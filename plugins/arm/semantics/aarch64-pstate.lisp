e9bd12f207e3cb65f894ab855aa47347ecb40c4f
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-04-21 10:10:29 +1000

    fixes aarch64 bitmask immediate encoding and implements some aarch64 instructions (#1458)
    * Add some bitvector functions and assert-msg macro
    
    * Add immediate decoding and logical immediate insns
    
    * Add barrier instructions via the special primitive
    
    * add MADD, MSUB, SDIV, UDIV and conditional select
    
    * Fix zeros and ones bitvector constructors
    
    * organise instructions into categories
    
    * move ADRP to integer arithmetic category
    
    * remove private access from arm-bits functions
    
    * Added stur instructions and fixed bug in `condition-holds` macro
    
    * Bug fixes for STUR insns
    
    * condense repeated definitions into macros
    
    * implement TBZ and TBNZ
    
    * separated into files
    
    * rename aarch64 files and make aarch64-helper
    
    * add some processor state instructions
    LLVM can't seem to disassemble ARMv8.4 instructions like RMIF, SETF8
    and SETF16. Also, CFINV gets turned into MSR (register) but LLVM
    returns ill-formed asm...?
    I've commented this in aarch64-pstate.lisp.
    
    * fix typo in pstate instructions
    i typed is_zero with underscore instead of primitive is-zero
    
    * implement rotate-left
    
    * add missing documentation and rename for clarity
    documentation added for macros and helper functions.
    
    * apply upstream changes from #1454
    
    * implement CAS and friends for X registers
    llvm mnemonics most likely incorrect, will investigate
    why bap's llvm doesn't disassemble these insns
    
    * correct CAS mnemonic and code
    i've used ` bap mc --cpu=cortex-a55 --triple=aarch64`
    to get the llvm mnemonic, but will need to talk to ivan
    about lisp context and
    specifying generic armv8.x instead of a specific cpu
    
    * fix typos in CAS instruction code
    
    * fix CFINV, RMIF, SETF8 and SETF16 using #1461
    
    * use clz instead of loop for highest-set-bit
    
    * miscellaneous changes to helper functions
    
    * update barrier code to use symbol-concat (#1452)
    
    * fix CAS and friends for X registers
    
    * generalise CAS to W or X registers
    
    * generalise CAS and friends to 8-bit and 16-bit
    
    * rename CAS parameter names
    
    * fixes the cast-signed Primus Lisp primitive
    
    * fixes the arithmetic modulus in Primus Lisp primitives
    
    The Primus Lisp semantic primitives were hardcoding 64-bit
    arithmetic, which was obviously incorrect. In addition, the
    shifting operations were coercing the operands to the same size,
    like in arithmetic operations, which contradicts the established
    semantics of shifts both in Core Theory and in BIL. Now, the shifting
    operators will produce values of the same sort as the sort of the
    first operand.
    
    * adds arbitrary-precision loopless clz and popcount in Primus Lisp
    
    * uses clz instead of clz64 and properly casts the operand
    
    * use fixed cast-signed primitive from #1462
    
    * fix bitmask decoding of W registers
    the result in decode-bit-masks should only be
    replicated to the width of the registers being assigned to.
    so, we need to pass in the register width when decoding.
    
    Co-authored-by: Tom Malcolm <ThomasMalcolm01@gmail.com>
    Co-authored-by: Tom <52238845+Thomas-Malcolm@users.noreply.github.com>
    Co-authored-by: ivg <ivg@ieee.org>

---
+++ plugins/arm/semantics/aarch64-pstate.lisp
@@ -0,0 +1,56 @@
+(declare (context (target arm armv8-a+le)))
+
+(in-package aarch64)
+
+;;; SPECIFIC PROCESSOR STATE INSTRUCTIONS
+
+(defmacro CCMN** (rn rm-or-imm nzcv cnd)
+  "(CCMN** rn rm-or-imm nzcv cnd) implements either CCMN*i
+   or CCMN*r. The semantics are the same at the lisp level;
+   an immediate or register value is given as second argument."
+  (if (condition-holds cnd)
+    (set-nzcv-from-registers (+ rn rm-or-imm) rn rm-or-imm)
+    (set-nzcv nzcv)))
+
+(defun CCMNWi (rn imm nzcv cnd) (CCMN** rn imm nzcv cnd))
+(defun CCMNXi (rn imm nzcv cnd) (CCMN** rn imm nzcv cnd))
+(defun CCMNWr (rn rm  nzcv cnd) (CCMN** rn rm  nzcv cnd))
+(defun CCMNXr (rn rm  nzcv cnd) (CCMN** rn rm  nzcv cnd))
+
+(defmacro CCMP** (rn rm-or-imm nzcv cnd)
+  "(CCMP** rn rm-or-imm nzcv cnd) implements either CCMP*i
+   or CCMP*r. The semantics are the same at the lisp level;
+   an immediate or register value is given as second argument."
+  (if (condition-holds cnd)
+    (let ((rm-or-imm (lnot rm-or-imm)))
+      (set-nzcv-from-registers (+ rn rm-or-imm 1) rn rm-or-imm))
+    (set-nzcv nzcv)))
+
+(defun CCMPWi (rn imm nzcv cnd) (CCMP** rn imm nzcv cnd))
+(defun CCMPXi (rn imm nzcv cnd) (CCMP** rn imm nzcv cnd))
+(defun CCMPWr (rn rm  nzcv cnd) (CCMP** rn rm  nzcv cnd))
+(defun CCMPXr (rn rm  nzcv cnd) (CCMP** rn rm  nzcv cnd))
+
+(defun CFINV () (set CF (lnot CF)))
+;; (defun MSR () )
+
+(defun RMIF (rn imm nzcv-mask)
+  (let ((tmp (extract 3 0 (rotate-right rn imm))))
+    (when (= 1 (select 3 nzcv-mask))
+      (set NF (select 3 tmp)))
+    (when (= 1 (select 2 nzcv-mask))
+      (set ZF (select 2 tmp)))
+    (when (= 1 (select 1 nzcv-mask))
+      (set CF (select 1 tmp)))
+    (when (= 1 (select 0 nzcv-mask))
+      (set VF (select 0 tmp)))))
+
+(defun SETF8 (rn)
+  (set NF (select 7 rn))
+  (set ZF (is-zero (extract 7 0 rn)))
+  (set VF (logxor (select (+ 7 1) rn) (select 7 rn))))
+
+(defun SETF16 (rn)
+  (set NF (select 15 rn))
+  (set ZF (is-zero (extract 15 0 rn)))
+  (set VF (logxor (select (+ 15 1) rn) (select 15 rn))))
--- plugins/arm/semantics/arm-bits.lisp
+++ plugins/arm/semantics/arm-bits.lisp
@@ -3,30 +3,54 @@
 
 (in-package arm)
 
-(defun set-flags (r x y)
+(defun set-nzcv-from-registers (r x y)
+  "(set-nzcv-from-registers r x y) sets the processor state flags
+   to the result of some arithmetic operation (op x y) with r as the result.
+   Common examples include:
+    (set-nzcv-from-registers (+ x y) x y)
+   or
+    (set-nzcv-from-registers (+ x y 1) x y)
+   This function was formerly named set-flags, but was renamed to improve clarity."
   (set NF (msb r))
   (set VF (overflow r x y))
   (set ZF (is-zero r))
   (set CF (carry r x y)))
 
+(defun set-nzcv (nzcv)
+  "(set-nzcv nzcv) sets the negative, zero, carry and overflow flags to
+   the bottom 4 bits of nzcv."
+  (set NF (select 3 nzcv))
+  (set ZF (select 2 nzcv))
+  (set CF (select 1 nzcv))
+  (set VF (select 0 nzcv)))
+
 (defun add-with-carry (rd x y c)
+  "(add-with-carry rd x y c) sets rd to the result of adding x and y
+   with carry bit c, and sets processor flags."
   (let ((r (+ c y x)))
-    (set-flags r x y)
+    (set-nzcv-from-registers r x y)
     (set$ rd r)))
 
 (defun add-with-carry/clear-base (rd x y c)
+  "(add-with-carry/clear-base rd x y c) sets rd to the result of adding x and y
+   with carry bit c after clearing the base register rd, and sets processor flags."
   (let ((r (+ c y x)))
-    (set-flags r y x)
+    (set-nzcv-from-registers r y x)
     (setw rd r)))
 
 (defun add-with-carry/it-block (rd x y c cnd)
+  "(add-with-carry/it-block rd x y c cnd) sets rd to the result of adding x and y
+   with carry bit c if cnd holds, and sets processor flags if cnd is unconditional."
   (when (condition-holds cnd)
     (let ((r (+ c y x)))
       (when (is-unconditional cnd)
-        (set-flags r x y))
+        (set-nzcv-from-registers r x y))
       (set$ rd r))))
 
 (defmacro shift-with-carry (shift rd rn rm cnd)
+  "(shift-with-carry shift rd rn rm cnd) sets rd to the shifted
+   value of rn and rm, and relevant processor flags, when cnd holds.
+   The overflow flag is not changed."
   (when (condition-holds cnd)
     (let ((r (cast-signed (word-width) rn)))
       (when (is-unconditional cnd)
@@ -36,11 +60,13 @@
         (set NF (msb rd))))))
 
 (defun condition-holds (cnd)
+  "(condition-holds cnd) calculates the result of the given condition cnd
+   based on the values of processor flags."
   (case cnd
     0b0000 ZF
     0b0001 (lnot ZF)
     0b0010 CF
-    0b0010 (lnot CF)
+    0b0011 (lnot CF)
     0b0100 NF
     0b0101 (lnot NF)
     0b0110 VF
@@ -54,9 +80,18 @@
     true))
 
 (defun is-unconditional (cnd)
+  "(is-unconditional cnd) checks whether cnd is unconditional, i.e. 0b1110."
   (= cnd 0b1110))
 
 (defmacro setw (reg val)
   "(set Wx V) sets a Wx register clearing the upper 32 bits."
-  (set$ (alias-base-register reg)
-        (cast-unsigned (word-width) val)))
+  (set$ (alias-base-register reg) val))
+
+(defun replicate-to-fill (bitv n)
+  "(replicate-to-fill bitv n) returns the result of repeating bitv
+   to a total of n bits. Requires that n is a multiple of bitv's length.
+   Modified from the bits(N) Replicate(bits(M) x) function from
+   ARMv8 ISA pseudocode."
+  (let ((bitv-length (word-width bitv)))
+    (assert-msg (= 0 (mod n bitv-length)) "replicate-to-fill n not multiple of len(bitv)")
+    (replicate bitv (/ n bitv-length))))
