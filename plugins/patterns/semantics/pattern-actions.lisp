1cb99072634e1b0adb8f7064b96271dd29303aee
Ivan Gotovchits <ivg@ieee.org>
2022-04-29 15:07:31 -0400

    enables disabling the patterns plugin (#1481)
    Just passing `--no-patterns` didn't work since the plugin was defining
    the `promise-function-start` function therefore the `patterns-action`
    Primus Lisp feature was issuing a missing function type error.
    
    To prevent this, we define a  context class that is instantiated if
    the plugin is enabled. An alternative solution would be to define the
    primitive even if the plugin is disabled.

--- plugins/patterns/semantics/pattern-actions.lisp
+++ plugins/patterns/semantics/pattern-actions.lisp
@@ -1,5 +1,7 @@
 (in-package bap)
 
+(declare (context (patterns enabled)))
+
 (defmethod bap:patterns-action (action addr attrs)
   (when (is-in action 'funcstart 'possiblefuncstart)
     (promise-function-start addr)))
