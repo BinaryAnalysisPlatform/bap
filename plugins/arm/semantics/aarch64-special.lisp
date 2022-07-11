d767f7ad3e89b6207be65a16f1c24e3725e427df
alistair <alistair.michael@uq.edu.au>
2022-07-11 06:45:33 +0000

    add BRK

--- plugins/arm/semantics/aarch64-special.lisp
+++ plugins/arm/semantics/aarch64-special.lisp
@@ -24,3 +24,34 @@
 
 (defun UDF (exn)
   (intrinsic 'undefined-instruction))
+
+
+(defun bvectosymbol (bv sym) 
+  (if (>= (word-width bv) 4)
+    (bvectosymbol 
+      (cast-low (- (word-width bv) 4) bv) 
+      (symbol-concat 
+        sym
+        (case (cast-high 4 bv)
+            0x0 '0
+            0x1 '1
+            0x2 '2
+            0x3 '3
+            0x4 '4
+            0x5 '5
+            0x6 '6
+            0x7 '7
+            0x8 '8
+            0x9 '9
+            0xa 'a
+            0xb 'b
+            0xc 'c
+            0xd 'd
+            0xe 'e
+            0xf 'f
+            0 'x
+    )))
+  sym))
+
+(defun BRK (option)
+  (intrinsic (symbol-concat 'software-breakpoint-$ (bvectosymbol option '0x))))
