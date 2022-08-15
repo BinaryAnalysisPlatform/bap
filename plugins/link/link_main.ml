52b2b99ee27350870153ec5978d74461df850364
bmourad01 <bmourad@draper.com>
2022-08-15 12:39:42 -0400

    Multiple improvements
    - Use aliases if there is no matching subroutine
    - Link in the minimal slice of the callgraph by default
    - Remove the stub subroutines after linking

--- plugins/link/link_main.ml
+++ plugins/link/link_main.ml
@@ -67,36 +67,61 @@ let collect_externals package target units =
             info "Creating symtab for unit %s%!" path;
             Symtab.create disasm calls >>= fun symtab ->
             info "Lifting program for unit %s%!" path;
-            Program.KB.lift symtab >>| fun prog ->
-            let subs =
-              Term.enum sub_t prog |>
-              Seq.fold ~init:String.Map.empty ~f:(fun subs sub ->
-                  Map.add_exn subs ~key:(Sub.name sub) ~data:sub) in
-            (path, subs) :: exts)
+            Program.KB.lift symtab >>= fun prog ->
+            let cg = lazy (Program.to_graph prog) in
+            let init = String.Map.(empty, empty) in
+            Term.enum sub_t prog |>
+            KB.Seq.fold ~init ~f:(fun (subs, aliases) sub ->
+                let subs = Map.add_exn subs ~key:(Sub.name sub) ~data:sub in
+                match Term.get_attr sub address with
+                | None -> !!(subs, aliases)
+                | Some addr ->
+                  let addr = Word.to_bitvec addr in
+                  Theory.Label.for_addr addr >>=
+                  KB.collect Theory.Label.aliases >>| fun a ->
+                  let aliases =
+                    Set.fold a ~init:aliases ~f:(fun m key ->
+                        Map.add_multi m ~key ~data:sub) in
+                  subs, aliases) >>| fun (subs, aliases) ->
+            (path, subs, aliases, cg) :: exts)
   end;
-  Toplevel.get result
+  let result = Toplevel.get result in
+  Toplevel.exec @@ KB.Symbol.set_package package;
+  result
+
+let find_sub_in_ext name (path, subs, aliases, _) =
+  match Map.find subs name with
+  | Some sub -> Some sub
+  | None -> match Map.find aliases name with
+    | None -> None
+    | Some [sub] -> Some sub
+    | Some _ ->
+      warning "Stub %s was found in unit %s, but has more \
+               than one implementation; skipping%!" name path;
+      None
 
 let resolve_stub exts stub =
   let name = Sub.name stub in
-  List.filter_map exts ~f:(fun (path, subs) -> Option.(
-      Map.find subs name >>= fun sub ->
+  List.filter_map exts ~f:(fun ((path, _, _, _) as ext) -> Option.(
+      find_sub_in_ext name ext >>= fun sub ->
       let is_stub = Term.has_attr sub Sub.stub in
       if is_stub then
         warning "Stub %s was found in unit %s, but still has the stub \
                  attribute; skipping%!" name path;
-      some_if (not is_stub) (Term.tid sub, path))) |> function
+      some_if (not is_stub) (Term.tid sub, Sub.name sub, path))) |> function
   | [] ->
     warning "Stub %s was not resolved, couldn't find suitable unit \
              containing this symbol%!" name;
     None
-  | [x] ->
-    info "Stub %s was resolved in unit %s%!" name (snd x);
-    Some x
+  | [tid, name', path] ->
+    info "Stub %s was resolved in unit %s (as %s)%!" name path name';
+    Some (tid, path)
   | x ->
-    let units = List.map x ~f:snd in
+    let units = List.map x ~f:(fun (_, name, path) ->
+        Format.sprintf "(%s, %s)" name path) in
     failwithf "Stub %s was not resolved, multiple units were \
                found containing this symbol: %s%!"
-      name (List.to_string ~f:Fn.id units) ()
+      name (String.concat units ~sep:", ") ()
 
 let replace_calls_to_stub prog replace =
   Term.filter_map sub_t prog ~f:(fun sub ->
@@ -113,31 +138,62 @@ let replace_calls_to_stub prog replace =
                   | Second _ -> jmp))
       else None)
 
-let resolve proj package stubs units =
+let minimal_slice tids cg =
+  let module N = Graphs.Callgraph.Node in
+  let stack = Stack.of_list tids in
+  let add acc s =
+    Seq.iter s ~f:(fun tid ->
+        if not @@ Set.mem acc tid then
+          Stack.push stack tid) in
+  let rec build acc = match Stack.pop stack with
+    | None -> acc
+    | Some tid ->
+      let acc = Set.add acc tid in
+      add acc @@ N.preds tid cg;
+      add acc @@ N.succs tid cg;
+      build acc in
+  build Tid.Set.empty
+
+let link_units tids exts to_link all prog =
+  let find_ext p =
+    List.find_exn exts ~f:(fun (path, _, _, _) ->
+        String.(p = path)) in
+  Set.fold to_link ~init:prog ~f:(fun prog path ->
+      let _, subs, _, cg = find_ext path in
+      if all then
+        Map.fold subs ~init:prog ~f:(fun ~key:_ ~data:sub prog ->
+            Term.append sub_t prog sub)
+      else
+        let tids = minimal_slice (Map.find_exn tids path) (Lazy.force cg) in
+        Map.fold subs ~init:prog ~f:(fun ~key:_ ~data:sub prog ->
+            if Set.mem tids @@ Term.tid sub then
+              Term.append sub_t prog sub
+            else prog))
+
+let remove_stubs replace prog =
+  Term.filter sub_t prog ~f:(fun sub ->
+      not @@ Map.mem replace @@ Term.tid sub)
+
+let resolve proj package stubs units all =
   let prog = Project.program proj in
   let target = Project.target proj in
   let exts = collect_externals package target units in
-  (* Reset the package. *)
-  Toplevel.exec @@ KB.Symbol.set_package package;
-  let replace, to_link =
-    let init = Tid.Map.empty, String.Set.empty in
+  let replace, tids, to_link =
+    let init = Tid.Map.empty, String.Map.empty, String.Set.empty in
     Term.enum sub_t prog |>
-    Seq.fold ~init ~f:(fun ((r, l) as acc) sub ->
+    Seq.fold ~init ~f:(fun ((r, t, l) as acc) sub ->
         let tid = Term.tid sub in
         if Set.mem stubs tid then
           match resolve_stub exts sub with
           | None -> acc
           | Some (new_tid, path) ->
-            Tid.set_name new_tid @@ Sub.name sub;
-            Map.set r ~key:tid ~data:new_tid, Set.add l path
+            Map.set r ~key:tid ~data:new_tid,
+            Map.add_multi t ~key:path ~data:new_tid,
+            Set.add l path
         else acc) in
-  let prog = replace_calls_to_stub prog replace in
-  let find_ext p =
-    List.find_exn exts ~f:(fun (path, _) -> String.(p = path)) in
-  Set.fold to_link ~init:prog ~f:(fun prog path ->
-      let _, subs = find_ext path in
-      Map.fold subs ~init:prog ~f:(fun ~key:_ ~data:sub prog ->
-          Term.append sub_t prog sub)) |>
+  replace_calls_to_stub prog replace |>
+  link_units tids exts to_link all |>
+  remove_stubs replace |>
   Project.with_program proj
 
 let post proj =
@@ -145,13 +201,11 @@ let post proj =
   List.filter ~f:Project.Pass.autorun |>
   List.fold ~init:proj ~f:(Fn.flip Project.Pass.run_exn)
 
-let main units proj =
+let main units all proj =
   let package = current_package () in
   let stubs = stubs proj package in
   if Set.is_empty stubs then proj
-  else
-    let proj = resolve proj package stubs units in
-    post proj
+  else resolve proj package stubs units all |> post
 
 let () = Config.manpage [
     `S "DESCRIPTION";
@@ -163,8 +217,10 @@ let () = Config.manpage [
                names of our stubs.");
     `I ("5.", "If one and only one match is found, then all calls to the \
                stub are replaced with calls to the matching subroutine. \
-               All subroutines from this unit are then merged into the \
-               main program.");
+               All subroutines that are reachable and/or reached from this \
+               subroutine are then merged into the main program; e.g. the \
+               minimal set of subroutines will be linked in according to \
+               the callgraph.");
   ]
 
 let units =
@@ -172,5 +228,11 @@ let units =
              all available units will be searched." in
   Config.(param (list string) ~default:[] ~doc "units")
 
-let () = Config.when_ready @@ fun {Config.get = (!)} ->
-  Project.register_pass (main !units)
+let all =
+  let doc = "If the implementation of a stub is found in a particular unit, \
+             then the entire unit will be linked in." in
+  Config.flag ~doc "all"
+
+let () =
+  Config.when_ready @@ fun {Config.get = (!)} ->
+  Project.register_pass @@ main !units !all
