4f98da206b0ef1e2cbbcd9e58604bd1ea3efb759
Kenneth Adam Miller <kennethadammiller@gmail.com>
2025-05-09 16:03:29 -0500

    Move cmdoptions from lib to plugin

--- lib/bap_superset_disasm/bap_superset_disasm.ml
+++ lib/bap_superset_disasm/bap_superset_disasm.ml
@@ -2,7 +2,6 @@ module Superset_impl = Superset_impl
 module Superset = Superset
 module Abstract_ssa = Abstract_ssa
 module Decision_trees = Decision_trees
-module Cmdoptions = Cmdoptions
 module Features = Features
 module Fixpoint = Fixpoint
 module Grammar = Grammar
