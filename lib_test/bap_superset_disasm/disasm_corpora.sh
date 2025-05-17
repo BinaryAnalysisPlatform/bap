7e23620465c345cfeb3402200d573ea7e8413553
Kenneth Adam Miller <kennethadammiller@gmail.com>
2025-05-16 20:32:49 -0500

    Update analyses and heuristics args in disasm_corpora.sh

--- lib_test/bap_superset_disasm/disasm_corpora.sh
+++ lib_test/bap_superset_disasm/disasm_corpora.sh
@@ -1,5 +1,5 @@
 find /Volumes -type f -executable -exec sh -c "file -i '{}' | grep -q 'x-executable; charset=binary'" \; -print > files.txt
 
-time cat files.txt | parallel "bap superset-disasm --find-fn-culprit --heuristics=Callsites3,FixedpointGrammar,ImgEntry,TrimLimitedClamped --ground-truth-bin={} {}"
+time cat files.txt | parallel "bap superset-disasm --find-fn-culprit --analyses="StronglyConnectedComponentData","Grammar-convergent" --heuristics=Callsites3,FixpointGrammar,ImgEntry,InterpretationDepthOne,FixpointInterpDepthOne --ground-truth-bin={} {}"
 
 bap supersetd-graph-metrics ./files.txt --print-fn-bins
